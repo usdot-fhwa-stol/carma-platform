441c3a68b887eb23dad84512565ff6cb66423f11
maefromm <MaeMargarette.M.Fromm@leidos.com>
2017-08-14 17:54:13 -0400

    commit changes of namespace from carmajava to carma

---
+++ carmajava/template/src/main/java/gov/dot/fhwa/saxton/carma/template/NodeName.java
@@ -0,0 +1,131 @@
+/*
+ * TODO: Copyright (C) 2017 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+//TODO: Naming convention of "package gov.dot.fhwa.saxton.carma.<template>;"
+//Originally "com.github.rosjava.carma.template;"
+package gov.dot.fhwa.saxton.carma.template;
+
+import org.apache.commons.logging.Log;
+import org.ros.message.MessageListener;
+import org.ros.node.topic.Subscriber;
+
+import org.ros.concurrent.CancellableLoop;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.NodeMain;
+import org.ros.node.topic.Publisher;
+
+import org.ros.node.parameter.ParameterTree;
+import org.ros.namespace.NameResolver;
+import org.ros.message.MessageFactory;
+
+/**
+ * A simple {@link Publisher} {@link NodeMain}.
+ * <p>
+ * Replace PubSub with the node name on Column D but using CamelCase.
+ *
+ * Command line test: rosrun carma template gov.dot.fhwa.saxton.carma.template.NodeName
+ */
+public class NodeName extends AbstractNodeMain {
+
+  //TODO: Replace with Column D node name
+  @Override
+  public GraphName getDefaultNodeName() {
+    return GraphName.of("node_name");
+  }
+
+  @Override
+  public void onStart(final ConnectedNode connectedNode) {
+
+    final Log log = connectedNode.getLog();
+
+    //TODO: Column G topic name
+    // Currently setup to listen to it's own message. Change to listen to someone other topic.
+    Subscriber<cav_msgs.SystemAlert> subscriber = connectedNode.newSubscriber("system_alert", cav_msgs.SystemAlert._TYPE);
+
+    subscriber.addMessageListener(new MessageListener<cav_msgs.SystemAlert>() {
+                                    @Override
+                                    public void onNewMessage(cav_msgs.SystemAlert message) {
+
+                                      String messageTypeFullDescription = "NA";
+
+                                      switch (message.getType()) {
+                                        case cav_msgs.SystemAlert.CAUTION:
+                                          messageTypeFullDescription = "Take caution! ";
+                                          break;
+                                        case cav_msgs.SystemAlert.WARNING:
+                                          messageTypeFullDescription = "I have a warning! ";
+                                          break;
+                                        case cav_msgs.SystemAlert.FATAL:
+                                          messageTypeFullDescription = "I am FATAL! ";
+                                          break;
+                                        case cav_msgs.SystemAlert.NOT_READY:
+                                          messageTypeFullDescription = "I am NOT Ready! ";
+                                          break;
+                                        case cav_msgs.SystemAlert.SYSTEM_READY:
+                                          messageTypeFullDescription = "I am Ready! ";
+                                          break;
+                                        default:
+                                          messageTypeFullDescription = "I am NOT Ready! ";
+                                      }
+
+                                      //TODO: Replace with Column D node name
+                                      log.info("node_name heard: \"" + message.getDescription() + ";" + messageTypeFullDescription + "\"");
+
+                                    }//onNewMessage
+                                  }//MessageListener
+    );//addMessageListener
+
+    //TODO: Column G topic name
+    final Publisher<cav_msgs.SystemAlert> systemAlertPublisher =
+      connectedNode.newPublisher("system_alert", cav_msgs.SystemAlert._TYPE);
+
+
+    //Getting the ros param called run_id.
+    ParameterTree param = connectedNode.getParameterTree();
+    final String rosRunID = param.getString("/run_id");
+    //params.setString("~/param_name", param_value);
+
+    // This CancellableLoop will be canceled automatically when the node shuts
+    // down.
+    connectedNode.executeCancellableLoop(new CancellableLoop() {
+                                           private int sequenceNumber;
+
+                                           @Override
+                                           protected void setup() {
+                                             sequenceNumber = 0;
+                                           }//setup
+
+                                           @Override
+                                           protected void loop() throws InterruptedException {
+
+                                             //TODO: Replace with column D node name
+                                             cav_msgs.SystemAlert systemAlertMsg = systemAlertPublisher.newMessage();
+                                             systemAlertMsg.setDescription("Hello World! " + "I am node_name. " + sequenceNumber + " run_id = " + rosRunID + ".");
+                                             systemAlertMsg.setType(cav_msgs.SystemAlert.SYSTEM_READY);
+
+                                             systemAlertPublisher.publish(systemAlertMsg);
+
+                                             sequenceNumber++;
+                                             Thread.sleep(1000);
+                                           }//loop
+
+                                         }//CancellableLoop
+    );//executeCancellableLoop
+  }//onStart
+}//AbstractNodeMain
+
---
+++ carmajava/template/src/main/java/gov/dot/fhwa/saxton/carma/template/PubSub.java
@@ -0,0 +1,99 @@
+/*
+ * TODO: Copyright (C) 2017 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+//TODO: Naming convention of "package gov.dot.fhwa.saxton.carma.<template>;"
+//Originally "com.github.rosjava.carma.template;"
+package gov.dot.fhwa.saxton.carma.template;
+
+import org.apache.commons.logging.Log;
+import org.ros.message.MessageListener;
+import org.ros.node.topic.Subscriber;
+
+import org.ros.concurrent.CancellableLoop;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.NodeMain;
+import org.ros.node.topic.Publisher;
+
+/**
+ * A simple {@link Publisher} {@link NodeMain}.
+ *
+ * Replace PubSub with the node name on Column D but using CamelCase.
+ *
+ * Command line test: rosrun carma template gov.dot.fhwa.saxton.carma.template.PubSub
+ */
+public class PubSub extends AbstractNodeMain {
+
+    //TODO: Replace with Column D node name
+    @Override
+    public GraphName getDefaultNodeName() {
+        return GraphName.of("pub_sub");
+    }
+
+    @Override
+    public void onStart(final ConnectedNode connectedNode) {
+
+        final Log log = connectedNode.getLog();
+
+        //TODO: Column G topic name
+        // Currently setup to listen to it's own message. Change to listen to someone other topic.
+        Subscriber<std_msgs.String> subscriber = connectedNode.newSubscriber("template", std_msgs.String._TYPE);
+
+        subscriber.addMessageListener(new MessageListener<std_msgs.String>() {
+                                          @Override
+                                          public void onNewMessage(std_msgs.String message) {
+                                              //TODO: Replace with Column D node name
+                                              log.info("pub_sub heard: \"" + message.getData() + "\"");
+                                          }//onNewMessage
+                                      }//MessageListener
+        );//addMessageListener
+
+	// Example cav_msgs
+	Subscriber<cav_msgs.BSM> bsm_sub = connectedNode.newSubscriber("bsm", cav_msgs.BSM._TYPE);
+
+        //TODO: Column G topic name
+        final Publisher<std_msgs.String> publisher =
+                connectedNode.newPublisher("template", std_msgs.String._TYPE);
+
+
+        // This CancellableLoop will be canceled automatically when the node shuts
+        // down.
+        connectedNode.executeCancellableLoop(new CancellableLoop() {
+                                                 private int sequenceNumber;
+
+                                                 @Override
+                                                 protected void setup() {
+                                                     sequenceNumber = 0;
+                                                 }//setup
+
+                                                 @Override
+                                                 protected void loop() throws InterruptedException {
+                                                     std_msgs.String str = publisher.newMessage();
+                                                     //TODO: Replace with Column D node name
+                                                     str.setData("Hello World! " + "I am pub_sub. " + sequenceNumber);
+                                                     publisher.publish(str);
+                                                     sequenceNumber++;
+                                                     Thread.sleep(1000);
+                                                 }//loop
+
+                                             }//CancellableLoop
+        );//executeCancellableLoop
+
+
+    }//onStart
+}//AbstractNodeMain
+
