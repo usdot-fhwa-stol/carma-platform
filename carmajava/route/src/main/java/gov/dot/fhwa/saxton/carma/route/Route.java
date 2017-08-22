b8a21750c35aa2d588fcdef170cde76156015e9a
Michael McConnell <Michael.McConnell-2@leidos.com>
2017-08-22 12:42:08 -0400

    added route package class files

---
+++ carmajava/route/src/main/java/gov/dot/fhwa/saxton/carma/route/Route.java
@@ -0,0 +1,114 @@
+/*
+ * TODO: Copyright (C) 2017 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package gov.dot.fhwa.saxton.carma.route;
+
+import java.util.List;
+import org.ros.message.Time;
+
+/**
+ * Class which represents the abstraction of a travel route which a vehicle will follow.
+ * A routes geometry is defined by RouteWaypoints which has lat/lon coordinates.
+ */
+public class Route {
+  Time expectedTimeOfArrival;
+  String routeID;
+  double routeLength;
+  List<RouteSegment> segments;
+  List<RouteWaypoint> waypoints;
+
+  /**
+   * Constructor which initializes a route object from the provided data file.
+   *
+   * @param filePath The path the the route file which will be loaded
+   * @param routeID The id to assign to the route. Should be unique
+   */
+  public Route(String filePath, String routeID) {
+  }
+
+  /**
+   * Constructor which initializes a route from a provided list of waypoints
+   *
+   * @param waypoints The list of waypoints which will be used to build the route
+   * @param routeID The id to assign to the route. Should be unique
+   * @param routeName The display name of the route
+   */
+  public Route(List<RouteWaypoint> waypoints, String routeID, String routeName) {
+
+  }
+
+  /**
+   * Gets the first (starting) segment of this route
+   *
+   * @return The route segment which is the first segment of the route
+   */
+  public RouteSegment getFirstSegment() {
+
+  }
+
+  /**
+   * Gets the last (ending) segment of this route
+   *
+   * @return The route segment which is the first segment of the route
+   */
+  public RouteSegment getLastSegment() {
+
+  }
+
+  /**
+   * Inserts the provided segment into the route at the specified index.
+   * The segment currently at that index will be placed before the inserted segment at index-1.
+   *
+   * @param segment The RouteSegment to be inserted. Must be able to connect to previous and next segments.
+   * @param index   The index at which to insert the RouteSegment. Inserting at a non-existent index will result in an exception.
+   * @return Returns true if the segment was inserted successfully. False otherwise.
+   */
+  public boolean insertSegment(RouteSegment segment, int index) {
+    return false;
+  }
+
+  /**
+   * Inserts the provided waypoint into the route at the specified index.
+   * The waypoint currently at that index will be placed before the inserted segment at index-1.
+   * Inserting a waypoint will result in an additional route segment being created.
+   *
+   * @param waypoint The RouteWaypoint to be inserted. Must be able to connect to previous and next waypoints
+   * @param index    The index at which to insert the RouteWaypoint. Inserting at a non-existent index will result in an exception.
+   * @return Returns true if the waypoint was inserted successfully. False otherwise.
+   */
+  public boolean insertWaypoint(RouteWaypoint waypoint, int index) {
+    return false;
+  }
+
+  /**
+   * Gets the length of this route in meters.
+   *
+   * @return The length of this route in meters
+   */
+  public double length() {
+
+  }
+
+  /**
+   * Loads a route into memory using the provided route loading strategy.
+   *
+   * @param strategy The strategy which will be used to load a route
+   * @return True if route loaded successfully false otherwise.
+   */
+  public boolean loadRoute(IRouteLoadStrategy strategy) {
+
+  }
+}
