42e08005d19e45111ecfe3ef7f08171a5c2d12ae
Kyle Rush <kyle.rush@leidos.com>
2017-09-06 16:38:53 -0400

    Add logging for not finding ItfcManager

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/GuidanceCommands.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/GuidanceCommands.java
@@ -17,6 +17,7 @@ public class GuidanceCommands implements Runnable {
     protected IPubSubService iPubSubService;
     protected ConnectedNode node;
     protected Log log;
+    protected long sleepDurationMillis = 30000;
 
     GuidanceCommands(IPubSubService iPubSubService, ConnectedNode node) {
         this.node = node;
@@ -60,8 +61,17 @@ public class GuidanceCommands implements Runnable {
 
             IPublisher<SpeedAccel> speedAccelPublisher =
                 iPubSubService.getPublisherForTopic(driverFqn, SpeedAccel._TYPE);
+
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(sleepDurationMillis);
+            }
+
         } catch (TopicNotFoundException e) {
+            log.error("No interface manager found to query for drivers!!!");
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 }
