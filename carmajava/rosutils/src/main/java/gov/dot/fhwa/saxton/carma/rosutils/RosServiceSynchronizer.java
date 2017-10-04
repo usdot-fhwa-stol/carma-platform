4a0a51f1a38fcc2c193672961831705cd93211d5
Kyle Rush <kyle.rush@leidos.com>
2017-10-04 10:30:27 -0400

    Improve doc comments

--- carmajava/rosutils/src/main/java/gov/dot/fhwa/saxton/carma/rosutils/RosServiceSynchronizer.java
+++ carmajava/rosutils/src/main/java/gov/dot/fhwa/saxton/carma/rosutils/RosServiceSynchronizer.java
@@ -1,3 +1,19 @@
+/*
+ * TODO: Copyright (C) 2017 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package gov.dot.fhwa.saxton.carma.rosutils;
 
 import org.ros.node.service.*;
@@ -13,6 +29,10 @@ public class RosServiceSynchronizer {
 
   /**
    * Asynchronously call a ROS service, returning a future that can be waited on as a result
+   * 
+   * @param client the {@link org.ros.node.service.ServiceClient} instance to use for communication
+   * @param request The request message to send
+   * @returns An awaitable future in the form of a {@link RosServiceResult}  instance
    */
   public static <R, T> RosServiceResult<T> callAsync(ServiceClient<R, T> client, R request) {
     final RosServiceResult<T> res = new RosServiceResult<>();
@@ -34,6 +54,10 @@ public class RosServiceSynchronizer {
 
   /**
    * Synchronously call a ROS service, handling the results with the ServiceResponseListener
+   * 
+   * @param client the {@link org.ros.node.service.ServiceClient} instance to use for communication
+   * @param request The request message to send
+   * @param listener The callbacks to execute upon success or failure of the ROS service call
    */
   public static final <R, T> void callSync(ServiceClient<R, T> client, R request, ServiceResponseListener<T> listener)
       throws InterruptedException {
