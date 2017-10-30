e631c7c7a8dbfb53c5f6cf54f78a29241b5a9a89
John Stark <starkj@leidos.com>
2017-10-30 16:34:44 -0400

    Added system version to GuidanceMain, added GetSystemVersion service; fixed obsolete version code in AbstractMockPlugin.

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/GuidanceMain.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/GuidanceMain.java
@@ -20,12 +20,17 @@ import cav_msgs.SystemAlert;
 import gov.dot.fhwa.saxton.carma.guidance.maneuvers.IManeuverInputs;
 import gov.dot.fhwa.saxton.carma.guidance.maneuvers.ManeuverInputs;
 import gov.dot.fhwa.saxton.carma.guidance.plugins.PluginManager;
+import cav_srvs.GetSystemVersion;
+import cav_srvs.GetSystemVersionRequest;
+import cav_srvs.GetSystemVersionResponse;
 import cav_srvs.SetGuidanceEngaged;
 import cav_srvs.SetGuidanceEngagedRequest;
 import cav_srvs.SetGuidanceEngagedResponse;
 import gov.dot.fhwa.saxton.carma.guidance.pubsub.*;
 import gov.dot.fhwa.saxton.carma.rosutils.AlertSeverity;
 import gov.dot.fhwa.saxton.carma.rosutils.SaxtonBaseNode;
+import gov.dot.fhwa.saxton.utils.ComponentVersion;
+
 import org.apache.commons.logging.Log;
 import org.ros.concurrent.CancellableLoop;
 import org.ros.exception.ServiceException;
@@ -54,6 +59,7 @@ public class GuidanceMain extends SaxtonBaseNode {
   // Member Variables
   protected ExecutorService executor;
   protected final int numThreads = 6;
+  protected ComponentVersion version = new ComponentVersion();
 
   protected GuidanceExceptionHandler guidanceExceptionHandler;
   protected IPubSubService pubSubService;
@@ -64,6 +70,28 @@ public class GuidanceMain extends SaxtonBaseNode {
   protected final AtomicBoolean engaged = new AtomicBoolean(false);
   protected final AtomicBoolean systemReady = new AtomicBoolean(false);
   protected boolean initialized = false;
+  
+  
+  
+  /**
+   * =============================================================================================================================
+   * This is the one and only place where system level version ID information is stored.  The rest of the system can get the info
+   * from here by using the get_system_version ROS service call.  Every release that external stakeholders will see (preferably
+   * every build) must have a unique ID (combination of major/intermediate/minor/build).
+   * =============================================================================================================================
+   */
+  GuidanceMain() {
+	  version.setName("Carma Platform");
+	  version.setMajorRevision(0);
+	  //if any one of the below items is not explicitly set it will not be displayed in the revision string.
+	  version.setIntermediateRevision(0);
+	  version.setMinorRevision(1);
+	  //version.setBuild(0);
+	  //version.setSuffix(""); //may want to add an explanatory tag to the end of the ID string to make test builds or one-offs more obvious
+  }
+  
+  
+  
 
   @Override
   public GraphName getDefaultNodeName() {
@@ -153,8 +181,6 @@ public class GuidanceMain extends SaxtonBaseNode {
     } //MessageListener
     );//addMessageListener
 
-
-
     guidanceEngageService = connectedNode.newServiceServer("set_guidance_engaged", SetGuidanceEngaged._TYPE,
         new ServiceResponseBuilder<SetGuidanceEngagedRequest, SetGuidanceEngagedResponse>() {
           @Override
@@ -166,6 +192,16 @@ public class GuidanceMain extends SaxtonBaseNode {
             setGuidanceEngagedResponse.setGuidanceStatus(state.get() == GuidanceState.ENGAGED);
           }
         });
+    
+    ServiceServer<GetSystemVersionRequest, GetSystemVersionResponse> systemVersionServer = 
+    		connectedNode.newServiceServer("get_system_version",  GetSystemVersion._TYPE, 
+    		new ServiceResponseBuilder<GetSystemVersionRequest, GetSystemVersionResponse>() {
+    			@Override
+    			public void build(GetSystemVersionRequest request, GetSystemVersionResponse response) throws ServiceException {
+    				response.setSystemName(version.componentName());
+    				response.setRevision(version.revisionString());
+    			}
+    		});
 
     // This CancellableLoop will be canceled automatically when the node shuts
     // down.
