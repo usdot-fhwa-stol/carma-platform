cce202b61b18c7254ac22b7e4377178a76b3104a
Michael McConnell <Michael.McConnell-2@leidos.com>
2017-10-30 12:10:03 -0400

    added license to package xml

--- carmajava/mock_drivers/src/test/data/pinpoint_stationary.m
+++ carmajava/mock_drivers/src/test/data/pinpoint_stationary.m
@@ -1,5 +1,18 @@
+%{
+    Copyright (C) 2017 LEIDOS.
 
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy of
+    the License at
 
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+    License for the specific language governing permissions and limitations under
+    the License.
+%}
 % Generate fake pinpoint data for non-moving vehicle
 % 10 minutes of data coming from a stationary host vehicle pinpoint
 % The data will be published at 50Hz in accorance with the real driver
