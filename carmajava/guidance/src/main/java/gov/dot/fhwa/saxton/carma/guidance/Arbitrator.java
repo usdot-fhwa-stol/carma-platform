036e0d9fd9512f9fd6952d25bcc7fd38af197e60
Kyle Rush <kyle.rush@leidos.com>
2017-11-02 10:35:06 -0400

    Add units to GlobalSpeedLimitConstraint instantiation log

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/Arbitrator.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/Arbitrator.java
@@ -161,7 +161,7 @@ public class Arbitrator extends GuidanceComponent {
     }
 
     constraints.add(new GlobalSpeedLimitConstraint(configuredSpeedLimit));
-    log.info("Arbitrator using GlobalSpeedLimitConstraint with limit: " + configuredSpeedLimit);
+    log.info("Arbitrator using GlobalSpeedLimitConstraint with limit: " + configuredSpeedLimit + " m/s");
 
     twistSubscriber = pubSubService.getSubscriberForTopic("velocity", TwistStamped._TYPE);
     twistSubscriber.registerOnMessageCallback(new OnMessageCallback<TwistStamped>() {
