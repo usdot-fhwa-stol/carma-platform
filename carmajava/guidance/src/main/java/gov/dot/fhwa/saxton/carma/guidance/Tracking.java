1c23efef14d716707dfdabf44159f80ddeb3d59a
Kyle Rush <kyle.rush@leidos.com>
2017-11-06 16:20:50 -0500

    Remove unecessary error handling

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/Tracking.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/Tracking.java
@@ -357,7 +357,6 @@ public class Tracking extends GuidanceComponent {
 					
 					@Override
 					public void onFailure(Exception e) {
-						throw new RosRuntimeException(e);
 					}
 				});
 				
