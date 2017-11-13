3e8ff67c0af558a432250be7df01e97383e41d6d
qswawrq <qiang@email.gwu.edu>
2017-11-13 12:32:27 -0800

    version id

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/CarmaVersion.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/CarmaVersion.java
@@ -38,8 +38,8 @@ public class CarmaVersion {
         int intermediate =  2;
         int minor =         0;
         // Don't touch this, automatically updated
-        int build = 1493;
-        String suffix = "less_logs_for_bsm5-SNAPSHOT(1)";
+        int build = 1539;
+        String suffix = "tracking_shudown_correctly1";
 
 //==============================================================================================================================
 
