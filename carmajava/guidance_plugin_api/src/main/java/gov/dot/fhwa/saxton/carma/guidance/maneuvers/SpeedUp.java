fa201f83f9e1a6a816b2ec49861c806cd6dffbaa
John Stark <starkj@leidos.com>
2017-11-15 08:24:22 -0500

    Slowdown maneuver now uses naturally negative deltaV and accel values; both speedup and slowdown throw exception when trying to plan for a negative displacement.

--- carmajava/guidance_plugin_api/src/main/java/gov/dot/fhwa/saxton/carma/guidance/maneuvers/SpeedUp.java
+++ carmajava/guidance_plugin_api/src/main/java/gov/dot/fhwa/saxton/carma/guidance/maneuvers/SpeedUp.java
@@ -75,6 +75,9 @@ public class SpeedUp extends LongitudinalManeuver {
         double deltaV = endSpeed_ - startSpeed_; //always positive
         double lagDistance = startSpeed_*inputs_.getResponseLag();
         double displacement = endDist - startDist - lagDistance;
+        if (displacement <= 0.0) {
+            throw new ArithmeticException("SpeedUp.planToTargetDistance attempting to use illegal displacement of " + displacement);
+        }
         workingAccel_ = (startSpeed_ * deltaV + 0.5 * deltaV * deltaV) / displacement;
         if (workingAccel_ <= 0.0  ||  workingAccel_ > maxAccel_) {
             throw new ArithmeticException("SpeedUp.plantoTargetDistance attempting to use illegal workingAccel of " + workingAccel_);
