b33f00e5ba1ac291fe9f5e2f4dc3d63bb2752d06
John Stark <starkj@leidos.com>
2017-11-15 07:50:29 -0500

    Add error traps to speed change maneuvers

--- carmajava/guidance_plugin_api/src/main/java/gov/dot/fhwa/saxton/carma/guidance/maneuvers/SpeedUp.java
+++ carmajava/guidance_plugin_api/src/main/java/gov/dot/fhwa/saxton/carma/guidance/maneuvers/SpeedUp.java
@@ -76,6 +76,9 @@ public class SpeedUp extends LongitudinalManeuver {
         double lagDistance = startSpeed_*inputs_.getResponseLag();
         double displacement = endDist - startDist - lagDistance;
         workingAccel_ = (startSpeed_ * deltaV + 0.5 * deltaV * deltaV) / displacement;
+        if (workingAccel_ <= 0.0  ||  workingAccel_ > maxAccel_) {
+            throw new ArithmeticException("SpeedUp.plantoTargetDistance attempting to use illegal workingAccel of " + workingAccel_);
+        }
 
         //compute the time it will take to perform this ideal speed change
         deltaT_ = deltaV / workingAccel_;
