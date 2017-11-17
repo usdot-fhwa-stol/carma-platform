020569af7601345bc937399892f823e8c990efd7
Kyle Rush <kyle.rush@leidos.com>
2017-11-17 15:15:26 -0500

    Flesh out ArbitratorStateMachine

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/arbitrator/ArbitratorStateMachine.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/arbitrator/ArbitratorStateMachine.java
@@ -16,17 +16,83 @@
 
 package gov.dot.fhwa.saxton.carma.guidance.arbitrator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class responsible for the logic of Arbitrator state transitions
  */
 public class ArbitratorStateMachine {
   private ArbitratorState state = ArbitratorState.INIT;
+  private List<ArbitratorStateChangeListener> listeners = new ArrayList<>();
 
-  public synchronized void processEvent(ArbitratorEvent event) {
-
+  /**
+   * Notify the ArbitratorStateMachine that an event has occurred and may cause the
+   * state to change.
+   */
+  public void processEvent(ArbitratorEvent event) {
+    ArbitratorState oldState;
+    synchronized (this) {
+      oldState = state;
+      switch (state) {
+      case INIT:
+        if (event == ArbitratorEvent.INITIALIZE) {
+          state = ArbitratorState.INITIAL_PLANNING;
+        }
+        break;
+      case INITIAL_PLANNING:
+        if (event == ArbitratorEvent.FINISHED_TRAJECTORY_PLANNING) {
+          state = ArbitratorState.AWAITING_REPLAN;
+        }
+        break;
+      case AWAITING_REPLAN:
+        if (event == ArbitratorEvent.TRAJECTORY_COMPLETION_ALERT) {
+          state = ArbitratorState.NORMAL_REPLANNING;
+        } else if (event == ArbitratorEvent.COMPLEX_TRAJECTORY_COMPLETION_ALERT) {
+          state = ArbitratorState.REPLAN_AFTER_COMPLEX_TRAJECTORY;
+        } else if (event == ArbitratorEvent.TRAJECTORY_FAILED_EXECUTION) {
+          state = ArbitratorState.REPLAN_DUE_TO_FAILED_TRAJECTORY;
+        }
+        break;
+      case NORMAL_REPLANNING:
+        if (event == ArbitratorEvent.FINISHED_TRAJECTORY_PLANNING) {
+          state = ArbitratorState.AWAITING_REPLAN;
+        }
+        break;
+      case REPLAN_AFTER_COMPLEX_TRAJECTORY:
+        if (event == ArbitratorEvent.FINISHED_TRAJECTORY_PLANNING) {
+          state = ArbitratorState.AWAITING_REPLAN;
+        }
+        break;
+      case REPLAN_DUE_TO_FAILED_TRAJECTORY:
+        if (event == ArbitratorEvent.FINISHED_TRAJECTORY_PLANNING) {
+          state = ArbitratorState.AWAITING_REPLAN;
+        }
+        break;
+      default:
+        throw new IllegalStateException("Arbitrator has detected an unhandled state!!!");
+      }
+    }
+    
+    // Call all the listeners if we've changed state
+    if (oldState != state) {
+      for (ArbitratorStateChangeListener listener : listeners) {
+        listener.onStateChange(state);
+      }
+    }
   }
 
+  /**
+   * Get the current state of the arbitrator state machine
+   */
   public synchronized ArbitratorState getState() {
     return state;
+  }
+
+  /**
+   * Register a state change listener to be called when the state changes
+   */
+  public void registerStateChangeListener(ArbitratorStateChangeListener listener) {
+    listeners.add(listener);
   }
 }
