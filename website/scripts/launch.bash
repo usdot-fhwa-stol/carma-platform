381b127f7d35b81de39053241860cbf21d5c8642
Michael McConnell <Michael.McConnell-2@leidos.com>
2017-12-19 21:17:13 -0500

    added working way of roslaunching from UI

--- website/scripts/launch.bash
+++ website/scripts/launch.bash
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+# Set JAVA_HOME and update PATH
+export JAVA_HOME=/usr/local/java/jdk1.8.0_131
+export PATH=${PATH}:${JAVA_HOME}/bin
+
+# Source ROS (Must be called after ROS_HOME assignment)
+source /opt/ros/kinetic/setup.bash
+
+# Source ROSJava
+source /home/mcconnelms/rosjava/devel/setup.bash
+
 # Source platform
-source ~/to13_ws/devel/setup.bash
+source /home/mcconnelms/to13_ws/devel/setup.bash
+
+# Ensure ROS_HOSTNAME is not set as we will be using ROS_IP
+unset ROS_HOSTNAME
 
 # Ensure ROS_IP is correct
 export ROS_IP=192.168.218.138
 
+# Ensure ROS_MASTER_URI is correct
+export ROS_MASTER_URI=http://192.168.218.138:11311/
+
 # Launch platform
-roslaunch --pid=~/to13_ws/pid.txt carma saxton_cav.launch mock_dsrc:=true mock_can:=true mock_radar:=true mock_pinpoint:=true mock_srx_controller:=true
+roslaunch --pid=/opt/carma/launch.pid carma saxton_cav.launch mock_dsrc:=true mock_can:=true mock_radar:=true mock_pinpoint:=true mock_srx_controller:=true
 
--- website/scripts/testLaunch.php
+++ website/scripts/testLaunch.php
@@ -1,7 +1,15 @@
 <?php
-  exec("bash /var/www/html/scripts/launch.bash");
-  echo "Launching"
-  sleep(8);
-  $output = shell_exec("cat /var/www/html/scripts/pid.txt");
-  exec("kill -INT " . $output);
+  $PID_FILE_PATH = "/opt/carma/launch.pid"; // File containing process id of roslaunch
+  if (file_exists($PID_FILE_PATH)) { // If this file exists roslaunch is running
+   header("Location: ../main.html"); // Move onto main.html
+   exit;
+  }
+  // Launch Platform
+  $bash_pid = shell_exec("sudo -u mcconnelms /var/www/html/scripts/launch.bash > /dev/null 2>&1 & echo $!");
+
+  // Switch to main.html
+  header("Location: ../main.html");
+  // How to kill roslaunch
+  // $launch_pid = shell_exec("cat " . $PID_FILE_PATH);
+  //exec("kill -INT " . $launch_pid);
 ?>
