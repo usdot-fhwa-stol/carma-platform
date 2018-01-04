cbf1bc4b90e81f400f62e6708fb5631547f5a761
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-01-04 10:40:26 -0500

    setting up launch system

--- carmajava/lateralcontroldriver/src/main/test/java/gov/dot/fhwa/saxton/carma/lateralcontroldriver/LateralControlDriverTest.java
+++ carmajava/lateralcontroldriver/src/main/test/java/gov/dot/fhwa/saxton/carma/lateralcontroldriver/LateralControlDriverTest.java
@@ -14,7 +14,7 @@
  * the License.
  */
 
-package gov.dot.fhwa.saxton.carma.interfacemgr;
+package gov.dot.fhwa.saxton.carma.lateralcontroldriver;
 
 import gov.dot.fhwa.saxton.carma.rosutils.SaxtonLogger;
 import org.apache.commons.logging.Log;
