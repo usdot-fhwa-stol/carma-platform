ebfdc430c1155a5a5aa7b6d2e6dd60f80eb3c64c
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-01-18 09:37:26 -0500

    adding recursive to chmod

--- engineering_tools/VehicleInstallScript.sh
+++ engineering_tools/VehicleInstallScript.sh
@@ -36,7 +36,7 @@ cd /var/www/html
 sudo rm -r *
 sudo cp -R -r /opt/carma/src/CarmaPlatform/website/* /var/www/html/
 
-chmod 777 /opt/carma/*
+chmod -R 777 /opt/carma/*
 
 echo 'Compiling carma and sourcing. ...'
 cd /opt/carma
