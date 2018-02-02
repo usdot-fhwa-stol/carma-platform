afafa78427658f4abce5c9517691cac15aea9700
Stark <starkj@leidos.com>
2018-02-02 09:00:19 -0500

    Changed version to 2.4.0.

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/CarmaVersion.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/CarmaVersion.java
@@ -39,8 +39,8 @@ public class CarmaVersion {
 
         String name =       "Carma Platform";
         int major =         2;
-        int intermediate =  3;
-        int minor =         5;
+        int intermediate =  4;
+        int minor =         0;
         // Don't touch this, automatically updated
         int build = 0;
         String suffix = "";
