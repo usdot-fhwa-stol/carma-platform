aade7fe5ad6d563b8c4a3bd785b3989bab6519fe
Kyle Rush <kyle.rush@leidos.com>
2018-02-13 13:48:13 -0500

    Add configurable notification time factor
    The plugin will now ensure that other vehicles receive a minimal amount of warning of an impending lane change

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/routefollowing/RouteFollowingPlugin.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/routefollowing/RouteFollowingPlugin.java
@@ -55,6 +55,7 @@ public class RouteFollowingPlugin extends AbstractPlugin implements IStrategicPl
     private double laneChangeRateFactor = 0.75;
     private double laneChangeDelayFactor = 1.5;
     private double laneChangeSafetyFactor = 1.5;
+    private double laneChangeNotificationTime = 1.0;
 
     private static final String LANE_CHANGE_PLUGIN_NAME = "Lane Change Plugin";
     private static final long LONG_SLEEP = 10000;
@@ -73,6 +74,7 @@ public class RouteFollowingPlugin extends AbstractPlugin implements IStrategicPl
         laneChangeRateFactor = pluginServiceLocator.getParameterSource().getDouble("~lane_change_rate_factor", 0.75);
         laneChangeDelayFactor = pluginServiceLocator.getParameterSource().getDouble("~lane_change_delay_factor", 1.5);
         laneChangeSafetyFactor = pluginServiceLocator.getParameterSource().getDouble("~lane_change_safety_factor", 1.5);
+        laneChangeNotificationTime = pluginServiceLocator.getParameterSource().getDouble("~lane_change_notification_time", 1.0);
         log.info("Route Following plugin initialized");
     }
 
@@ -142,7 +144,7 @@ public class RouteFollowingPlugin extends AbstractPlugin implements IStrategicPl
             if (laneChangeAtTrajEnd.getLocation() > traj.getEndLocation()) {
                 double speedLimit = routeService.getSpeedLimitAtLocation(laneChangeAtTrajEnd.getLocation()).getLimit();
                 double distanceRequiredForLaneChange = speedLimit * laneChangeDelayFactor
-                        + speedLimit * laneChangeRateFactor + speedLimit * laneChangeSafetyFactor;
+                        + speedLimit * laneChangeRateFactor + speedLimit * laneChangeSafetyFactor + speedLimit * laneChangeNotificationTime;
                 double distanceAvailableForLaneChange = laneChangeAtTrajEnd.getLocation() - traj.getEndLocation();
 
                 if (distanceAvailableForLaneChange < distanceRequiredForLaneChange) {
