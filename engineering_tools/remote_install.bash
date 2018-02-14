78260362cca4a73222d6d6625b03e82156ae1fb7
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-02-14 12:50:16 -0500

    fixing a number of bugs in the script syntax

--- engineering_tools/remote_install.bash
+++ engineering_tools/remote_install.bash
@@ -8,7 +8,8 @@
 USERNAME=$1
 
 HOST="192.168.88.10"
-INSTALL_DIR="/opt/carma/install"
+CATKIN_WS="/opt/carma"
+INSTALL_DIR="${CATKIN_WS}/install"
 BUILD=false
 EVERYTHING=true
 EXECUTABLES=false
@@ -17,24 +18,24 @@ ROUTES=false
 URDF=false
 LAUNCH=false
 
-while getopts h:b:e:p:r:u:e:t:l:i: option
+while getopts h:bpruelt:c: option
 do
 	case "${option}"
 	in
-		h) HOST=${OPTARG};
-		b) BUILD=${OPTARG};
-		p) PARAMS=${OPTARG};
-		r) ROUTES=${OPTARG};
-		u) URDF=${OPTARG};
-		e) EXECUTABLES=${OPTARG};
-		t) TARGET=${OPTARG};
-		l) LAUNCH=${OPTARG};
-		i) INSTALL_DIR=${OPTARG};
+		h) HOST=${OPTARG};;
+		b) BUILD=true;;
+		p) PARAMS=true;;
+		r) ROUTES=true;;
+		u) URDF=true;;
+		e) EXECUTABLES=true;;
+		l) LAUNCH=true;;
+		t) TARGET=${OPTARG};;
+		c) CATKIN_WS=${OPTARG};;
 	esac
 done
 
 # Move from engineering_tools to CarmaPlatform
-cd ../../
+cd ..
 LOCAL_CARMA_DIR=${PWD}
 
 if [ ! ${LOCAL_CARMA_DIR:(-13)} == "CarmaPlatform" ]; then
@@ -58,23 +59,23 @@ CARMA_DIR="/opt/carma"
 APP_DIR="${CARMA_DIR}/app"
 
 # If copy executables, params, routes, urdf, or launch is set then don't copy everything
-if [${EXECUTABLES} = true] || [${PARAMS} = true] || [${ROUTES} = true]  || [${URDF} = true] || [${LAUNCH} = true]; then
+if [ ${EXECUTABLES} == true ] || [ ${PARAMS} == true ] || [ ${ROUTES} == true ]  || [ ${URDF} == true ] || [ ${LAUNCH} == true ]; then
 	EVERYTHING=false
 fi
 
 # If we want to copy the executables
-if [${EVERYTHING} = true] || [${EXECUTABLES} = true]; then
+if [ ${EVERYTHING} == true ] || [ ${EXECUTABLES} == true ]; then
 	echo "Trying to copy executables..."
 	# Check if we have a current install to copy
-	if [! -d ${INSTALL_DIR}]; then
+	if [ ! -d ${INSTALL_DIR} ]; then
 		echo "No install folder exists at: ${INSTALL_DIR} . Initiating a rebuild"
 		BUILD=true
 	fi
 
 	# If we wish to rebuild
-	if [${BUILD} = true]; then
+	if [ ${BUILD} == true ]; then
 		# Move to catkin_ws
-		cd "../${INSTALL_DIR}"
+		cd "${CATKIN_WS}"
 		# Try catkin_make install in local machine
 		catkin_make clean
 		exitfn () {
@@ -90,12 +91,16 @@ if [${EVERYTHING} = true] || [${EXECUTABLES} = true]; then
 
 	# Find Version Number
 	GUIDANCE_MAVEN="${INSTALL_DIR}/share/maven/gov/dot/fhwa/saxton/carma/guidance"
-	VERSION="$(ls -d ${GUIDANCE_MAVEN} \*/)"
+	cd "${GUIDANCE_MAVEN}" # Go to Maven directory for guidance
+	VERSION="$(ls -d */)"
+	VERSION=${VERSION::(-1)} # Remove backslash on directory name
 	GUIDANCE_JAR="${GUIDANCE_MAVEN}/${VERSION}/guidance-${VERSION}.jar"
-
+	cd "${LOCAL_CARMA_DIR}" # Return to carma directory
 	# Extract version number file
 	jar xf ${GUIDANCE_JAR} version
 	FULL_VERSION_ID="${VERSION}-$(sed -n 1p version)-$(sed -n 2p version)"
+
+	echo "Version appears to be: ${FULL_VERSION_ID}"
 
 	# Determine target folder
 	TARGET="${APP_DIR}_${FULL_VERSION_ID}"
@@ -111,28 +116,28 @@ if [${EVERYTHING} = true] || [${EXECUTABLES} = true]; then
 fi
 
 # If we want to copy params
-if [${EVERYTHING} = true] || [${PARAMS} = true]; then
+if [ ${EVERYTHING} == true ] || [ ${PARAMS} == true ]; then
 	echo "Trying to copy params"
 	# Copy the entire folder to the remote machine
 	scp -r "${PARAMS_DIR}" ${USERNAME}@${HOST}:"${CARMA_DIR}"
 fi
 
 # If we want to copy routes
-if [${EVERYTHING} = true] || [${ROUTES} = true]; then
+if [ ${EVERYTHING} == true ] || [ ${ROUTES} == true ]; then
 	echo "Trying to copy routes..."
 	# Copy the entire folder to the remote machine
 	scp -r "${ROUTES_DIR}" ${USERNAME}@${HOST}:"${CARMA_DIR}"
 fi
 
 # If we want to copy urdf
-if [${EVERYTHING} = true] || [${URDF} = true]; then
+if [ ${EVERYTHING} == true ] || [ ${URDF} == true ]; then
 	echo "Trying to copy urdf..."
 	# Copy the entire folder to the remote machine
 	scp -r "${URDF_DIR}" ${USERNAME}@${HOST}:"${CARMA_DIR}"
 fi
 
 # If we want to copy launch file
-if [${EVERYTHING} = true] || [${LAUNCH} = true]; then
+if [ ${EVERYTHING} == true ] || [ ${LAUNCH} == true ]; then
 	echo "Trying to copy launch ..."
 	# Copy the launch file to the remote machine using current symlink
 	scp -r "${LAUNCH_FILE}" ${USERNAME}@${HOST}:"${APP_DIR}/launch/"
