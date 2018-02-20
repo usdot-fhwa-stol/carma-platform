a3bc1f8444eb0410fb25215435cdb7434fa876a0
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-02-20 10:49:19 -0500

    publishing shutdown message before killing platform

--- website/scripts/kill.bash
+++ website/scripts/kill.bash
@@ -4,6 +4,18 @@
 launch_pid=$1
 echo $launch_pid
 
+# Assumes ROS Java was installed as package and included in this source
+source /opt/ros/kinetic/setup.bash
+
+# Source platform and set ros environment variables
+source /opt/carma/app/bin/setup.bash
+
+# Publish system alert
+rostopic pub /saxton_cav/system_alert cav_msgs/SystemAlert "type: 6 description: 'Shutdown requested by UI'"
+
+# Wait for 5 seconds for roslaunch shutdown then kill process
+sleep 5
+
 #TODO make this more secure with somekind of validation on the target process
 
 # Kill the roslaunch file
