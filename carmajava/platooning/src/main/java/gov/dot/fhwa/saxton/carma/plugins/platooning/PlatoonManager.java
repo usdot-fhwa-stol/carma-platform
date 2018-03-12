00c7e7a3394fc1272df01fa362517dbfc8ff7670
qswawrq <qswawrq@gmail.com>
2018-03-12 11:28:48 -0700

    APF

--- carmajava/platooning/src/main/java/gov/dot/fhwa/saxton/carma/plugins/platooning/PlatoonManager.java
+++ carmajava/platooning/src/main/java/gov/dot/fhwa/saxton/carma/plugins/platooning/PlatoonManager.java
@@ -17,6 +17,7 @@
 package gov.dot.fhwa.saxton.carma.plugins.platooning;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -131,156 +132,153 @@ public class PlatoonManager implements Runnable {
      * CommandGenerator will use the output of this function as the baseline cmd_speed 
      */
     protected synchronized PlatoonMember getLeader() {
-        PlatoonMember leader = null;
+        PlatoonMember newLeader = null;
         if(platoon.isEmpty()) {
             // This will make sure we only apply leader selection algorithm when the platoon list is not empty
-            return leader;
+            return newLeader;
         } else {
             // return the first vehicle in the platoon as default if no valid algorithm is indicated
-            leader = platoon.get(0);
+            newLeader = platoon.get(0);
         }
         if(plugin.getAlgorithmType() == 1) {
-            PlatoonMember newLeader = allPredecessorFollowing();
-            leader = newLeader;
-            previousLeader = leader == null ? "" : leader.getStaticId();
-        }
-        return leader;
-    }
-    
-    //If this method returns null, it means it assume itself as the leader
-    private PlatoonMember allPredecessorFollowing() {
-        PlatoonMember newLeader = platoon.get(0);
-        IManeuverInputs inputs = this.psl.getManeuverPlanner().getManeuverInputs();
-        // if the distance headway between the subject vehicle and its predecessor is an issue, it should follow its predecessor
-        if(inputs.getDistanceToFrontVehicle() < plugin.getMinGap() ||
-                (inputs.getDistanceToFrontVehicle() < plugin.getMaxGap() && previousLeader.equals(platoon.get(platoon.size() - 1).getStaticId()))) {
-            newLeader = platoon.get(platoon.size() - 1);
-        } else {
-            // calculate the time headway between every consecutive pair of vehicles
-            double[] timeHeadways = new double[platoon.size()];
-            for(int i = 0; i < timeHeadways.length; i++) {
-                if(i < timeHeadways.length - 1) {
-                    timeHeadways[i] = (platoon.get(i).getVehiclePosition() - platoon.get(i + 1).getVehiclePosition()) / platoon.get(i + 1).getVehicleSpeed(); 
-                } else {
-                    timeHeadways[i] = inputs.getDistanceToFrontVehicle() / inputs.getCurrentSpeed();
-                }
-            }
-            int closestLowerBoundaryViolation = -1, closestMaximumSpacingViolation = -1;
-            if(previousLeader.equals(platoon.get(0).getStaticId())) {
-                for(int i = timeHeadways.length - 1; i >= 0; i++) {
-                    if(timeHeadways[i] < plugin.getLowerBoundary()) {
-                        closestLowerBoundaryViolation = i;
-                        break;
-                    }
-                }
-                for(int i = timeHeadways.length - 1; i >= 0; i++) {
-                    if(timeHeadways[i] > plugin.getMaxSpacing()) {
-                        closestMaximumSpacingViolation = i;
-                        break;
-                    }
-                }
-                if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation == -1) {
-                    return newLeader;
-                } else if(closestLowerBoundaryViolation != -1 && closestMaximumSpacingViolation == -1) {
-                    newLeader = platoon.get(closestLowerBoundaryViolation);
-                } else if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation != -1) {
-                    int leaderIndex = closestMaximumSpacingViolation + 1;
-                    newLeader = leaderIndex > platoon.size() - 1 ? null : platoon.get(leaderIndex);
-                } else {
-                    if(closestLowerBoundaryViolation > closestMaximumSpacingViolation) {
-                        newLeader = platoon.get(closestLowerBoundaryViolation);
-                    } else if(closestLowerBoundaryViolation < closestMaximumSpacingViolation) {
-                        int leaderIndex = closestMaximumSpacingViolation + 1;
-                        newLeader = leaderIndex > platoon.size() - 1 ? null : platoon.get(leaderIndex);
-                    } else {
-                        log.error("APF Leader selection cannot handle this case 1");
-                    }
-                }
-            } else if(!previousLeader.equals("")) {
-                int indexOfPreviousLeader = -1;
-                for(int i = 0; i < platoon.size(); i++) {
-                    if(platoon.get(i).getStaticId().equals(previousLeader)) {
-                        indexOfPreviousLeader = i;
-                        break;
-                    }
-                }
-                if(indexOfPreviousLeader == -1) {
-                    log.error("APF Leader selection cannot handle this case 2");
-                    return newLeader;
-                }
-                double[] temporary_time_headways = new double[platoon.size() - indexOfPreviousLeader];
-                for(int i = 0; i < temporary_time_headways.length; i++) {
-                    temporary_time_headways[i] = timeHeadways[indexOfPreviousLeader + i];
-                }
-                for(int i = temporary_time_headways.length - 1; i >= 0; i++) {
-                    if(temporary_time_headways[i] < plugin.getLowerBoundary()) {
-                        closestLowerBoundaryViolation = i;
-                        break;
-                    }
-                }
-                for(int i = temporary_time_headways.length - 1; i >= 0; i++) {
-                    if(temporary_time_headways[i] > plugin.getMaxSpacing()) {
-                        closestMaximumSpacingViolation = i;
-                        break;
-                    }
-                }
-                if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation == -1) {
-                    if(timeHeadways[indexOfPreviousLeader] < plugin.getUpperBoundary() ||
-                            timeHeadways[indexOfPreviousLeader - 1] > plugin.getMinSpacing()) {
-                        newLeader = platoon.get(indexOfPreviousLeader);
-                    } else if(timeHeadways[indexOfPreviousLeader] > plugin.getUpperBoundary() &&
-                            timeHeadways[indexOfPreviousLeader - 1] < plugin.getMinSpacing()) {
-                        closestLowerBoundaryViolation = -1;
-                        closestMaximumSpacingViolation = -1;
-                        for(int i = timeHeadways.length - 1; i >= 0; i++) {
-                            if(timeHeadways[i] < plugin.getLowerBoundary()) {
-                                closestLowerBoundaryViolation = i;
-                                break;
-                            }
-                        }
-                        for(int i = timeHeadways.length - 1; i >= 0; i++) {
-                            if(timeHeadways[i] > plugin.getMaxSpacing()) {
-                                closestMaximumSpacingViolation = i;
-                                break;
-                            }
-                        }
-                        if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation == -1) {
-                            newLeader = platoon.get(0);
-                        } else if(closestLowerBoundaryViolation != -1 && closestMaximumSpacingViolation == -1) {
-                            newLeader = platoon.get(closestLowerBoundaryViolation);
-                        } else if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation != -1) {
-                            int leaderIndex = closestMaximumSpacingViolation + 1; 
-                            newLeader = leaderIndex > platoon.size() - 1 ? null : platoon.get(leaderIndex);
-                        } else {
-                            if(closestLowerBoundaryViolation > closestMaximumSpacingViolation) {
-                                newLeader = platoon.get(closestLowerBoundaryViolation);
-                            } else if(closestLowerBoundaryViolation < closestMaximumSpacingViolation) {
-                                int leaderIndex = closestMaximumSpacingViolation + 1;
-                                newLeader = leaderIndex > platoon.size() - 1 ? null : platoon.get(leaderIndex);
-                            } else {
-                                log.error("APF Leader selection cannot handle this case 3");
-                            }
-                        }
-                    }
-                    // Make sure thoese else if are correct.......
-                    else if(closestLowerBoundaryViolation != -1 && closestMaximumSpacingViolation == -1) {
-                        newLeader = platoon.get(indexOfPreviousLeader - 1 + closestLowerBoundaryViolation);
-                    } else if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation != -1) {
-                        newLeader = platoon.get(indexOfPreviousLeader + closestMaximumSpacingViolation);
-                    } else {
-                        if(closestLowerBoundaryViolation > closestMaximumSpacingViolation) {
-                            newLeader = platoon.get(indexOfPreviousLeader - 1 + closestLowerBoundaryViolation);
-                        } else if(closestLowerBoundaryViolation < closestMaximumSpacingViolation) {
-                            newLeader = platoon.get(indexOfPreviousLeader + closestMaximumSpacingViolation);
-                        } else {
-                            log.error("APF Leader selection cannot handle this case 4");
-                        }
-                    }
-                }
-            }
+            int newLeaderIndex = allPredecessorFollowing();
+            newLeader = newLeaderIndex >= platoon.size() ? null : platoon.get(newLeaderIndex);
+            previousLeader = newLeader == null ? "" : newLeader.getStaticId();
         }
         return newLeader;
-        
+    }
+    
+    // If this method returns the size of platoon, it means the subject vehicle should follow his own commands
+    // Otherwise, it will return the index of the leader in the platoon list
+    private int allPredecessorFollowing() {
+        int result = 0;
+        // If we do not have any leader in the previous time step
+        if(previousLeader.equals("")) {
+            return result;
+        }
+        IManeuverInputs inputs = this.psl.getManeuverPlanner().getManeuverInputs();
+        // Generate an array of downtrack distance for every vehicles in this platoon including the host vehicle
+        double[] downtrackDistance = new double[platoon.size() + 1];
+        for(int i = 0; i < platoon.size(); i++) {
+            downtrackDistance[i] = platoon.get(i).getVehiclePosition(); 
+        }
+        downtrackDistance[downtrackDistance.length - 1] = inputs.getDistanceFromRouteStart();
+        // Generate an array of speed for every vehicles in this platoon including the host vehicle
+        double[] speed = new double[platoon.size() + 1];
+        for(int i = 0; i < platoon.size(); i++) {
+            speed[i] = platoon.get(i).getVehicleSpeed();
+        }
+        speed[speed.length - 1] = inputs.getCurrentSpeed();
+        // if the distance headway between the subject vehicle and its predecessor is an issue, it should follow its predecessor
+        if(hasEnoughGapWithPredecessor(inputs.getDistanceToFrontVehicle())) {
+            result = platoon.size() - 1;
+        } else {
+            // implementation of the regular APF algorithm
+            double[] timeHeadways = calculateTimeHeadway(downtrackDistance, speed);
+            int closestLowerBoundaryViolation, closestMaximumSpacingViolation;
+            // if the previous leader is the first vehicle in the platoon
+            if(previousLeader.equals(platoon.get(0).getStaticId())) {
+                result = determineLeaderBasedOnViolation(timeHeadways);
+            } else {
+                // if the previous leader is not the first one
+                int indexOfPreviousLeader = findPreviousLeader();
+                double[] temporaryTimeHeadways = calculateTimeHeadwayFromIndex(timeHeadways, indexOfPreviousLeader);
+                closestLowerBoundaryViolation = findLowerBoundaryViolationClosestToTheHostVehicle(temporaryTimeHeadways);
+                closestMaximumSpacingViolation = findMaximumSpacingViolationClosestToTheHostVehicle(temporaryTimeHeadways);
+                // if there is no vialations in time headways
+                if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation == -1) {
+                    // Two conditions for assigning leadership further downstream
+                    boolean condition1 = timeHeadways[indexOfPreviousLeader] > plugin.getUpperBoundary();
+                    boolean condition2 = timeHeadways[indexOfPreviousLeader - 1] < plugin.getMinSpacing(); 
+                    if(condition1 && condition2) {
+                        result = determineLeaderBasedOnViolation(timeHeadways);
+                    } else {
+                        result = indexOfPreviousLeader;
+                    }
+                } else if(closestLowerBoundaryViolation != -1 && closestMaximumSpacingViolation == -1) {
+                    result = indexOfPreviousLeader - 1 + closestLowerBoundaryViolation;
+                } else if(closestLowerBoundaryViolation == -1 && closestMaximumSpacingViolation != -1) {
+                    result = indexOfPreviousLeader + closestMaximumSpacingViolation;
+                } else {
+                    if(closestLowerBoundaryViolation > closestMaximumSpacingViolation) {
+                        result = indexOfPreviousLeader - 1 + closestLowerBoundaryViolation;
+                    } else if(closestLowerBoundaryViolation < closestMaximumSpacingViolation) {
+                        result = indexOfPreviousLeader + closestMaximumSpacingViolation;
+                    } else {
+                        log.error("APF Leader selection cannot handle this case.");
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    
+    // Check if we have enough gap with the front vehicle
+    private boolean hasEnoughGapWithPredecessor(double distanceToFrontVehicle) {
+        boolean frontGapIsTooSmall = distanceToFrontVehicle < plugin.getMinGap();
+        boolean previousLeaderIsPredecessor = previousLeader.equals(platoon.get(platoon.size() - 1).getStaticId());
+        boolean frontGapIsNotLargeEnough = distanceToFrontVehicle < plugin.getMaxGap() && previousLeaderIsPredecessor;
+        return frontGapIsTooSmall || frontGapIsNotLargeEnough;
+    }
+    
+    // calculate the time headway between every consecutive pair of vehicles
+    private double[] calculateTimeHeadway(double[] downtrackDistance, double[] speed) {
+        double[] timeHeadways = new double[downtrackDistance.length - 1];
+        for(int i = 0; i < timeHeadways.length; i++) {
+            if(speed[i + 1] != 0) {
+                timeHeadways[i] = (downtrackDistance[i] - downtrackDistance[i + 1]) / speed[i + 1];
+            } else {
+                timeHeadways[i] = Double.POSITIVE_INFINITY;
+            }
+        }
+        return timeHeadways;
+    }
+    
+    // calculate the time headway between every consecutive pair of vehicles from start index
+    private double[] calculateTimeHeadwayFromIndex(double[] timeHeadways, int start) {
+        return Arrays.stream(timeHeadways).skip(start).toArray();
+    }
+    
+    private int findLowerBoundaryViolationClosestToTheHostVehicle(double[] timeHeadways) {
+        for(int i = timeHeadways.length - 1; i >= 0; i--) {
+            if(timeHeadways[i] < plugin.getLowerBoundary()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    private int findMaximumSpacingViolationClosestToTheHostVehicle(double[] timeHeadways) {
+        for(int i = timeHeadways.length - 1; i >= 0; i--) {
+            if(timeHeadways[i] > plugin.getMaxSpacing()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    private int findPreviousLeader() {
+        if(!previousLeader.equals("")) {
+            for(int i = 0; i < platoon.size(); i++) {
+                if(previousLeader.equals(platoon.get(i).getStaticId())) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    private int determineLeaderBasedOnViolation(double[] timeHeadways) {
+        int closestLowerBoundaryViolation = findLowerBoundaryViolationClosestToTheHostVehicle(timeHeadways);
+        int closestMaximumSpacingViolation = findMaximumSpacingViolationClosestToTheHostVehicle(timeHeadways);
+        if(closestLowerBoundaryViolation > closestMaximumSpacingViolation) {
+            return closestLowerBoundaryViolation;
+        } else if(closestLowerBoundaryViolation < closestMaximumSpacingViolation) {
+            return closestMaximumSpacingViolation + 1;
+        } else {
+            log.error("APF algorithm cannot handle this case.");
+            return 0;  
+        }
     }
     
     protected int getPlatooningSize() {
