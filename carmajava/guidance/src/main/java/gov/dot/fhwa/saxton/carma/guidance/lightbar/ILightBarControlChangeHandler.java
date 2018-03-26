7bd7cde0426c8b3c1788ce7fa78da9b27f6ad5a3
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-03-26 11:15:00 -0400

    adding missed file

---
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/lightbar/ILightBarControlChangeHandler.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package gov.dot.fhwa.saxton.carma.guidance.lightbar;
+
+import java.util.List;
+
+/**
+ * A ConflictDetector is responsible for providing rapid conflict detection to other guidance components
+ * An internal set of vehicle paths is maintained which a suggested path can be queried against to identify conflicts
+ */
+public interface ILightBarControlChangeHandler {
+  void controlLost(LightBarIndicator lostIndicator);
+}
