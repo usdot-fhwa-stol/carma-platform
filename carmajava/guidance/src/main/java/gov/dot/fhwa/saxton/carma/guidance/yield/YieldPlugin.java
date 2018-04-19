d22f8c1157dc362187978456c9866162dfacb17d
Kyle Rush <kyle.rush@leidos.com>
2018-04-19 09:59:15 -0400

    Added more detail to log statements

--- carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/yield/YieldPlugin.java
+++ carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/yield/YieldPlugin.java
@@ -242,10 +242,13 @@ public class YieldPlugin extends AbstractPlugin
             double epsilon = minConflictAvoidanceTimegap;
             requiredAcceleration = 2 * (d - (v * (t + epsilon))) / Math.pow(t + epsilon, 2);
 
-            log.debug(String.format("d=%.02f, v=%.02f, t=%.02f, epsilon=%.02f, a=%.02f", d, v , t, epsilon, requiredAcceleration));
+            log.debug(String.format("Evaluating candidate solution: d=%.02f, v=%.02f, t=%.02f, epsilon=%.02f, a=%.02f", d, v , t, epsilon, requiredAcceleration));
 
             if (Math.abs(requiredAcceleration) <= maxYieldAccelAuthority) {
+                log.debug("Candidate solution acceptable.");
                 solved = true;
+            } else {
+                log.debug("Candidate solution unacceptable, iterating...");
             }
         }
 
@@ -267,15 +270,18 @@ public class YieldPlugin extends AbstractPlugin
             double t = timeAvailableForConflictAvoidance;
             double epsilon = minConflictAvoidanceTimegap;
             requiredAcceleration = 2 * (d - (v * (t + epsilon))) / Math.pow(t + epsilon, 2);
-            log.debug(String.format("d=%.02f, v=%.02f, t=%.02f, epsilon=%.02f, a=%.02f", d, v , t, epsilon, requiredAcceleration));
+            log.debug(String.format("Evaluating last effort solution from start of trajectory: d=%.02f, v=%.02f, t=%.02f, epsilon=%.02f, a=%.02f", d, v , t, epsilon, requiredAcceleration));
         }
 
         if (Math.abs(requiredAcceleration) > maxYieldAccelAuthority) {
+            log.error("Overall solution unacceptable, throwing error!");
             // Nothing we can do, throw control to driver
             throw new RosRuntimeException(String.format(
                     "Yield plugin unable to solve conflict at [%.02f, %.02f]m within acceleration constraints maxYieldAccelAuthority=%.02m/s/s, requiredAccel=%.02fm/s/s",
                     conflict.getStartDowntrack(), conflict.getEndDowntrack(), maxYieldAccelAuthority,
                     requiredAcceleration));
+        } else {
+            log.info("Overall solution within acceleration constraints!");
         }
 
         // We solved it, implement solution
