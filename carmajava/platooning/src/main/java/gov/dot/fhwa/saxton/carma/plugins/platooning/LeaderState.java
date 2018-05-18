6283e0e5f028f2bb9c284d8df3f263a408c62834
qswawrq <qswawrq@gmail.com>
2018-05-18 11:03:28 -0700

    use real location to prevent replan

--- carmajava/platooning/src/main/java/gov/dot/fhwa/saxton/carma/plugins/platooning/LeaderState.java
+++ carmajava/platooning/src/main/java/gov/dot/fhwa/saxton/carma/plugins/platooning/LeaderState.java
@@ -1,5 +1,6 @@
 package gov.dot.fhwa.saxton.carma.plugins.platooning;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import cav_msgs.MobilityHeader;
@@ -14,6 +15,7 @@ import gov.dot.fhwa.saxton.carma.guidance.plugins.PluginServiceLocator;
 import gov.dot.fhwa.saxton.carma.guidance.trajectory.Trajectory;
 import gov.dot.fhwa.saxton.carma.guidance.util.ILogger;
 import gov.dot.fhwa.saxton.carma.guidance.util.RouteService;
+import gov.dot.fhwa.saxton.carma.guidance.util.trajectoryconverter.RoutePointStamped;
 
 /**
  * The PlatoonLeaderState is a state which platooning algorithm is enabled on the current trajectory
@@ -134,17 +136,17 @@ public class LeaderState implements IPlatooningState {
                     log.debug("Found a platoon with id = " + platoonId + " in front of us.");
                     MobilityRequest request = plugin.mobilityRequestPublisher.newMessage();
                     String planId = UUID.randomUUID().toString();
+                    long currentTime = System.currentTimeMillis();
                     request.getHeader().setPlanId(planId);
                     request.getHeader().setRecipientId(senderId);
                     // TODO Need to have a easy way to get bsmId from plugin
                     request.getHeader().setSenderBsmId(pluginServiceLocator.getTrackingService().getCurrentBSMId());
                     request.getHeader().setSenderId(pluginServiceLocator.getMobilityRouter().getHostMobilityId());
-                    request.getHeader().setTimestamp(System.currentTimeMillis());
-                    // TODO Need to have a easy way to get current XYZ location in ECEF
-                    request.getLocation().setEcefX(0);
-                    request.getLocation().setEcefY(0);
-                    request.getLocation().setEcefZ(0);
-                    request.getLocation().setTimestamp(System.currentTimeMillis());
+                    request.getHeader().setTimestamp(currentTime);
+                    RoutePointStamped currentLocation = new RoutePointStamped(plugin.getManeuverInputs().getDistanceFromRouteStart(),
+                    plugin.getManeuverInputs().getCrosstrackDistance(), currentTime / 1000);
+                    cav_msgs.Trajectory currentLocationMsg = pluginServiceLocator.getTrajectoryConverter().pathToMessage(Arrays.asList(currentLocation));
+                    request.setLocation(currentLocationMsg.getLocation());
                     request.getPlanType().setType(PlanType.JOIN_PLATOON_AT_REAR);
                     request.setStrategy(PlatooningPlugin.MOBILITY_STRATEGY);
                     String strategyParamsString = String.format(PlatooningPlugin.JOIN_AT_REAR_PARAMS,
