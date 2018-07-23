007569b2771676f329288ca69c75819a73f77850
aphelyon <mhc6kp@virginia.edu>
2018-07-23 13:03:40 -0400

    assume ip is set

--- website/scripts/launch.bash
+++ website/scripts/launch.bash
@@ -10,15 +10,6 @@ source /opt/ros/kinetic/setup.bash
 # Source platform and set ros environment variables
 source /opt/carma/app/bin/setup.bash
 
-# Ensure ROS_HOSTNAME is not set as we will be using ROS_IP
-unset ROS_HOSTNAME
-
-# Ensure ROS_IP is correct
-export ROS_IP=192.168.0.4
-
-# Ensure ROS_MASTER_URI is correct
-export ROS_MASTER_URI=http://192.168.88.10:11311/
-
 # Set ros to run from /opt/carma instead of home directory
 export ROS_HOME=/opt/carma/.ros
 
