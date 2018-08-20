de35522ffb44fad0dd08c5ef18cd860e4be38afc
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-08-20 11:19:37 -0400

    Add comments and more exception handling

--- carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ead/MovesFuelCostModel.java
+++ carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ead/MovesFuelCostModel.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import gov.dot.fhwa.saxton.carma.signal_plugin.ead.trajectorytree.ICostModel;
 import gov.dot.fhwa.saxton.carma.signal_plugin.ead.trajectorytree.Node;
@@ -18,10 +18,11 @@ import gov.dot.fhwa.saxton.carma.signal_plugin.logger.LoggerManager;
  * Cost model for use in the trajectory tree solution. Computes a cost between neighboring nodes
  * based on an estimation of fuel cost to travel between those nodes.
  *
- * This version uses equation (3) of the UCR white paper, Glidepath II Algorithm, dated Aug 2017.
+ * This version uses the cost model presented in the UCR white paper, Glidepath II MOVES Brief, dated Dec 2017.
+ * Some assumptions are made in this implementation.
  * It assumes a flat roadway (no grade) and uniform acceleration between the given nodes.
- * It uses simple average speed between the beginning and ending nodes rather than integrating.
- * It also assumes some fixed, positive cost at zero speed to represent fuel consumption at idle.
+ * It also assumes acceleration <= -2.0 corresponds to deceleration
+ * It also assumes that unknown operating modes have a cost equal to the highest cost present in the provided tables
  */
 public class MovesFuelCostModel implements ICostModel {
 
@@ -38,7 +39,10 @@ public class MovesFuelCostModel implements ICostModel {
     private final Map<Integer,List<Double>> baseRateTable;
     private final int BASE_RATE_ENERGY_COL = 4;
     private final double ROAD_GRADE = 0.0; // By default we assume the road is flat. 0.0 in rad
-    private final int EXPECTED_BASE_RATE_TABLE_LENGTH = 7;
+    private final int EXPECTED_BASE_RATE_TABLE_LENGTH = 7; // Number of columns in the energy consumption table
+    private final double SEC_PER_HR = 3600.0;
+    private final double J_PER_KJ = 1000.0;
+    private final double DEFAULT_PEAK_ENERGY_KJ; // The highest cost found in the energy consumption table
 
     private int numCosts = 0;
     protected static ILogger log = LoggerManager.getLogger(FuelCostModel.class);
@@ -46,28 +50,46 @@ public class MovesFuelCostModel implements ICostModel {
 
     /**
      * Builds the cost model object with several injected calculation parameters.
-     * TODO
-     * @param mass - vehicle mass, kg
-     * @param rollRes - rolling resistance coefficient override; if this is 0 then internal calc will be used
-     * @param dragCoef - vehicle aerodynamic drag coefficient
-     * @param area - vehicle frontal cross-sectional area, m^2
-     * @param airDensity - air density, kg/m^3
-     * @param idleCost - rate of energy consumption at zero speed due to engine idling, J/s
-     * @param useIdleMin - if true the the idleCost will be the lower limit returned for all situations;
-     *                   if false then idleCost will only be applied if speed is zero
+     * These parameters should come from the EPA "MOVES2010 Highway Vehicle Population and Activity Data",
+     * in accordance with the MOVES Brief document provided by UCR.
+     * @param rollingTermA - Term corresponding to vehicle roll. Units: kW - s/m
+     * @param rotatingTermB - Term corresponding to vehicle rotation. Units: kW - s^2/m^2
+     * @param dragTermC - Term corresponding to vehicle drag. Units: kW - s^3/m^3
+     * @param vehicleMassInTons - Source vehicle mass. Units: metric tons
+     * @param fixedMassFactor - Fixed mass factor. Units: metric tons
+     * @param baseRateTablePath - Path to the csv file used to store the energy consumption parameters based on vehicle state
+     * @throws IOException - Exception thrown when the file specified by baseRateTablePath cannot be loaded properly
      */
     public MovesFuelCostModel(double rollingTermA, double rotatingTermB, double dragTermC, double vehicleMassInTons,
-        double fixedMassFactor, String baseRateTablePath) {
-        //assign injected params for mass, rolling resistance, drag coefficient, frontal area, air density, idle cost
+        double fixedMassFactor, String baseRateTablePath) throws IOException {
+        //assign injected params
         this.rollingTermA = rollingTermA;
         this.rotatingTermB = rotatingTermB;
         this.dragTermC = dragTermC;
         this.vehicleMassInTons = vehicleMassInTons;
         this.fixedMassFactor = fixedMassFactor;
         this.baseRateTable = loadTable(baseRateTablePath); // Load the base rates table
+
+        // Find the highest energy cost in the table and store it for use when values fall outside table scope
+        double maxValue = 0; 
+        for(Entry<Integer, List<Double>> entry: this.baseRateTable.entrySet()) {
+            if (entry.getValue().get(BASE_RATE_ENERGY_COL) > maxValue) {
+                maxValue = entry.getValue().get(BASE_RATE_ENERGY_COL);
+            }
+        }
+
+        this.DEFAULT_PEAK_ENERGY_KJ = maxValue;
     }
 
-    private Map<Integer,List<Double>> loadTable(String baseRateTablePath) {
+    /**
+     * Helper function to load a csv file containing the host vehicle energy consumption parameters based on vehicle state
+     * 
+     * @param baseRateTablePath - Path to the csv file used to store the energy consumption parameters based on vehicle state
+     * @throws IOException - Exception thrown when the file specified by baseRateTablePath cannot be loaded properly
+     * 
+     * @return A mapping of operating mode id with energy consumption parameters 
+     */
+    private Map<Integer,List<Double>> loadTable(String baseRateTablePath) throws IOException {
         String line = "";
         String delimiter = ",";
         boolean firstLine = true;
@@ -84,7 +106,7 @@ public class MovesFuelCostModel implements ICostModel {
                 String[] data = line.split(delimiter);
 
                 if (data.length != EXPECTED_BASE_RATE_TABLE_LENGTH) {
-                    // TODO throw exception
+                    throw new IOException("MOVES fuel cost model data file contained " + data.length + " columns but expected " + EXPECTED_BASE_RATE_TABLE_LENGTH);
                 }
 
                 Integer key = Integer.parseInt(data[0]);
@@ -99,8 +121,7 @@ public class MovesFuelCostModel implements ICostModel {
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
-            // TODO handle load failure
+            throw e;
         }
         return map;
     }
@@ -108,9 +129,13 @@ public class MovesFuelCostModel implements ICostModel {
 
     /**
      * Expresses the cost of moving from n1 to n2 in terms of energy consumed.
+     * Output cost is in units of KJ
      * Assumes n2.distance >= n1.distance and n2.time > n1.time
+     * Calculation based on UCR MOVES Brief
+     * 
      * @param n1 First node
      * @param n2 Second node
+     * 
      * @return energy cost, J, or very large number if inputs are incorrect
      */
     @Override
@@ -138,7 +163,8 @@ public class MovesFuelCostModel implements ICostModel {
         final double g = 9.8; // Acceleration due to gravity = 9.8 m/s^2
         final double v = n2.getSpeedAsDouble(); // We are using the target node's velocity
         final double v_sqr = v*v;
-        final double a = (v - n1.getSpeedAsDouble()) / (n2.getTimeAsDouble() - n1.getTimeAsDouble()); // a = dV / t : We are using the acceleration to get from current speed to target speed
+        final double dt = n2.getTimeAsDouble() - n1.getTimeAsDouble(); // Change in time in seconds
+        final double a = (v - n1.getSpeedAsDouble()) / dt; // a = dV / dt : We are using the acceleration to get from current speed to target speed
         final double theta = ROAD_GRADE; 
 
         // Calculate the VehicleSpecificPower (VSP)
@@ -147,12 +173,26 @@ public class MovesFuelCostModel implements ICostModel {
         // Determine Operating Mode from OpModeTable
         final int opMode = getModeConditional(VSP, v, a);
 
+        // Return the highest cost which would still be in the table when our result is in the undefined region
+        // Additionally log the occurrence
+        if (opMode == -1) {
+            log.warn("EAD", "Invalid operating mode found for MOVES cost calculation. Using highest known cost in table: " + DEFAULT_PEAK_ENERGY_KJ + " KJ");
+            log.warn("EAD", "    Node 1: " + n1.toString());
+            log.warn("EAD", "    Node 2: " + n2.toString());
+            return J_PER_KJ * ((DEFAULT_PEAK_ENERGY_KJ / SEC_PER_HR) * dt);
+        }
+
         // Get Parameters
         final List<Double> baseRateList = baseRateTable.get(opMode);
 
-        return baseRateList.get(BASE_RATE_ENERGY_COL); // Return the energy in KJ/hr
+        return J_PER_KJ * ((baseRateList.get(BASE_RATE_ENERGY_COL) / SEC_PER_HR) * dt); // Return the energy in J by converting KJ/hr to KJ/s and multiplying by dt and J/KJ
     }
 
+    /**
+     * Helper function for identifying the operating mode of the host vehicle based on vehicle specific power (VSP), velocity, and acceleration
+     * 
+     * This is a reconstruction of the conditional table lookup required by the UCR MOVES brief
+     */
     private int getModeConditional(double VSP, double velocity, double acceleration) {
         // In the MOVES documentation this table is presented in mi/hr here it is converted to m/s to avoid conversions
         final double FIFTY_MPH_IN_MPS = 22.352;
@@ -160,7 +200,7 @@ public class MovesFuelCostModel implements ICostModel {
         final double ONE_MPH_IN_MPS = 0.44704;
         // NOTE: This acceleration check is a simplification of a <= -2 OR (a < -1.0 AND a_t-1 < -1.0 AND a_t-2 < -1.0)
         //       The simplification was made to avoid the need to get previous acceleration values into this function
-        if (acceleration <= -2.0) {
+        if (acceleration <= -1.0) {
             return 0;
         }
         if (velocity >= FIFTY_MPH_IN_MPS) {
