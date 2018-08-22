42eb9bfe2deea3fcc78e1f1bd03af40bd6f85847
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-08-22 17:30:53 -0400

    Update interface for distance step instead of timestep

--- carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ead/IMotionInterpolator.java
+++ carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ead/IMotionInterpolator.java
@@ -24,17 +24,19 @@ import gov.dot.fhwa.saxton.carma.signal_plugin.ead.trajectorytree.Node;
 /**
  * A MotionInterpolator is responsible for interpolating the position of the host vehicle between two or more nodes in a plan
  * The returned list of RoutePointStamped objects can be used to check conflicts with the CARMA conflict detection system
+ * 
  */
 public interface IMotionInterpolator {
 
   /**
    * Interpolates the vehicle position between each pair of nodes in a vehicle's path.
    * The returned list is in the RoutePointStamped format for use with conflict detection systems provided by CARMA
+   * The returned list will always include all points in the provided trajectory
    * 
    * @param path A time sorted list of nodes which define the host vehicle's desired trajectory
-   * @param timeStep The time gap between each point in the output list
+   * @param distanceStep The max distance gap between each point in the output list
    * 
    * @return A list of route point stamped
    */
-  public List<RoutePointStamped> interpolateMotion(List<Node> trajectory, double timeStep);
+  public List<RoutePointStamped> interpolateMotion(List<Node> trajectory, double distanceStep);
 }
