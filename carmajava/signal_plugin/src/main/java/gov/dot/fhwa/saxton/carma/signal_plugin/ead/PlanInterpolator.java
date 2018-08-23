bc1497f9c41714fba276f972e39ed6b825fc6c80
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-08-23 13:41:26 -0400

    remove old comment

--- carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ead/PlanInterpolator.java
+++ carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ead/PlanInterpolator.java
@@ -63,10 +63,6 @@ public class PlanInterpolator implements IMotionInterpolator {
 
       final double a = dv / dt; // Assume constant acceleration
       final double two_a_x = 2 * a * distanceStep;
-      
-      // double v = v_0;
-      // double x = x_0;
-      // double t = t_0;
 
       double v_old = v_0;
       double v = Math.sqrt(v_old * v_old + two_a_x);
