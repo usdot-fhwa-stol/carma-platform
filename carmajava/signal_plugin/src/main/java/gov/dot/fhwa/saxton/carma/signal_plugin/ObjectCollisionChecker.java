4fd6018ad7c32e8ee1ae2e7cb9cb795d1fbc500b
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-08-27 15:38:34 -0400

    Added object expiration and replanning calls

--- carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ObjectCollisionChecker.java
+++ carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/ObjectCollisionChecker.java
@@ -1,6 +1,5 @@
-package gov.dot.fhwa.saxton.carma.signalplugin;
+package gov.dot.fhwa.saxton.carma.signal_plugin;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -8,43 +7,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicReference;
 
-import org.apache.commons.logging.Log;
-import org.ros.rosjava_geometry.Transform;
+import org.ros.message.Time;
 
-import cav_msgs.MapData;
 import cav_msgs.RoadwayObstacle;
-import cav_msgs.SPAT;
-import cav_srvs.GetTransformRequest;
-import geometry_msgs.Twist;
-import geometry_msgs.TwistStamped;
-import gov.dot.fhwa.saxton.carma.geometry.cartesian.Point3D;
-import gov.dot.fhwa.saxton.carma.guidance.arbitrator.TrajectoryPlanningResponse;
+import gov.dot.fhwa.saxton.carma.guidance.ArbitratorService;
 import gov.dot.fhwa.saxton.carma.guidance.conflictdetector.IConflictDetector;
-import gov.dot.fhwa.saxton.carma.guidance.plugins.AbstractPlugin;
-import gov.dot.fhwa.saxton.carma.guidance.plugins.IStrategicPlugin;
 import gov.dot.fhwa.saxton.carma.guidance.plugins.PluginServiceLocator;
-import gov.dot.fhwa.saxton.carma.guidance.pubsub.ISubscriber;
-import gov.dot.fhwa.saxton.carma.guidance.trajectory.Trajectory;
 import gov.dot.fhwa.saxton.carma.guidance.util.RouteService;
 import gov.dot.fhwa.saxton.carma.guidance.util.trajectoryconverter.RoutePointStamped;
 import gov.dot.fhwa.saxton.carma.rosutils.SaxtonLogger;
-import gov.dot.fhwa.saxton.carma.signal_plugin.appcommon.DataElementHolder;
-import gov.dot.fhwa.saxton.carma.signal_plugin.appcommon.DataElementKey;
-import gov.dot.fhwa.saxton.carma.signal_plugin.appcommon.IntersectionCollectionDataElement;
-import gov.dot.fhwa.saxton.carma.signal_plugin.appcommon.IAsdListDataElement;
-import gov.dot.fhwa.saxton.carma.signal_plugin.asd.IAsdMessage;
-import gov.dot.fhwa.saxton.carma.signal_plugin.asd.IntersectionCollection;
-import gov.dot.fhwa.saxton.carma.signal_plugin.asd.IntersectionData;
-import gov.dot.fhwa.saxton.carma.signal_plugin.asd.map.MapMessage;
-import gov.dot.fhwa.saxton.carma.signal_plugin.asd.spat.SpatMessage;
-import gov.dot.fhwa.saxton.carma.signal_plugin.ead.EadAStar;
-import gov.dot.fhwa.saxton.carma.signal_plugin.ead.IEad;
 import gov.dot.fhwa.saxton.carma.signal_plugin.ead.IMotionInterpolator;
 import gov.dot.fhwa.saxton.carma.signal_plugin.ead.IMotionPredictor;
 import gov.dot.fhwa.saxton.carma.signal_plugin.ead.INodeCollisionChecker;
 import gov.dot.fhwa.saxton.carma.signal_plugin.ead.trajectorytree.Node;
-import sensor_msgs.NavSatFix;;
 
 /**
  *
@@ -57,31 +34,50 @@ public class ObjectCollisionChecker implements INodeCollisionChecker {
   Map<Integer, PriorityQueue<RoadwayObstacle>> trackedLaneObjectsHistory = new HashMap<>();  
   Map<Integer, List<RoutePointStamped>> trackedLaneObjectsPredictions = new HashMap<>();
 
+  AtomicReference<List<RoutePointStamped>> interpolatedHostPlan;
+
   private SaxtonLogger log;
   private RouteService routeService;
+  private ArbitratorService arbitratorService;
 
-  private final int objectHistoricalSampleCount;
-  private final double distanceStep;
-  private final double timeDuration;
-  private final double timeRange;
-  private final double distanceBuffer;
-  private final double crosstrackBuffer;
+  private final IMotionPredictor motionPredictor;
+  private final IMotionInterpolator motionInterpolator;
+  private final long maxHistoricalDataAge; // ms
+  private final double distanceStep; // m
+  private final double timeDuration; // s
+  private final double timeRange; // s
+  private final double distanceBuffer; // m
+  private final double crosstrackBuffer; // m
+  private final long NCVReplanPeriod; // ms
+  private final double MS_PER_S = 1000.0;
+  private Long ncvDetectionTime = null;
 
-  public ObjectCollisionChecker(PluginServiceLocator psl, SaxtonLogger log) {
-    routeService = psl.getRouteService();
-    psl.getParameterSource().getString("ead.NCVHandling.objectMotionPredictorModel");
-    psl.getParameterSource().getInteger("ead.NCVHandling.objectHistoricalSampleCount");
-    psl.getParameterSource().getDouble("ead.NCVHandling.collision.distanceStep");
-    psl.getParameterSource().getDouble("ead.NCVHandling.collision.timeDuration");
-    psl.getParameterSource().getDouble("ead.NCVHandling.collision.timeRange");
-    psl.getParameterSource().getDouble("ead.NCVHandling.collision.distanceBuffer");
-    psl.getParameterSource().getDouble("ead.NCVHandling.collision.crosstrackBuffer");
+  public ObjectCollisionChecker(PluginServiceLocator psl, SaxtonLogger log,
+    IMotionPredictorModelFactory modelFactory, IMotionInterpolator motionInterpolator) {
+    this.routeService = psl.getRouteService();
+    this.arbitratorService = psl.getArbitratorService();
+
+    String predictionModel = psl.getParameterSource().getString("ead.NCVHandling.objectMotionPredictorModel");
+    this.motionPredictor = modelFactory.getMotionPredictor(predictionModel);
+
+    this.maxHistoricalDataAge = psl.getParameterSource().getInteger("ead.NCVHandling.collision.maxObjectHistoricalDataAge");
+    this.distanceStep = psl.getParameterSource().getDouble("ead.NCVHandling.collision.distanceStep");
+    this.timeDuration = psl.getParameterSource().getDouble("ead.NCVHandling.collision.timeDuration");
+    this.NCVReplanPeriod = (long) ((timeDuration / 2.0) * MS_PER_S); // Always replan after half of the ncv prediction has elapsed
+    this.timeRange = psl.getParameterSource().getDouble("ead.NCVHandling.collision.timeRange");
+    this.distanceBuffer = psl.getParameterSource().getDouble("ead.NCVHandling.collision.distanceBuffer");
+    this.crosstrackBuffer = psl.getParameterSource().getDouble("ead.NCVHandling.collision.crosstrackBuffer");
+
+    this.motionInterpolator = motionInterpolator;
+    
   }
 
   public void updateObjects(List<RoadwayObstacle> obstacles) {
 
     // Iterate over detected objects and keep only those in front of us in the same lane. 
     int currentLane = routeService.getCurrentRouteSegment().determinePrimaryLane(routeService.getCurrentCrosstrackDistance());
+    int inLaneObjectCount = 0;
+
     for (RoadwayObstacle obs : obstacles) {
 
       double frontObjectDistToCenters =  obs.getDownTrack() - routeService.getCurrentDowntrackDistance();
@@ -89,13 +85,15 @@ public class ObjectCollisionChecker implements INodeCollisionChecker {
 
       // If the object is in the same lane and in front of the host vehicle
       // Add it to the set of tracked object histories
+      // TODO this currently does not handle if the lane index changes between the host and detected object
       if (inLane && frontObjectDistToCenters > -0.0) {
-
+        
+        inLaneObjectCount++;
         if (!trackedLaneObjectsHistory.containsKey(obs.getObject().getId())) {
           // Sort object history as time sorted priority queue
           PriorityQueue<RoadwayObstacle> objectHistory = new PriorityQueue<RoadwayObstacle>(new Comparator<RoadwayObstacle> (){
             public int compare(RoadwayObstacle r1, RoadwayObstacle r2) {
-              // Assume all objects are in front of host vehicle and in same lane TODO comment
+              // Sort objects so oldest objects are at the front of the queue
               return r1.getObject().getHeader().getStamp().compareTo(r2.getObject().getHeader().getStamp());
             }
           });
@@ -106,38 +104,86 @@ public class ObjectCollisionChecker implements INodeCollisionChecker {
         
         // Add new historical data and remove old data to maintain queue size if needed
         objHistory.add(obs);
-
-        if (objHistory.size() > maxObjectHistory) {
-          objHistory.poll();
-        }
       }
     }
 
-    // TODO we need to remove expired objects from map
-
-    double distanceStep, timeDuration; //TODO
-
     // Predict and cache the motion of each object
-    IMotionPredictor motionPredictor; // TODO
+    // Also remove expired data and identify expired objects
+    final Time minObjStamp = Time.fromMillis(System.currentTimeMillis() - maxHistoricalDataAge);
+    List<Integer> expiredObjIds = new LinkedList<>(); // List of objects which are expired and marked for removal
+
     for (Entry<Integer, PriorityQueue<RoadwayObstacle>> e: trackedLaneObjectsHistory.entrySet()) {
+
+      removeExpiredData(e.getValue(), minObjStamp); // Remove expired history data
+      
+      if (e.getValue().isEmpty()) { // Check if an object is totally expired
+        expiredObjIds.add(e.getKey());
+        continue; // No point in computing prediction if the object is expired anyway
+      }
+
       trackedLaneObjectsPredictions.put(
         e.getKey(), 
         motionPredictor.predictMotion(e.getKey().toString(), e.getValue(), distanceStep, timeDuration)
       );
     }
+
+    // Remove expired objects
+    for (Integer objId: expiredObjIds) {
+      trackedLaneObjectsHistory.remove(objId);
+      trackedLaneObjectsPredictions.remove(objId);
+    }
+
+    // Check for collisions using new object data
+    boolean collisionDetected = checkCollision(interpolatedHostPlan.get());
+
+    if (collisionDetected) { // Any time there is a detected collision force a replan
+      ncvDetectionTime = System.currentTimeMillis();
+      arbitratorService.requestNewPlan(); // Request a replan from the arbitrator
+
+    } else if (ncvDetectionTime != null && System.currentTimeMillis() - ncvDetectionTime > NCVReplanPeriod){ // If there was previously a replan to avoid a ncv and enough time has passed replan
+      ncvDetectionTime = System.currentTimeMillis();
+      arbitratorService.requestNewPlan(); // Request a replan from the arbitrator
+
+    } else if (inLaneObjectCount == 0) { // If no in lane objects were detected the ncv is no longer an issue. The assumption being made here is that sensor fusion has already filtered our incoming object data.
+      
+      ncvDetectionTime = null;
+    } else {
+      // There is no need to take action when the plan is executing well without collisions
+    }
   }
 
-  @Override
-  public boolean hasCollision(List<Node> trajectory) {
-    
-    List<RoutePointStamped> hostPlan; // TODO
+  private void removeExpiredData(PriorityQueue<RoadwayObstacle> objHistory, Time minObjStamp) {
+
+    Time oldestObjStamp = objHistory.peek().getObject().getHeader().getStamp();
+    while (oldestObjStamp != null && oldestObjStamp.compareTo(minObjStamp) < 0) {
+      objHistory.poll();
+      oldestObjStamp = objHistory.peek().getObject().getHeader().getStamp();
+    }
+  }
+
+  public void setHostPlan(List<Node> hostPlan) {
+    interpolatedHostPlan.set(motionInterpolator.interpolateMotion(hostPlan, distanceStep));
+  }
+
+  private boolean checkCollision(List<RoutePointStamped> routePlan) {
+    // Check the proposed trajectory against all tracked objects for collisions
     for (Entry<Integer, List<RoutePointStamped>> objPrediction: trackedLaneObjectsPredictions.entrySet()) {
       // Check for conflicts against each object and return true if any conflict is found
-      if (!conflictDetector.getConflicts(hostPlan, objPrediction.getValue()).isEmpty()) {
+      if (!conflictDetector.getConflicts(routePlan, objPrediction.getValue()).isEmpty()) { // TODO we need to pass in the appropriate object bounds
         return true;
       }
     }
 
     return false;
+  }
+
+  @Override
+  public boolean hasCollision(List<Node> trajectory) {
+    
+    // Convert the proposed trajectory to route points
+    List<RoutePointStamped> routePlan = motionInterpolator.interpolateMotion(trajectory, distanceStep);
+
+    return checkCollision(routePlan); // Check for collisions with tracked objects
+
   }
 }
