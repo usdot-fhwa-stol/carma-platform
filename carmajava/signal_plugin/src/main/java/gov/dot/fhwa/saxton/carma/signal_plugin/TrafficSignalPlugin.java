4e8e083e77c9ac51bf546b70da593c2be951c55a
Michael McConnell <Michael.McConnell-2@leidos.com>
2018-10-29 10:53:03 -0400

    Make the continue through intersection popup appear a bit earlier

--- carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/TrafficSignalPlugin.java
+++ carmajava/signal_plugin/src/main/java/gov/dot/fhwa/saxton/carma/signal_plugin/TrafficSignalPlugin.java
@@ -127,6 +127,7 @@ public class TrafficSignalPlugin extends AbstractPlugin implements IStrategicPlu
     private double operSpeedScalingFactor = 1.0;
     private double speedCommandQuantizationFactor = 0.1;
     private AtomicBoolean involvedInControl = new AtomicBoolean(false);
+    private double popupOnRedTime;
     static private final double CM_PER_M = 100.0;
     static private final double MAX_DTSB = Integer.MAX_VALUE - 5.0; // Legacy Glidepath code returns Integer.MAX_VALUE for invalid dtsb. Add fudge factor for detecting it as a double
 
@@ -152,6 +153,8 @@ public class TrafficSignalPlugin extends AbstractPlugin implements IStrategicPlu
         // Pass params into GlidepathAppConfig
         appConfig = new GlidepathAppConfig(pluginServiceLocator.getParameterSource(), pluginServiceLocator.getRouteService());
         GlidepathApplicationContext.getInstance().setAppConfigOverride(appConfig);
+
+        popupOnRedTime = appConfig.getDoubleValue("popupOnRedTime");
 
         acceptableStopDistance = appConfig.getDoubleDefaultValue("ead.acceptableStopDistance", 6.0);
 
@@ -605,10 +608,11 @@ public class TrafficSignalPlugin extends AbstractPlugin implements IStrategicPlu
      * Helper function to evaluate state variables for the stopping condition
      */
     private void evaluateStatesForStopping() {
-        // We were stopped at a red light and now the light is green
+        // We were stopped at a red light and now the light is green or will be in a few seconds
         boolean stoppedForLight = stoppedAtLight();
         boolean phaseIsGreen = checkCurrentPhase(SignalPhase.GREEN);
-        if (stoppedForLight && phaseIsGreen) {
+        boolean phaseIsGreenSoon = phaseIsGreen || checkCurrentPhaseAndRemainingTime(SignalPhase.RED, popupOnRedTime);
+        if (stoppedForLight && phaseIsGreenSoon) {
             // If we have not requested acknowledgement from the user do it now
             if (awaitingUserConfirmation.compareAndSet(false, true)) {
                 // Update state variables
@@ -692,6 +696,22 @@ public class TrafficSignalPlugin extends AbstractPlugin implements IStrategicPlu
     private boolean checkCurrentPhase(SignalPhase phase) {
         List<gov.dot.fhwa.saxton.carma.signal_plugin.asd.IntersectionData> sortedIntersections = glidepathTrajectory.getSortedIntersections();
         return !sortedIntersections.isEmpty() && sortedIntersections.get(0).currentPhase == phase;
+    }
+
+    /**
+     * Helper function returns true if the current phase of the nearest intersection is equal to the requested phase
+     * and there is less than or equal time remaining to the specified maxTimeRemaining
+     * 
+     * @param phase The phase to check against
+     * @param maxTimeRemaining The max time allowed to be remaining in this phase for the function to return true
+     * 
+     * @return True if nearest intersection phase is equal to current phase and has less than or equal time remaining to maxTimeRemaining
+     */
+    private boolean checkCurrentPhaseAndRemainingTime(SignalPhase phase, double maxTimeRemaining) {
+        List<gov.dot.fhwa.saxton.carma.signal_plugin.asd.IntersectionData> sortedIntersections = glidepathTrajectory.getSortedIntersections();
+        return !sortedIntersections.isEmpty() 
+            && sortedIntersections.get(0).currentPhase == phase
+            && sortedIntersections.get(0).timeToNextPhase <= maxTimeRemaining;
     }
 
     @Override
