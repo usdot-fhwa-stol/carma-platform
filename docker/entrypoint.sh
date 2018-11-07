421a2917df22ca6718a786ce176aab50c21fe667
Kyle Rush <kyle.rush@leidos.com>
2018-11-07 16:03:31 -0500

    Run Apache webserver prior to CARMA startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,6 +17,8 @@
 
 # CARMA Docker Entrypoint Script
 
+sudo service apache2 start
+
 source /opt/carma/app/bin/setup.bash
 tmux new-session -s carma -- roslaunch carma saxton_cav.launch 
 # mock_can:=true mock_dsrc:=true mock_srx_controller:=true mock_pinpoint:=true mock_radar:=true mock_lateral_controller:=true 
