4f19400dce0f44ed9bd1476f0d398552567e3d2d
Kyle Rush <kyle.rush@leidos.com>
2018-11-07 11:21:15 -0500

    Add remote deploy script

---
+++ engineering_tools/docker-deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#  Copyright (C) 2018 LEIDOS.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain a copy of
+#  the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# Docker deployment script for CARMA
+#
+# Loads a docker image in the local registry to the remote target
+# Params:
+# $1 - image name to load
+# $2 - Username to use on remote host
+# $3 - IP address or hostname of remote host
+
+if [ ! -z $(command -v pv) ]; then
+    echo "Installing docker image $1 to host $2@$3..."
+    docker save $1 | bzip2 | pv | ssh $2@$3 'bunzip2 | docker load'
+    echo "Install complete!"
+else 
+    echo "Installing docker image $1 to host $2@$3..."
+    echo "pv not installed, no progress info available..."
+    docker save $1 | bzip2 | ssh $2@$3 'bunzip2 | docker load'
+    echo "Install complete!"
+fi
