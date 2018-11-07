ea3905614415bc4b286d8e74b72dccef87d935df
Kyle Rush <kyle.rush@leidos.com>
2018-11-07 09:46:01 -0500

    Move all source code managment activity to checkout.sh

--- docker/checkout.sh
+++ docker/checkout.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# CARMA extra packages checkout script
+# CARMA packages checkout script
+
+echo "Checking out base CARMA platform packages version: ${CARMA_VERSION}..."
+cd /root 
+mkdir src 
+cd src 
+git clone git@github.com:fhwa-saxton/CARMASensitive.git --branch $CARMA_VERSION
 
 if [[ ! -z $EXTRA_PACKAGES ]]; then
-    echo "Attempting to check out extra CARMA packages from $EXTRA_PACKAGES..."
-    git clone $EXTRA_PACKAGES
+    echo "Attempting to check out extra CARMA packages from $EXTRA_PACKAGES version $EXTRA_PACKAGES_VERSION..."
+    git clone $EXTRA_PACKAGES --branch $EXTRA_PACKAGES_VERSION
 else 
     echo "No repository specified for extra CARMA packages to be downloaded from"
 fi
