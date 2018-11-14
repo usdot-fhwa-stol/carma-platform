0f26c92fd5e785d6c9331cbce2ef050e321ad7b8
Kyle Rush <kyle.rush@leidos.com>
2018-11-14 10:08:27 -0500

    Add dockerfile for web-ui and begin script modification

--- website/scripts/launch.bash
+++ website/scripts/launch.bash
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # Kill all ROS related processes
-pkill -f ros
-
-# Source ROS (Must be called after ROS_HOME assignment)
-# Assumes ROS Java was installed as package and included in this source
-source /opt/ros/kinetic/setup.bash
-
-# Source platform and set ros environment variables
-source /opt/carma/app/bin/setup.bash
-
-# Set ros to run from /opt/carma instead of home directory
-export ROS_HOME=/opt/carma/.ros
-
-# Remove bad launch.pid file if it exists
-rm /opt/carma/launch.pid
+docker kill carma
 
 # Launch platform
 rosBagRecord=$1
-roslaunch --pid=/opt/carma/launch.pid carma saxton_cav.launch use_rosbag:=$rosBagRecord
+CONFIG_FOLDER=/opt/carma/vehicle
+docker run \
+    --rm \
+    --net=host \
+    --name carma \
+    -v $CONFIG_FOLDER/HostVehicleParams.yaml:/opt/carma/params/HostVehicleParams.yaml \
+    -v $CONFIG_FOLDER/saxton_cav.urdf:/opt/carma/urdf/saxton_cav.urdf \
+    -v $CONFIG_FOLDER/saxton_cav.launch:/opt/carma/launch/saxton_cav.launch \
+    -v $CONFIG_FOLDER/drivers.launch:/opt/carma/launch/drivers.launch \
+    -v $CONFIG_FOLDER/carma.config.js:/var/www/html/scripts/carma.config.js \
+    -itd carma $rosbagRecord
