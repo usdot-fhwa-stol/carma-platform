e11770a632b99302c20e08794eb82342c7c9a078
Kyle Rush <kyle.rush@leidos.com>
2019-01-31 13:11:28 -0500

    Add doc comment for entrypoint

--- carma_base/entrypoint.sh
+++ carma_base/entrypoint.sh
@@ -14,4 +14,9 @@
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
+# Intialize the CARMA environment by sourcing the necessary ROS shell scripts
+# then run whatever string is passed as argument to this script in that 
+# initialized context. E.g. "entrypoint.sh roslaunch carma saxton_cav_docker.launch"
+# runs "roslaunch carma saxton_cav_docker.launch" after setting up the environment
+# such that ROS and CARMA are on the user's PATH
 source ~/.base-image/init-env.sh; exec "$@"
