6b5a76a4c199837ba4c997a2684cb8c2e4712e09
Michael McConnell <Michael.McConnell-2@leidos.com>
2019-05-17 17:03:40 -0400

    Update mock controller to new api spec

--- carmajava/mock_drivers/src/main/java/gov/dot/fhwa/saxton/carma/mock_drivers/MockControllerDriver.java
+++ carmajava/mock_drivers/src/main/java/gov/dot/fhwa/saxton/carma/mock_drivers/MockControllerDriver.java
@@ -49,18 +49,14 @@ public class MockControllerDriver extends AbstractMockDriver {
 
   // Topics
   // Published
-  protected final Publisher<diagnostic_msgs.DiagnosticArray> diagnosticsPub;
   protected Publisher<RobotEnabled> statusPub;
-  protected Publisher<LightBarStatus> lightBarStatusPub; //MF 02/2019
 
   // Subscribed
-  protected final Subscriber<std_msgs.Float32> longEffortSub;
-  protected final Subscriber<cav_msgs.SpeedAccel> subscriber;
+  protected final Subscriber<autoware_msgs.VehicleCmd> cmdSub;
 
   // Services
   // Server
   protected final ServiceServer<SetEnableRoboticRequest, SetEnableRoboticResponse> enabledSrv;
-  protected final ServiceServer<GetLightsRequest, GetLightsResponse> getLightsService;
   protected final ServiceServer<cav_srvs.SetLightsRequest, cav_srvs.SetLightsResponse> setLightsService;
 
   private final int EXPECTED_DATA_COL_COUNT = 9;
@@ -84,11 +80,12 @@ public class MockControllerDriver extends AbstractMockDriver {
     super(connectedNode);
     // Topics
     // Published
-    statusPub = connectedNode.newPublisher("control/robot_status", RobotEnabled._TYPE);
-    lightBarStatusPub = connectedNode.newPublisher("control/light_bar_status", LightBarStatus._TYPE); //MF 02/2019
+    statusPub = connectedNode.newPublisher("controller/robot_status", RobotEnabled._TYPE);
 
-    diagnosticsPub = connectedNode.newPublisher("/diagnostics", diagnostic_msgs.DiagnosticArray._TYPE);
-    enabledSrv = connectedNode.newServiceServer("control/enable_robotic", cav_srvs.SetEnableRobotic._TYPE,
+    // Subscribed
+    longEffortSub = connectedNode.newSubscriber("controller/vehicle_cmd", autoware_msgs.VehicleCmd._TYPE);
+
+    enabledSrv = connectedNode.newServiceServer("controller/enable_robotic", cav_srvs.SetEnableRobotic._TYPE,
         new ServiceResponseBuilder<SetEnableRoboticRequest, SetEnableRoboticResponse>() {
           @Override
           public void build(SetEnableRoboticRequest arg0, SetEnableRoboticResponse arg1) throws ServiceException {
@@ -96,25 +93,8 @@ public class MockControllerDriver extends AbstractMockDriver {
           }
         });
 
-    // Subscribed
-    longEffortSub = connectedNode.newSubscriber("control/cmd_longitudinal_effort", std_msgs.Float32._TYPE);
-    subscriber = connectedNode.newSubscriber("control/cmd_speed", cav_msgs.SpeedAccel._TYPE);
-
     // Services
     // Server
-    getLightsService = connectedNode.newServiceServer("control/get_lights", cav_srvs.GetLights._TYPE,
-        new ServiceResponseBuilder<cav_srvs.GetLightsRequest, cav_srvs.GetLightsResponse>() {
-          @Override
-          public void build(cav_srvs.GetLightsRequest request, cav_srvs.GetLightsResponse response) {
-
-            cav_msgs.LightBarStatus lightStatus = response.getStatus();
-            lightStatus.setFlash((byte) (lightBarFlash ? 1 : 0));
-            lightStatus.setLeftArrow((byte) (leftArrow ? 1 : 0));
-            lightStatus.setRightArrow((byte) (rightArrow ? 1 : 0));
-            lightStatus.setTakedown((byte) (takedown ? 1 : 0));
-            response.setStatus(lightStatus);
-          }
-        });
     setLightsService = connectedNode.newServiceServer("control/set_lights", cav_srvs.SetLights._TYPE,
         new ServiceResponseBuilder<SetLightsRequest, SetLightsResponse>() {
           @Override
@@ -135,19 +115,7 @@ public class MockControllerDriver extends AbstractMockDriver {
   protected void publishData(List<String[]> data) throws IllegalArgumentException {
 
     for (String[] elements : data) {
-      // Make messages
-      diagnostic_msgs.DiagnosticArray diagMsg = diagnosticsPub.newMessage();
       RobotEnabled statusMsg = statusPub.newMessage();
-      LightBarStatus lightBarStatusMsg = lightBarStatusPub.newMessage(); //MF 02/2019
-
-      // //MF 02/2019: Build Light Bar Status Message //Right Arrow
-      // GREEN FLASH
-      lightBarStatusMsg.setGreenFlash(LightBarStatus.ON);
-      lightBarStatusMsg.setLeftArrow(LightBarStatus.OFF);
-      lightBarStatusMsg.setRightArrow(LightBarStatus.OFF);
-      lightBarStatusMsg.setGreenSolid(LightBarStatus.OFF);
-      lightBarStatusMsg.setFlash(LightBarStatus.OFF);
-      lightBarStatusMsg.setTakedown(LightBarStatus.OFF);
 
       // Build RobotEnabled Message
       statusMsg.setBrakeDecel(Double.parseDouble(elements[BRAKE_DECEL_IDX]));
@@ -155,32 +123,8 @@ public class MockControllerDriver extends AbstractMockDriver {
       statusMsg.setRobotActive(Boolean.parseBoolean(elements[ROBOT_ENABLED_IDX]));
       statusMsg.setTorque(Double.parseDouble(elements[TORQUE_IDX]));
 
-      // Build Diagnostics Message: Assumes that only diagnostic is in a data file line
-      std_msgs.Header hdr = messageFactory.newFromType(std_msgs.Header._TYPE);
-      hdr.setFrameId("0");
-      hdr.setSeq(Integer.parseInt(elements[SAMPLE_ID_IDX]));
-      hdr.setStamp(connectedNode.getCurrentTime());
-
-      diagMsg.setHeader(hdr);
-
-      diagnostic_msgs.DiagnosticStatus diagnosticStatus = messageFactory
-          .newFromType(diagnostic_msgs.DiagnosticStatus._TYPE);
-      diagnosticStatus.setHardwareId(elements[HARDWARE_ID_IDX]);
-      diagnosticStatus.setLevel(Byte.valueOf(elements[DIAG_LEVEL_IDX]));
-      diagnosticStatus.setName(getGraphName().toString());
-      diagnosticStatus.setMessage(elements[DIAG_MSG_IDX]);
-
-      diagnostic_msgs.KeyValue keyValue = messageFactory.newFromType(diagnostic_msgs.KeyValue._TYPE);
-      keyValue.setKey(elements[DIAG_KEY_MSG]);
-      keyValue.setValue(elements[DIAG_VALUE_IDX]);
-
-      diagnosticStatus.setValues(new ArrayList<>(Arrays.asList(keyValue)));
-      diagMsg.setStatus(new ArrayList<>(Arrays.asList(diagnosticStatus)));
-
       // Publish Data
-      diagnosticsPub.publish(diagMsg);
       statusPub.publish(statusMsg);
-      lightBarStatusPub.publish(lightBarStatusMsg); //MF 02/2019
     }
   }
 
@@ -196,13 +140,13 @@ public class MockControllerDriver extends AbstractMockDriver {
 
   @Override
   protected List<String> getDriverTypesList() {
-    return new ArrayList<>(Arrays.asList("lon_controller"));
+    return new ArrayList<>(Arrays.asList("controller"));
   }
 
   @Override
   public List<String> getDriverAPI() {
     return new ArrayList<>(
-        Arrays.asList("control/enable_robotic", "control/cmd_longitudinal_effort",
-            "control/cmd_speed", "control/get_lights", "control/set_lights", "control/robot_status"));
+        Arrays.asList("controller/enable_robotic", "controller/vehicle_cmd",
+            "controller/set_lights", "controller/robot_status"));
   }
 }
