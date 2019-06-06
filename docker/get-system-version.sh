e4f80c5626d5d3a61342cacd2f4335ae38ed5d18
Kyle Rush <kyle.rush@leidos.com>
2019-06-06 16:28:15 -0400

    Fix incorrect variable reference

--- docker/get-system-version.sh
+++ docker/get-system-version.sh
@@ -2,5 +2,5 @@
 
 SYSTEM_TAG_PREFIX="CARMASystem"
 
-git describe --all --match="$COMPONENT_TAG_PREFIX*" --always --dirty="-SNAPSHOT" | awk -F "/" '{print $NF}'
+git describe --all --match="$SYSTEM_TAG_PREFIX*" --always --dirty="-SNAPSHOT" | awk -F "/" '{print $NF}'
 
