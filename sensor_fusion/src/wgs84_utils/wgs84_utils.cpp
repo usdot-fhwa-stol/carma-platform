7344a5ce3d78961edbfa5a3b5e069d0d15c5d41f
Michael McConnell <Michael.McConnell-2@leidos.com>
2019-06-14 17:01:24 -0400

    update cmake lists to allow wgs84_utils to be used in other packages

--- sensor_fusion/src/wgs84_utils/wgs84_utils.cpp
+++ sensor_fusion/src/wgs84_utils/wgs84_utils.cpp
@@ -31,7 +31,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-#include "wgs84_utils.h"
+#include "wgs84_utils/wgs84_utils.h"
 
 #include <cmath>
 /**
@@ -41,11 +41,9 @@
  */
 double wgs84_utils::calcMetersPerRadLat(const wgs84_utils::wgs84_coordinate &tie_point)
 {
-    double a = EARTH_RADIUS_METERS;
-    double e_sq = 0.00669438;
     double s_sq = pow(sin(tie_point.lat * DEG2RAD), 2);
 
-    double R_m = a * (1.0 - e_sq) / pow(sqrt(1.0 - e_sq * s_sq), 3);
+    double R_m = Rea * (1.0 - e_sqr) / pow(sqrt(1.0 - e_sqr * s_sq), 3);
 
     double meters_per_rad_lat = R_m + tie_point.elevation;
 
@@ -59,11 +57,9 @@ double wgs84_utils::calcMetersPerRadLat(const wgs84_utils::wgs84_coordinate &tie
  */
 double wgs84_utils::calcMetersPerRadLon(const wgs84_utils::wgs84_coordinate &tie_point)
 {
-    double a = EARTH_RADIUS_METERS;
-    double e_sq = 0.00669438;
     double s_sq = pow(sin(tie_point.lat * DEG2RAD), 2);
 
-    double R_n = a / (sqrt(1.0 - e_sq * s_sq));
+    double R_n = Rea / (sqrt(1.0 - e_sqr * s_sq));
 
     double meters_per_rad_lon = (R_n + tie_point.elevation) * cos(tie_point.lat * DEG2RAD);
 
@@ -122,16 +118,6 @@ void wgs84_utils::convertToOdom(const wgs84_utils::wgs84_coordinate &src,
  */
 tf2::Vector3 wgs84_utils::geodesic_to_ecef(const wgs84_utils::wgs84_coordinate &loc, tf2::Transform ecef_in_ned) {
 
-    constexpr double Rea = 6378137.0; // Semi-major axis radius meters
-    constexpr double Rea_sqr = Rea*Rea;
-    constexpr double f = 1.0 / 298.257223563; //The flattening factor
-    constexpr double Reb = Rea * (1.0 - f); // //The semi-minor axis = 6356752.0
-    constexpr double Reb_sqr = Reb*Reb;
-    constexpr double e = 0.08181919084262149; // The first eccentricity (hard coded as optimization) calculated as Math.sqrt(Rea*Rea - Reb*Reb) / Rea; as C++11 sqrt is not constexpr
-    constexpr double e_sqr = e*e;
-    constexpr double e_p = 0.08209443794969568; // e prime (hard coded as optimization) calculated as Math.sqrt((Rea_sqr - Reb_sqr) / Reb_sqr); as C++11 sqrt is not constexpr
-
-
     // frame2ecefTransform needs to define the position of the ecefFrame relative to the desired frame
     // Put geodesic in proper units
     double lonRad = loc.lon;
@@ -154,6 +140,43 @@ tf2::Vector3 wgs84_utils::geodesic_to_ecef(const wgs84_utils::wgs84_coordinate &
     tf2::Vector3 point_in_ned = ecef_in_ned * ecef_point; 
     return point_in_ned;
 }
+
+  /**
+   * Converts a given 3d cartesian point in the ECEF frame into a WSG-84 geodesic location
+   * The provided point should be defined relative to a frame which has a transform with the ECEF
+   * @param point The cartesian point to be converted
+   * @return The calculated WSG-84 geodesic location
+   */
+  wgs84_utils::wgs84_coordinate wgs84_utils::ecef_to_geodesic(const tf2::Vector3& point) {
+
+    const double x = point[0];
+    const double y = point[1];
+    const double z = point[2];
+
+    // Calculate lat,lon,alt
+    const double p = sqrt((x*x) + (y*y));
+    // Handle special case of poles
+    if (p < 1.0e-10) {
+      const double poleLat = z < 0 ? -90:90;
+      constexpr double poleLon = 0;
+      const double poleAlt = z < 0 ? -z - Reb : z - Reb;
+      struct wgs84_coordinate geo_point = {poleLat, poleLon, 0, z};
+      return geo_point;
+    }
+    const double theta = atan((z*Rea) / (p*Reb));
+
+    const double lon = 2.0*atan(y / (x + p));
+    const double lat = atan((z + (e_p * e_p) * Reb * pow(sin(theta), 3)) / (p - e_sqr * Rea * pow(cos(theta), 3)));
+
+    const double cosLat = cos(lat);
+    const double sinLat = sin(lat);
+
+    const double N = Rea_sqr / sqrt(Rea_sqr * cosLat * cosLat + Reb_sqr * sinLat * sinLat);
+    const double alt = (p / cosLat) - N;
+
+    struct wgs84_coordinate geo_point = {lat * RAD2DEG, lon * RAD2DEG, 0, alt};
+    return geo_point;
+  }
 
 /**
  * Generates a transform describing the location/orientation of an NED frame in the ECEF frame based on the provided lat lon point.
@@ -181,3 +204,4 @@ tf2::Transform wgs84_utils::ecef_to_ned_from_loc(wgs84_coordinate loc) {
     
     return tf2::Transform(rotMat, ecef_point);
   }
+
