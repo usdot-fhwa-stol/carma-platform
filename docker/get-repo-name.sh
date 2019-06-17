27f666a6092cd9455d5701493f2b9474c28507ca
Kyle Rush <kyle.rush@leidos.com>
2019-06-17 11:04:53 -0400

    Finalize automated image tagging/versioning scripts

--- docker/get-repo-name.sh
+++ docker/get-repo-name.sh
@@ -14,7 +14,4 @@
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
-SYSTEM_TAG_PREFIX="CARMASystem"
-
-git describe --all --match="$SYSTEM_TAG_PREFIX*" --always --dirty="-SNAPSHOT" | awk -F "/" '{print $NF}'
-
+basename -s .git `git config --get remote.origin.url`
