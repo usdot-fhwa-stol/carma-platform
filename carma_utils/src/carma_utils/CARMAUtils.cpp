95feabae77ae13941bba687d2d239488bdc62638
Michael McConnell <Michael.McConnell-2@leidos.com>
2019-06-20 12:58:54 -0400

    Add CARMANodeHandle to provide exception handling

---
+++ carma_utils/src/carma_utils/CARMAUtils.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2018-2019 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+ * Library implementation entry point file. This file can be used to generate .o files for templated classes which include their implementation in the header. 
+ */ 
+
+#include "carma_utils/CARMAUtils.h"
