0f5d3f4b058b37aaba0e41e2d1d65ce6a09f6eca
itabrl <samir.manafzadehtabriz@leidos.com>
2019-09-23 12:27:28 -0700

    Update sate machine

--- guidance/src/guidance/guidance_state_machine_factory.cpp
+++ guidance/src/guidance/guidance_state_machine_factory.cpp
@@ -28,12 +28,12 @@ namespace guidance
         return GuidanceStateMachinePtr;
     }
 
-    std::unique_ptr<GuidanceStateMachine> createStateMachineInstance(std::string vehicle_state_machine_type) {
+    std::unique_ptr<GuidanceStateMachine> GuidanceStateMachineFactory::createStateMachineInstance(std::string vehicle_state_machine_type) {
         if(vehicle_state_machine_type == "cadilac") {
-            return guidance_state_machine_factory.createCadilacInstance();
+            return createCadilacInstance();
         }
         else if (vehicle_state_machine_type == "lexus") {
-            return guidance_state_machine_factory.createLexusInstance();
+            return createLexusInstance();
         }
         else {
             return nullptr;
