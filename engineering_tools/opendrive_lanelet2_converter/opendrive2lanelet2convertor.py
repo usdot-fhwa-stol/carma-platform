a6b5e9b212d57ba054f9e3641aaf809ff1f1be8b
itabrl <samir.manafzadehtabriz@leidos.com>
2019-12-11 12:02:25 -0800

    update convertor

--- engineering_tools/opendrive_lanelet2_converter/opendrive2lanelet2convertor.py
+++ engineering_tools/opendrive_lanelet2_converter/opendrive2lanelet2convertor.py
@@ -1,7 +1,7 @@
 import os
 import sys, getopt
 from pyproj import Proj, transform
-
+import numpy as np
 from commonroad.common.file_writer import CommonRoadFileWriter
 from opendrive2lanelet.opendriveparser.parser import parse_opendrive
 from opendrive2lanelet.network import Network
@@ -37,20 +37,23 @@ class Way:
         for i in self.nodes:
             xml.SubElement(way_element, "nd", {"ref": str(i.id)})
         xml.SubElement(way_element, "tag", {"k": "level", "v": "0"})
-
+        xml.SubElement(way_element, "tag", {"k": "type", "v": "linestring"})
         return way_element
 
 # class representing relation in lanelet2
 class Relation:
-    def __init__(self, id, member_left_id, member_right_id, from_cad_id, to_cad_id, cad_id, relation_type):
+    def __init__(self, id, member_left, member_right, from_cad_id, to_cad_id, cad_id, relation_type):
         self.id = str(id)
-        self.member_left_id = str(member_left_id)
-        self.member_right_id = str(member_right_id)
+        self.member_left = member_left
+        self.member_right = member_right
+        self.member_left_id = str(member_left.id)
+        self.member_right_id = str(member_right.id)
         self.from_cad_id = from_cad_id
         self.to_cad_id = to_cad_id
         self.relation_type = relation_type
         self.cad_id = cad_id
         self.turn_direction = "straight"
+        self.set_turn_direction()
 
     def create_xml_relation_object(self):
         relation_element = xml.Element("relation", {"id": self.id, "version": str(1), "visible": "true"})
@@ -61,21 +64,49 @@ class Relation:
         xml.SubElement(relation_element, "tag", {"k": "level", "v": "0"})
         xml.SubElement(relation_element, "tag", {"k": "location", "v": "private"})
         xml.SubElement(relation_element, "tag", {"k": "participant:vehicle", "v": "yes"})
-        xml.SubElement(relation_element, "tag", {"k": "road_type", "v": "junction_road"})
-        xml.SubElement(relation_element, "tag", {"k": "subtype", "v": "junction_road"})
+        xml.SubElement(relation_element, "tag", {"k": "road_type", "v": "road"})
+        xml.SubElement(relation_element, "tag", {"k": "subtype", "v": "road"})
         xml.SubElement(relation_element, "tag", {"k": "type", "v": "lanelet"})
         xml.SubElement(relation_element, "tag", {"k": "from_cad_id", "v": self.from_cad_id})
         xml.SubElement(relation_element, "tag", {"k": "to_cad_id", "v": self.to_cad_id})
         xml.SubElement(relation_element, "tag", {"k": "near_spaces", "v": "[]"})
-        xml.SubElement(relation_element, "tag", {"k": "turn_direction", "v": "straight"})
-
+        xml.SubElement(relation_element, "tag", {"k": "turn_direction", "v": self.turn_direction})
         return relation_element
+    
+    def set_direction(self, a, b, c):
+        if(((b[0] - a[0])*(c[1] - a[1]) - (b[1] - a[1])*(c[0] - a[0])) > 0):
+            return 1
+        else:
+            return 0
+
+    def set_turn_direction(self):
+        turn_direction = "straight"
+        if(len(self.from_cad_id) == 1 and len(self.to_cad_id) == 1):
+            start = np.array([self.member_left.nodes[0].local_x,self.member_left.nodes[0].local_y])
+            mid = np.array([self.member_left.nodes[1].local_x,self.member_left.nodes[1].local_y])
+            end = np.array([self.member_left.nodes[-1].local_x,self.member_left.nodes[-1].local_y])
+
+            start_mid = mid - start
+            start_end = end - start
+            cosine_angle = np.dot(start_mid, start_end) / (np.linalg.norm(start_mid) * np.linalg.norm(start_end))
+            alpha = np.arccos(cosine_angle)
+
+            if(np.degrees(alpha) > 10):
+                if(self.set_direction(start,mid,end) == 0):
+                    turn_direction = "right"
+                else:
+                    turn_direction = "left"
+
+        self.turn_direction = turn_direction
 
 # class used to convert opendrive map to lanelet2 map
 class Opendrive2Lanelet2Convertor:
     def __init__(self, fn):
         self.scenario, self.geoReference = self.open_drive_loader(fn)
         self.root = xml.Element('osm',{'generator': 'lanelet2','version': '0.6'})
+        self.nodes = xml.Element('nodes')
+        self.ways = xml.Element('ways')
+        self.relations = xml.Element('relations')
 
     def open_drive_loader(self, fn):
         fi = open(fn.format(os.path.dirname(os.path.realpath(__file__))), "r")
@@ -93,18 +124,33 @@ class Opendrive2Lanelet2Convertor:
         outProj = Proj(init="epsg:4326")
         return transform(inProj,outProj,x,y)
 
+    def get_point_utm(self,lon,lat):
+        inProj = Proj("+proj=utm +zone=23K, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+        return inProj(lon,lat)
+
     def write_xml_to_file(self,fn):
+                
+        for child in self.nodes.getchildren():
+            self.root.append(child)
+
+        for child in self.ways.getchildren():
+            self.root.append(child)
+
+        for child in self.relations.getchildren():
+            self.root.append(child)
+
         tree = xml.ElementTree(self.root)
         fh = open(fn, "wb")
         tree.write(fh)
-
 
     # convert vertice from opendrive to a node in lanelet 
     def convert_vertice_to_node(self,node_id,vertice):
         x = vertice[0]
         y = vertice[1]
         lon, lat = self.get_point_geo(x,y)
-        return Node(node_id,lat,lon,x,y)
+        # local_x, local_y = self.get_point_utm(lon,lat)
+        local_x, local_y = x, y
+        return Node(node_id,lat,lon, local_x, local_y)
     
     # apply convert_vertice_to_node to a list of nodes
     # bound_id 0,1 for left and right 
@@ -112,9 +158,9 @@ class Opendrive2Lanelet2Convertor:
         nodes = []
         for j in range(len(vertices)):
             node_id = relation_id + str(bound_id) + "{0:0=3d}".format(j)
-            node = self.convert_vertice_to_node(node_id, vertices[j])                
+            node = self.convert_vertice_to_node(node_id, vertices[j])
             nodes.append(node)
-            self.root.append(node.create_xml_node_object())
+            self.nodes.append(node.create_xml_node_object())
         return nodes
 
     def convert(self, fn):
@@ -133,14 +179,13 @@ class Opendrive2Lanelet2Convertor:
             right_way_id = relation_id + '1'
             right_way = Way(right_way_id,right_nodes)
 
-            self.root.append(left_way.create_xml_way_object())
-            self.root.append(right_way.create_xml_way_object())
+            self.ways.append(left_way.create_xml_way_object())
+            self.ways.append(right_way.create_xml_way_object())
 
             from_cad_id = self.scenario._lanelet_network._lanelets[i]._successor
             to_cad_id = self.scenario._lanelet_network._lanelets[i]._predecessor
-            relation = Relation(relation_id, left_way.id, right_way.id, from_cad_id, to_cad_id, cad_id, "lanelet")
-
-            self.root.append(relation.create_xml_relation_object())
+            relation = Relation(relation_id, left_way, right_way, from_cad_id, to_cad_id, cad_id, "lanelet")
+            self.relations.append(relation.create_xml_relation_object())
 
         self.write_xml_to_file(fn)
 
