06d9ff112fc82d42f25547877fb9293afbaf6ef1
maefromm <MaeMargarette.M.Fromm@leidos.com>
2019-12-20 16:13:42 -0500

    updated to enforce docker ID 999 when using this script

--- engineering_tools/install-docker.sh
+++ engineering_tools/install-docker.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 # Docker installation script
+
+# Ensure docker group id is 999. CARMA UI is dependent on having the group id be the same between the image and vehicle PC.
+if [[ -z $(grep  -i "docker" /etc/group) ]]; then
+    	echo "User docker does not exists in /etc/group, creating docker group id of 999"
+   	    addgroup --gid 999 docker
+else
+	echo "User docker already exists in /etc/group."
+
+	if [[ $(grep  -i "docker" /etc/group) == *"docker:x:999"* ]]; then
+	    echo "Docker group id is correct"
+	else
+	    echo "ERROR: CARMA requires the docker group id 999 in the host PC. Please update the Host PC to correct this before trying again."
+	    exit
+	fi
+fi
+
 # Compiled from https://docs.docker.com/install/linux/docker-ce/ubuntu/
 
 sudo apt-get remove docker docker-engine docker.io
