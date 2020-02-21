eaaea7c8fcb535bce0101c8a0e8cc20d8ce1e145
Aswath Suresh <as10616@nyu.edu>
2020-02-21 12:55:18 -0800

    objectTrackigDetection

--- object_detection_tracking/src/object_detection_tracking_worker.cpp
+++ object_detection_tracking/src/object_detection_tracking_worker.cpp
@@ -33,34 +33,31 @@ void ObjectDetectionTrackingWorker::detectedObjectCallback(const autoware_msgs::
 		//Presence vector message is used to describe objects coming from potentially
 		//different sources. The presence vector is used to determine what items are set
 		//by the producer
-		obj.presence_vector=obj.presence_vector | ID_PRESENCE_VECTOR;
-		obj.presence_vector=obj.presence_vector | POSE_PRESENCE_VECTOR;
-		obj.presence_vector=obj.presence_vector | VELOCITY_PRESENCE_VECTOR;
-		obj.presence_vector=obj.presence_vector | SIZE_PRESENCE_VECTOR;
-		obj.presence_vector=obj.presence_vector | OBJECT_TYPE_PRESENCE_VECTOR;
-		obj.presence_vector=obj.presence_vector | DYNAMIC_OBJ_PRESENCE;
+		obj.presence_vector=obj.presence_vector | obj.ID_PRESENCE_VECTOR;
+		obj.presence_vector=obj.presence_vector | obj.POSE_PRESENCE_VECTOR;
+		obj.presence_vector=obj.presence_vector | obj.VELOCITY_PRESENCE_VECTOR;
+		obj.presence_vector=obj.presence_vector | obj.SIZE_PRESENCE_VECTOR;
+		obj.presence_vector=obj.presence_vector | obj.OBJECT_TYPE_PRESENCE_VECTOR;
+		obj.presence_vector=obj.presence_vector | obj.DYNAMIC_OBJ_PRESENCE;
 		
 		//Object id. Matching ids on a topic should refer to the same object within some time period, expanded
 		obj.id=obj_array.objects[i].id;
 
 		//Pose of the object within the frame specified in header
 		obj.pose.pose=obj_array.objects[i].pose;
-		obj.pose.covariance[0]=obj_array.objects[i].variance[0];
-		obj.pose.covariance[7]=obj_array.objects[i].variance[1];
-		obj.pose.covariance[17]=obj_array.objects[i].variance[2];
+		obj.pose.covariance[0]=obj_array.objects[i].variance.x;
+		obj.pose.covariance[7]=obj_array.objects[i].variance.y;
+		obj.pose.covariance[17]=obj_array.objects[i].variance.z;
 
 		//Average velocity of the object within the frame specified in header
 		obj.velocity.twist=obj_array.objects[i].velocity;
 
 		//The size of the object aligned along the axis of the object described by the orientation in pose
 		//Dimensions are specified in meters
-		for(int j=0;j<obj_array.objects[i].dimensions.size();j++)
-		{
-		obj.size[j]=obj_array.objects[i].dimensions[j];
-	    }
-
+		obj.size=obj_array.objects[i].dimensions;
+	    
 		//describes a general object type as defined in this message
-        obj.object_type=UNKNOWN;
+        obj.object_type=obj.UNKNOWN;
 
 		// Binary value to show if the object is static or dynamic (1: dynamic, 0: static)
 		
@@ -79,7 +76,7 @@ void ObjectDetectionTrackingWorker::detectedObjectCallback(const autoware_msgs::
 		pub_object_.publish(msg);
 }
 
-void set_publishers(ros::Publisher pub_object)
+void ObjectDetectionTrackingWorker::set_publishers(ros::Publisher pub_object)
 {
 	pub_object_=pub_object;
 }
