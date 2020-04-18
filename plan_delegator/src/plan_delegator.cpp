566b729cac277b4ed580ba97967a08337f308e9e
mishkamn <bayartsem@gmail.com>
2020-04-18 23:29:37 +0000

    fixed plan_delegator

--- plan_delegator/src/plan_delegator.cpp
+++ plan_delegator/src/plan_delegator.cpp
@@ -35,9 +35,9 @@ namespace plan_delegator
         traj_pub_ = nh_.advertise<cav_msgs::TrajectoryPlan>("plan_trajectory", 5);
         plan_sub_ = nh_.subscribe("final_maneuver_plan", 5, &PlanDelegator::maneuverPlanCallback, this);
         twist_sub_ = nh_.subscribe<geometry_msgs::TwistStamped>("current_velocity", 5,
-            [&](const geometry_msgs::TwistStampedConstPtr& twist) {latest_twist_ = *twist;});
+            [&latest_twist_ = this->latest_twist_](const geometry_msgs::TwistStampedConstPtr& twist) {latest_twist_ = *twist;});
         pose_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("current_pose", 5,
-            [&](const geometry_msgs::PoseStampedConstPtr& pose) {latest_pose_ = *pose;});
+            [&latest_pose_ = this->latest_pose_](const geometry_msgs::PoseStampedConstPtr& pose) {latest_pose_ = *pose;});
 
         ros::CARMANodeHandle::setSpinCallback(std::bind(&PlanDelegator::spinCallback, this));
         ros::CARMANodeHandle::setSpinRate(spin_rate_);
