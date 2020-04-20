1481859e369a324f6f3a1c94238a11ff409ace40
Michael McConnell <Michael.McConnell-2@leidos.com>
2020-04-20 12:11:31 -0700

    Added script to setup an ssh key for git

---
+++ engineering_tools/ssh_key_gen.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#  Copyright (C) 2018-2020 LEIDOS.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain a copy of
+#  the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# Script generates an SSH Key using the provided email.
+
+ssh-keygen -t rsa -b 4096 -C "$1"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
+gnome-terminal & firefox https://help.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
