1c3439eafc359e2ab87eb5e89c453d6316a51781
itabrl <samir.manafzadehtabriz@leidos.com>
2020-06-10 12:25:18 -0400

    add test

--- platooning_tactical_plugin/include/platooning_tactical_plugin/platooning_tactical_plugin.h
+++ platooning_tactical_plugin/include/platooning_tactical_plugin/platooning_tactical_plugin.h
@@ -7,8 +7,21 @@
 #include <vector>
 #include <boost/assign/std/vector.hpp>
 
-#include <iostream>
-#include <fstream>
+#include <vector>
+#include <cav_msgs/TrajectoryPlan.h>
+#include <cav_msgs/TrajectoryPlanPoint.h>
+#include <cav_msgs/Plugin.h>
+#include <autoware_msgs/Lane.h>
+#include <boost/shared_ptr.hpp>
+#include <carma_utils/CARMAUtils.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+
+#include <cav_srvs/PlanManeuvers.h>
+#include <cav_srvs/PlanTrajectory.h>
+
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
 namespace platooning_tactical_plugin {
 
@@ -24,11 +37,17 @@ namespace platooning_tactical_plugin {
             */
             PlatooningTacticalPlugin(ros::NodeHandle& nodeHandle);
 
+            // create uneven trajectory from waypoints
+            std::vector<cav_msgs::TrajectoryPlanPoint> create_uneven_trajectory_from_waypoints(std::vector<autoware_msgs::Waypoint> waypoints);
 
-            // final waypoint subscriber
-            // service call plan trajectory request respose
+            // get a sublist of waypoints marked by desired time span
+            std::vector<autoware_msgs::Waypoint> get_waypoints_in_time_boundary(std::vector<autoware_msgs::Waypoint> waypoints, double time_span);
 
-            // waypoint to trajectory convertor
+            // postprocess traj to add plugin names and shift time origin to the current ROS time
+            std::vector<cav_msgs::TrajectoryPlanPoint> post_process_traj_points(std::vector<cav_msgs::TrajectoryPlanPoint> trajectory);
+
+            // local copy of pose
+            boost::shared_ptr<geometry_msgs::PoseStamped const> pose_msg_;
 
             /*!
             * Destructor.
@@ -46,12 +65,38 @@ namespace platooning_tactical_plugin {
             */
             bool readParameters();
 
-            //! ROS speedAccel subscriber.
+            //! ROS subscribers.
             ros::Subscriber waypoint_subscriber_;
+            ros::Subscriber pose_subscriber_;
+            ros::Subscriber twist_subscriber_;
 
-            void waypoint_subscriber_callback(const autoware_msgs::Lane::ConstPtr& msg);
+            // ros service servers
+            ros::ServiceServer trajectory_srv_;
 
-            bool serviceCallback(std_srvs::Trigger::Request& request, std_srvs::Trigger::Response& response);
+            // generated trajectory plan
+            cav_msgs::TrajectoryPlan trajectory_msg;
+            // Array of waypoints
+            std::vector<autoware_msgs::Waypoint> waypoints_list;
+
+            void waypoints_cb(const autoware_msgs::LaneConstPtr& msg);
+            void pose_cb(const geometry_msgs::PoseStampedConstPtr& msg);
+            void twist_cd(const geometry_msgs::TwistStampedConstPtr& msg);
+
+            bool plan_trajectory_cb(cav_srvs::PlanTrajectoryRequest &req, cav_srvs::PlanTrajectoryResponse &resp);
+
+            // Plugin discovery message
+            cav_msgs::Plugin plugin_discovery_msg_;
+            ros::Publisher platooning_tactical_plugin_discovery_pub_;
+
+            // ROS params
+            double trajectory_time_length_;
+            double trajectory_point_spacing_;
+
+            // current vehicle speed
+            double current_speed_;
+
+            // convert waypoints to a trajectory
+            std::vector<cav_msgs::TrajectoryPlanPoint> compose_trajectory_from_waypoints(std::vector<autoware_msgs::Waypoint> waypoints);
 
     };
 }
--- platooning_tactical_plugin/src/platooning_tactical_plugin/platooning_tactical_plugin.cpp
+++ platooning_tactical_plugin/src/platooning_tactical_plugin/platooning_tactical_plugin.cpp
@@ -9,7 +9,25 @@ namespace platooning_tactical_plugin {
             ROS_ERROR("Could not read parameters.");
         }
 
-        waypoint_subscriber_ = nodeHandle_.subscribe("final_waypoint", 1, &PlatooningTacticalPlugin::waypoint_subscriber_callback, this);
+        waypoint_subscriber_ = nodeHandle_.subscribe("final_waypoint", 1, &PlatooningTacticalPlugin::waypoints_cb, this);
+        pose_subscriber_ = nodeHandle_.subscribe("current_pose", 1, &PlatooningTacticalPlugin::pose_cb, this);
+        twist_subscriber_ = nodeHandle_.subscribe("current_velocity", 1, &PlatooningTacticalPlugin::twist_cd, this);
+
+
+        platooning_tactical_plugin_discovery_pub_ = nodeHandle_.advertise<cav_msgs::Plugin>("plugin_discovery", 1);
+        plugin_discovery_msg_.name = "PlatooningTacticalPlugin";
+        plugin_discovery_msg_.versionId = "v1.0";
+        plugin_discovery_msg_.available = true;
+        plugin_discovery_msg_.activated = false;
+        plugin_discovery_msg_.type = cav_msgs::Plugin::TACTICAL;
+        plugin_discovery_msg_.capability = "tactical_plan/plan_trajectory";
+
+        ros::CARMANodeHandle::setSpinCallback([this]() -> bool {
+            platooning_tactical_plugin_discovery_pub_.publish(plugin_discovery_msg_);
+            return true;
+        });
+
+        trajectory_srv_ = nodeHandle_.advertiseService("plugins/PlatooningTachticalPlugin/plan_trajectory", &PlatooningTacticalPlugin::plan_trajectory_cb, this);
 
         ROS_INFO("Successfully launched node.");
     }
@@ -20,24 +38,146 @@ namespace platooning_tactical_plugin {
 
     bool PlatooningTacticalPlugin::readParameters()
     {
-        // if (!nodeHandle_.getParam("trajectory_time_length", trajectory_time_length)) {
-        //     return false;
-        // }
+        nodeHandle_.param("trajectory_time_length", trajectory_time_length_, 6.0);
+        nodeHandle_.param("trajectory_point_spacing", trajectory_point_spacing_, 0.1);
 
         return true;
     }
 
-    void PlatooningTacticalPlugin::waypoint_subscriber_callback(const autoware_msgs::Lane::ConstPtr& msg)
-    {
-        ROS_DEBUG("I heard SpeedAccel");
-    };
+    bool PlatooningTacticalPlugin::plan_trajectory_cb(cav_srvs::PlanTrajectoryRequest &req, cav_srvs::PlanTrajectoryResponse &resp){
+        resp.trajectory_plan = trajectory_msg;
+        resp.related_maneuvers.push_back(cav_msgs::Maneuver::LANE_FOLLOWING);
+        resp.maneuver_status.push_back(cav_srvs::PlanTrajectory::Response::MANEUVER_IN_PROGRESS);
 
-
-    bool PlatooningTacticalPlugin::serviceCallback(std_srvs::Trigger::Request& request, std_srvs::Trigger::Response& response)
-    {
-        response.success = true;
-        response.message = "ok";
         return true;
+    }
+
+    void PlatooningTacticalPlugin::waypoints_cb(const autoware_msgs::LaneConstPtr& msg)
+    {
+        if(msg->waypoints.size() == 0)
+        {
+            ROS_WARN_STREAM("Received an empty waypoints!");
+            return;
+        }
+
+        cav_msgs::TrajectoryPlan trajectory;
+        trajectory.header.frame_id = msg->header.frame_id;
+        trajectory.header.stamp = ros::Time::now();
+        trajectory.trajectory_id = boost::uuids::to_string(boost::uuids::random_generator()());
+        trajectory.trajectory_points = compose_trajectory_from_waypoints(msg->waypoints);
+        waypoints_list = msg->waypoints;
+        trajectory_msg = trajectory;
+
+        
+        ROS_DEBUG("I heard waypoints");
     };
+
+
+    std::vector<cav_msgs::TrajectoryPlanPoint> PlatooningTacticalPlugin::compose_trajectory_from_waypoints(std::vector<autoware_msgs::Waypoint> waypoints)
+    {
+        std::vector<autoware_msgs::Waypoint> partial_waypoints = get_waypoints_in_time_boundary(waypoints, trajectory_time_length_);
+        std::vector<cav_msgs::TrajectoryPlanPoint> tmp_trajectory = create_uneven_trajectory_from_waypoints(partial_waypoints);
+        std::vector<cav_msgs::TrajectoryPlanPoint> final_trajectory = post_process_traj_points(tmp_trajectory);
+        return final_trajectory;
+    }
+
+    std::vector<cav_msgs::TrajectoryPlanPoint> PlatooningTacticalPlugin::create_uneven_trajectory_from_waypoints(std::vector<autoware_msgs::Waypoint> waypoints)
+    {
+        std::vector<cav_msgs::TrajectoryPlanPoint> uneven_traj;
+        // TODO land id is not populated because we are not using it in Autoware
+        // Adding current vehicle location as the first trajectory point if it is not on the first waypoint
+        if(fabs(pose_msg_->pose.position.x - waypoints[0].pose.pose.position.x) > 0.1 || fabs(pose_msg_->pose.position.y - waypoints[0].pose.pose.position.y) > 0.1)
+        {
+            cav_msgs::TrajectoryPlanPoint starting_point;
+            starting_point.target_time = 0.0;
+            starting_point.x = pose_msg_->pose.position.x;
+            starting_point.y = pose_msg_->pose.position.y;
+            uneven_traj.push_back(starting_point);
+        }
+
+        double previous_wp_v = waypoints[0].twist.twist.linear.x;
+        double previous_wp_x = pose_msg_->pose.position.x;
+        double previous_wp_y = pose_msg_->pose.position.y;
+        double previous_wp_t = 0.0;
+        for(int i = 0; i < waypoints.size(); ++i)
+        {
+            if(i != 0)
+            {
+                previous_wp_v = waypoints[i - 1].twist.twist.linear.x;
+                previous_wp_x = uneven_traj.back().x;
+                previous_wp_y = uneven_traj.back().y;
+                previous_wp_t = uneven_traj.back().target_time;
+            }
+            if(i == 0 && uneven_traj.size() == 0)
+            {
+                cav_msgs::TrajectoryPlanPoint starting_point;
+                starting_point.target_time = 0.0;
+                starting_point.x = waypoints[i].pose.pose.position.x;
+                starting_point.y = waypoints[i].pose.pose.position.y;
+                uneven_traj.push_back(starting_point);
+                continue;
+            }
+            cav_msgs::TrajectoryPlanPoint traj_point;
+            // assume the vehicle is starting from stationary state because it is the same assumption made by pure pursuit wrapper node
+            double average_speed = previous_wp_v;
+            double delta_d = sqrt(pow(waypoints[i].pose.pose.position.x - previous_wp_x, 2) + pow(waypoints[i].pose.pose.position.y - previous_wp_y, 2));
+            traj_point.target_time = (delta_d / average_speed) * 1e9 + previous_wp_t;
+            traj_point.x = waypoints[i].pose.pose.position.x;
+            traj_point.y = waypoints[i].pose.pose.position.y;
+            uneven_traj.push_back(traj_point);
+        }
+
+        return uneven_traj;
+    }
+
+    std::vector<autoware_msgs::Waypoint> PlatooningTacticalPlugin::get_waypoints_in_time_boundary(std::vector<autoware_msgs::Waypoint> waypoints, double time_span)
+    {
+        std::vector<autoware_msgs::Waypoint> sublist;
+        double total_time = 0.0;
+        for(int i = 0; i < waypoints.size(); ++i)
+        {
+            sublist.push_back(waypoints[i]);
+            if(i == 0)
+            {
+                continue;
+            }
+            double delta_x_square = pow(waypoints[i].pose.pose.position.x - waypoints[i - 1].pose.pose.position.x, 2);
+            double delta_y_square = pow(waypoints[i].pose.pose.position.y - waypoints[i - 1].pose.pose.position.y, 2);
+            // Here we ignore z attribute because it is not used by Autoware
+            
+            double delta_d = sqrt(delta_x_square + delta_y_square);
+            double average_v = 0.5 * (waypoints[i].twist.twist.linear.x + waypoints[i - 1].twist.twist.linear.x);
+            double delta_t = delta_d / average_v;
+            total_time += delta_t;
+            if(total_time >= time_span)
+            {
+                break;
+            }
+        }
+        return sublist;
+    }
+
+    std::vector<cav_msgs::TrajectoryPlanPoint> PlatooningTacticalPlugin::post_process_traj_points(std::vector<cav_msgs::TrajectoryPlanPoint> trajectory)
+    {
+        uint64_t current_nsec = ros::Time::now().toNSec();
+        for(int i = 0; i < trajectory.size(); ++i)
+        {
+            trajectory[i].controller_plugin_name = "mpc_follower";
+            trajectory[i].planner_plugin_name = "platooning_tactical_plugin";
+            trajectory[i].target_time += current_nsec;
+        }
+
+        return trajectory;
+    }
+
+    void PlatooningTacticalPlugin::pose_cb(const geometry_msgs::PoseStampedConstPtr& msg)
+    {
+        pose_msg_ = msg;
+    }
+
+    void PlatooningTacticalPlugin::twist_cd(const geometry_msgs::TwistStampedConstPtr& msg)
+    {
+        current_speed_ = msg->twist.linear.x;
+    }
 
 }
