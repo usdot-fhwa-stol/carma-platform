5616f9c2692863ed131bfcc6ee5e5a98f97722a7
mishkamn <bayartsem@gmail.com>
2020-06-12 19:38:04 +0000

    made small name change

--- carma_wm_ctrl/include/carma_wm_ctrl/WMBroadcaster.h
+++ carma_wm_ctrl/include/carma_wm_ctrl/WMBroadcaster.h
@@ -104,7 +104,7 @@ public:
 
 private:
   void addSpeedLimit(std::shared_ptr<Geofence> gf_ptr);
-  const std::shared_ptr<Geofence> geofenceFromMsg(const cav_msgs::ControlMessage& geofence_msg);
+  std::shared_ptr<Geofence> geofenceFromMsg(const cav_msgs::ControlMessage& geofence_msg) const;
   lanelet::LaneletMapPtr base_map_;
   lanelet::LaneletMapPtr current_map_;
   std::vector<lanelet::LaneletMapPtr> cached_maps_;
