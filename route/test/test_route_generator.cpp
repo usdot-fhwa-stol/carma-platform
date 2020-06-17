6e2bd408dbd1ea525b4c68690d0000b3d43ce4f3
Michael McConnell <Michael.McConnell-2@leidos.com>
2020-06-17 06:59:47 -0700

    Updated unit test to use the lanelet2_extension libraries for the DigitalSpeedLimit regulatory element, and changed the test to use lanelet IDs instead of starting/ending points because that was not working correctly

--- route/test/test_route_generator.cpp
+++ route/test/test_route_generator.cpp
@@ -25,29 +25,79 @@
 #include <lanelet2_core/Attribute.h>
 #include <lanelet2_core/geometry/LineString.h>
 #include <lanelet2_core/primitives/Traits.h>
+#include <lanelet2_extension/traffic_rules/CarmaUSTrafficRules.h>
+#include <lanelet2_extension/utility/query.h>
+#include <lanelet2_extension/utility/utilities.h>
 #include <lanelet2_extension/projection/local_frame_projector.h>
+#include <lanelet2_extension/io/autoware_osm_parser.h>
+
 #include <gtest/gtest.h>
 #include <ros/ros.h>
+
+/*
+Using this file:
+    1) Libraries you should have: carma-base, carma-config, carma-msgs (integration/routing branch), 
+        carma-platform (integration/routing branch), carma-utils, caram-web-ui, opendrive2lanelet.
+    2) Then the test can be built with the command: 
+        ./carma_build -c /workspaces/carma_ws/carma/ -a /workspaces/carma_ws/autoware.ai/ -x -m "--only-pkg-with-deps route"
+    3) Update the osm file location and starting/ending IDs to match the file you want to test
+    4) Run the unit test with
+        catkin_make run_tests_route
+    5) Confirm that the test passed and that the list of lanelet IDs does traverse from the start to the end
+*/
 
 TEST(RouteGeneratorTest, testLaneletRouting)
 {
     tf2_ros::Buffer tf_buffer;
     carma_wm::WorldModelConstPtr wm;
     route::RouteGeneratorWorker worker(tf_buffer);
-    //lanelet::LaneletMapPtr map = lanelet::load("/home/qswawrq/Desktop/ATEF_pretty.osm",
-    //    lanelet::projection::LocalFrameProjector("+proj=tmerc +lat_0=39.46636844371259 +lon_0=-76.16919523566943 +k=1 +x_0=0 +y_0=0 +units=m +vunits=m"));
-    lanelet::LaneletMapPtr map = lanelet::load("/home/qswawrq/Desktop/ATEF_pretty.osm", lanelet::Origin({0, 0}));
+
+    int projector_type = 0;
+    std::string target_frame;
+    lanelet::ErrorMessages load_errors;
+
+    // If the output is an error about the geoReference field in the osm file, here is a correct example. If the lat/lon coordinates are already correct, simply add:
+    // <geoReference>+proj=tmerc +lat_0=0 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +geoidgrids=egm96_15.gtx +vunits=m +no_defs</geoReference>
+
+    // File location of osm file
+    std::string file = "/workspaces/carma_ws/carma/src/Town02_pretty.osm";
+    // Starting and ending lanelet IDs. It's easiest to grab these from JOSM
+    lanelet::Id start_id = 111;
+    lanelet::Id end_id = 144;
+
+    // The parsing in this file was copied from https://github.com/usdot-fhwa-stol/carma-platform/blob/develop/carma_wm_ctrl/test/MapToolsTest.cpp
+    lanelet::io_handlers::AutowareOsmParser::parseMapParams(file, &projector_type, &target_frame);
+    lanelet::projection::LocalFrameProjector local_projector(target_frame.c_str());
+    lanelet::LaneletMapPtr map = lanelet::load(file, local_projector, &load_errors);
+
+    // Grabs lanelet elements from the start and end IDs. Fails the unit test if there is no lanelet with the matching ID
+    lanelet::Lanelet start_lanelet;
+    lanelet::Lanelet end_lanelet;
+    try
+    {
+        start_lanelet = map->laneletLayer.get(start_id);
+    }
+    catch (const lanelet::NoSuchPrimitiveError& e)
+    {
+        FAIL() << "The specified starting lanelet Id of " << start_id << " does not exist in the provided map.";
+    }
+    try
+    {
+        end_lanelet = map->laneletLayer.get(end_id);
+    }
+    catch (const lanelet::NoSuchPrimitiveError& e)
+    {
+        FAIL() << "The specified ending lanelet Id of " << end_id << " does not exist in the provided map.";
+    }
+
     lanelet::LaneletMapConstPtr const_map(map);
-    lanelet::BasicPoint2d start(-1056.75, -70.6);
-    std::vector<lanelet::BasicPoint2d> via;
-    // via.emplace_back(lanelet::BasicPoint2d(-158.0, 533.5));
-    //via.emplace_back(lanelet::BasicPoint2d(-17.5, 322.0));
-    lanelet::BasicPoint2d end(1052.35, 771.24);
     lanelet::traffic_rules::TrafficRulesUPtr traffic_rules = lanelet::traffic_rules::TrafficRulesFactory::create(lanelet::Locations::Germany, lanelet::Participants::VehicleCar);
     lanelet::routing::RoutingGraphUPtr map_graph = lanelet::routing::RoutingGraph::build(*map, *traffic_rules);
     // Output graph for debugging
-    map_graph->exportGraphViz("/home/qswawrq/Desktop/routing.txt");
-    auto route = worker.routing(start, via, end, const_map, std::move(map_graph));
+    map_graph->exportGraphViz("/workspaces/carma_ws/carma/src/routing.txt");
+
+    // Computes the shortest path and prints the list of lanelet IDs to get from the start to the end. Can be manually confirmed in JOSM
+    auto route = worker.routing_from_lanelets(start_lanelet, end_lanelet, const_map, std::move(map_graph));
     if(!route) {
         ASSERT_FALSE(true);
     } else {
