03af39b58f716775b3ed00a7ce496e15bfbb0ab4
itabrl <samir.manafzadehtabriz@leidos.com>
2020-06-22 10:56:24 -0400

    Update collision detection with tests

--- carma_wm/include/carma_wm/collision_detection.h
+++ carma_wm/include/carma_wm/collision_detection.h
@@ -13,44 +13,49 @@
 #include <iostream>
 #include <fstream>
 
+#include <carma_wm/Geometry.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 
 namespace carma_wm {
 
     namespace collision_detection {
 
-        namespace bg = boost::geometry;
         typedef boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian> point_t;
         typedef boost::geometry::model::polygon<point_t> polygon_t;
 
         struct MovingObject {
             polygon_t object_polygon;
             geometry_msgs::Vector3 linear_velocity;
+            // std::vector<geometry_msgs::Pose> future_poses;
+            std::vector<polygon_t> future_polygons;
         };
 
-        template <typename point_t>
-        class ApplySpeed {
-            private :
-                geometry_msgs::Vector3 linear_velocity;
-                double time;
+        // template <typename point>
+        // class ApplySpeed {
+        //     private :
+        //         geometry_msgs::Vector3 linear_velocity;
+        //         double time;
 
-                inline double apply_speed(double distance, double velocity) {
-                    return distance + (velocity * time);
-                }
+        //         inline double apply_speed(double distance, double velocity) {
+        //             return distance + (velocity * time);
+        //         }
 
-            public :
-                ApplySpeed (geometry_msgs::Vector3 lv, double t) {
-                    linear_velocity = lv;
-                    time = t;
-                }
+        //     public :
+        //         ApplySpeed (geometry_msgs::Vector3 lv, double t) {
+        //             linear_velocity = lv;
+        //             time = t;
+        //         }
 
-                inline void operator()(point_t& p)
-                {
-                    using boost::geometry::get;
-                    using boost::geometry::set;
-                    set<0>(p, apply_speed(get<0>(p), linear_velocity.x));
-                    set<1>(p, apply_speed(get<1>(p), linear_velocity.y));
-                }
-        };
+        //         inline void operator()(point& p)
+        //         {
+        //             using boost::geometry::get;
+        //             using boost::geometry::set;
+        //             set<0>(p, apply_speed(get<0>(p), linear_velocity.x));
+        //             set<1>(p, apply_speed(get<1>(p), linear_velocity.y));
+        //         }
+        // };
 
 
         /*!
@@ -61,19 +66,13 @@ namespace carma_wm {
             public:
                 std::vector<cav_msgs::RoadwayObstacle> WorldCollisionDetection(cav_msgs::RoadwayObstacleList rwol, cav_msgs::TrajectoryPlan tp, geometry_msgs::Vector3& size, geometry_msgs::Twist veloctiy, int target_time);
                 collision_detection::MovingObject ConvertRoadwayObstacleToMovingObject(cav_msgs::RoadwayObstacle rwo);
-                collision_detection::MovingObject ConvertVehicleToMovingObject(cav_msgs::TrajectoryPlan tp, geometry_msgs::Vector3& size, geometry_msgs::Twist veloctiy);
-
-                /*!
-                * Destructor.
-                */
-                virtual ~CollisionChecking();
-                
-
-            private:
+                collision_detection::MovingObject ConvertVehicleToMovingObject(cav_msgs::TrajectoryPlan tp, geometry_msgs::Vector3 size, geometry_msgs::Twist veloctiy);                
 
                 collision_detection::MovingObject PredictObjectPosition(collision_detection::MovingObject op, int target_time);
                 bool CheckPolygonIntersection(collision_detection::MovingObject ob1, collision_detection::MovingObject ob2);
                 bool DetectCollision(collision_detection::MovingObject ob1, collision_detection::MovingObject ob2, int target_time);
+                template <class P>
+                P ObjectToBoostPolygon(const geometry_msgs::Pose& pose, const geometry_msgs::Vector3& size);
 
         };
     }
--- carma_wm/src/collision_detection.cpp
+++ carma_wm/src/collision_detection.cpp
@@ -1,4 +1,4 @@
-#include "collision_detection.h"
+#include "carma_wm/collision_detection.h"
 
 namespace carma_wm {
 
@@ -6,14 +6,26 @@ namespace carma_wm {
 
         std::vector<cav_msgs::RoadwayObstacle> CollisionChecking::WorldCollisionDetection(cav_msgs::RoadwayObstacleList rwol, cav_msgs::TrajectoryPlan tp, geometry_msgs::Vector3& size, geometry_msgs::Twist veloctiy, int target_time){
 
+            std::cout << "start function" << std::endl;
+
             std::vector<cav_msgs::RoadwayObstacle> rwo_collison;
 
             collision_detection::MovingObject vehicle_object = CollisionChecking::ConvertVehicleToMovingObject(tp, size, veloctiy);
 
-            for (int i = 0; i <= rwol.roadway_obstacles.size(); i++) {
+            std::cout << "after ConvertVehicleToMovingObjects" << rwol.roadway_obstacles.size() << std::endl;
+
+            for (int i = 0; i < rwol.roadway_obstacles.size(); i++) {
+
+                std::cout << "start ConvertRoadwayObstacleToMovingObject" << std::endl;
+
                 collision_detection::MovingObject rwo = CollisionChecking::ConvertRoadwayObstacleToMovingObject(rwol.roadway_obstacles[i]);
+                std::cout << "after ConvertRoadwayObstacleToMovingObject" << std::endl;
+
                 bool collision = CollisionChecking::DetectCollision(vehicle_object, rwo, target_time);
+                std::cout << "after DetectCollision" << std::endl;
+
                 if(collision) {
+                    std::cout << "after rwo_collison" << std::endl;
                     rwo_collison.push_back(rwol.roadway_obstacles[i]);
                 }
             }
@@ -24,32 +36,53 @@ namespace carma_wm {
         collision_detection::MovingObject CollisionChecking::ConvertRoadwayObstacleToMovingObject(cav_msgs::RoadwayObstacle rwo){
 
             collision_detection::MovingObject ro;
-            const geometry_msgs::Pose& pose;
-            pose.position.x = rwo.object.pose.pose.position.x;
-            pose.position.y = rwo.object.pose.pose.position.y;
-            geometry_msgs::Vector3& size = rwo.object.size;
             
-            ro.object_polygon = carma_wm::geometry::objectToMapPolygon(pose, size);;
+            ro.object_polygon = CollisionChecking::ObjectToBoostPolygon<polygon_t>(rwo.object.pose.pose, rwo.object.size);
+
+            for (auto i : rwo.object.predictions){
+                ro.future_polygons.push_back(CollisionChecking::ObjectToBoostPolygon<polygon_t>(i.predicted_position, rwo.object.size));
+                // ro.future_poses.push_back(i.predicted_position);
+            }
+
             ro.linear_velocity = rwo.object.velocity.twist.linear;
 
             return ro;
         };
 
-        collision_detection::MovingObject CollisionChecking::ConvertVehicleToMovingObject(cav_msgs::TrajectoryPlan tp, geometry_msgs::Vector3& size, geometry_msgs::Twist veloctiy){
+        collision_detection::MovingObject CollisionChecking::ConvertVehicleToMovingObject(cav_msgs::TrajectoryPlan tp, geometry_msgs::Vector3 size, geometry_msgs::Twist veloctiy){
             
             collision_detection::MovingObject v;
 
-            const geometry_msgs::Pose& pose;
+            geometry_msgs::Pose pose;
             pose.position.x = tp.trajectory_points[0].x;
             pose.position.y = tp.trajectory_points[0].y;
 
-            v.object_polygon = carma_wm::geometry::objectToMapPolygon(pose, size);
-            v.linear_velocity = velocity.twist.linear;
+            tf2::Quaternion tf_orientation;
+            tf_orientation.setRPY(0, 0, 1.5708);
+
+            pose.orientation.x = tf_orientation.getX();
+            pose.orientation.y = tf_orientation.getY();
+            pose.orientation.z = tf_orientation.getZ();
+            pose.orientation.w = tf_orientation.getW();
+
+
+            v.object_polygon = CollisionChecking::ObjectToBoostPolygon<polygon_t>(pose, size);
+            v.linear_velocity = veloctiy.linear;
+
+            for (auto i : tp.trajectory_points){
+                geometry_msgs::Pose pose;
+                pose.position.x = i.x;
+                pose.position.y = i.y;
+                // v.future_poses.push_back(pose);
+                v.future_polygons.push_back(CollisionChecking::ObjectToBoostPolygon<polygon_t>(pose, size));
+            }
 
             return v;
         };
 
         bool CollisionChecking::DetectCollision(collision_detection::MovingObject ob_1, collision_detection::MovingObject ob_2, int target_time) {            
+            std::cout << "DetectCollision" << boost::geometry::wkt(ob_1.object_polygon) << std::endl;
+            std::cout << "DetectCollision" << boost::geometry::wkt(ob_2.object_polygon) << std::endl;
 
             collision_detection::MovingObject ob_1_after = CollisionChecking::PredictObjectPosition(ob_1,target_time);
             collision_detection::MovingObject ob_2_after = CollisionChecking::PredictObjectPosition(ob_2,target_time);
@@ -61,9 +94,13 @@ namespace carma_wm {
             return false;
         };
 
-        bool CollisionChecking::CheckPolygonIntersection(collision_detection::MovingObject ob_1, collision_detection::MovingObject ob_2) {            
+        bool CollisionChecking::CheckPolygonIntersection(collision_detection::MovingObject ob_1, collision_detection::MovingObject ob_2) {    
+
+
 
                 std::deque<polygon_t> output;
+                std::cout << "CheckPolygonIntersection" << boost::geometry::wkt(ob_1.object_polygon) << std::endl;
+                std::cout << "CheckPolygonIntersection" << boost::geometry::wkt(ob_2.object_polygon) << std::endl;
 
                 boost::geometry::intersection(ob_1.object_polygon, ob_2.object_polygon, output); 
 
@@ -79,15 +116,18 @@ namespace carma_wm {
 
         collision_detection::MovingObject CollisionChecking::PredictObjectPosition(collision_detection::MovingObject op, int target_time){
 
-            std::vector<point_t> orignial_polygon_points = op.object_polygon.outer();
-            boost::geometry::for_each_point(op.object_polygon, ApplySpeed<point_t>(op.linear_velocity, target_time));
+            int size = 0;
+            for(int i = 0; i< target_time; i++){
+                size = size + op.future_polygons[i].outer().size();
+            }
 
-            std::vector<point_t> new_polygon_points = op.object_polygon.outer();
             std::vector<point_t> unioin_polygon_points;
 
-            unioin_polygon_points.reserve( orignial_polygon_points.size() + new_polygon_points.size() ); 
-            unioin_polygon_points.insert( unioin_polygon_points.end(), orignial_polygon_points.begin(), orignial_polygon_points.end() );
-            unioin_polygon_points.insert( unioin_polygon_points.end(), new_polygon_points.begin(), new_polygon_points.end() );
+            unioin_polygon_points.reserve(size); 
+
+            for(int i = 0; i< target_time; i++){
+                unioin_polygon_points.insert( unioin_polygon_points.end(), op.future_polygons[i].outer().begin(), op.future_polygons[i].outer().end());
+            }
 
             polygon_t union_polygon;  
             boost::geometry::assign_points(union_polygon, unioin_polygon_points);
@@ -96,8 +136,47 @@ namespace carma_wm {
             boost::geometry::convex_hull(union_polygon, hull_polygon);
 
             collision_detection::MovingObject output_object = {hull_polygon, op.linear_velocity};
+            
+            
+            std::cout << "PredictObjectPosition" << boost::geometry::wkt(op.future_polygons[0]) << std::endl;
 
             return output_object;
         };
+
+        template <class P>
+        P CollisionChecking::ObjectToBoostPolygon(const geometry_msgs::Pose& pose, const geometry_msgs::Vector3& size) {
+
+            tf2::Transform object_tf;
+            tf2::fromMsg(pose, object_tf);
+
+            double half_x_bound = size.x / 2;
+            double half_y_bound = size.y / 2;
+
+            // 4 corners of the object starting with upper left and moving in clockwise direction in pose frame
+            tf2::Vector3 obj_p1(half_x_bound, half_y_bound, 0);
+            tf2::Vector3 obj_p2(half_x_bound, -half_y_bound, 0);
+            tf2::Vector3 obj_p3(-half_x_bound, -half_y_bound, 0);
+            tf2::Vector3 obj_p4(-half_x_bound, half_y_bound, 0);
+
+            tf2::Vector3 obj_p1_map = object_tf * obj_p1;
+            tf2::Vector3 obj_p2_map = object_tf * obj_p2;
+            tf2::Vector3 obj_p3_map = object_tf * obj_p3;
+            tf2::Vector3 obj_p4_map = object_tf * obj_p4;
+
+            point_t p1(obj_p1_map.getX(), obj_p1_map.getY());
+            point_t p2(obj_p2_map.getX(), obj_p2_map.getY());
+            point_t p3(obj_p3_map.getX(), obj_p3_map.getY());
+            point_t p4(obj_p4_map.getX(), obj_p4_map.getY());
+
+
+            P p;
+            p.outer().push_back(p1);
+            p.outer().push_back(p2);
+            p.outer().push_back(p3);
+            p.outer().push_back(p4);
+
+            return p;
+        }
+
     }
 }
