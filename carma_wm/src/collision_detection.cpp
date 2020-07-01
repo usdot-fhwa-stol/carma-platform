b9a344f9f385acaea732c79c1342afb4bd355e5d
itabrl <samir.manafzadehtabriz@leidos.com>
2020-07-01 16:53:11 -0400

    update smell code

--- carma_wm/src/collision_detection.cpp
+++ carma_wm/src/collision_detection.cpp
@@ -38,7 +38,7 @@ namespace carma_wm {
             return ro;
         };
 
-        collision_detection::MovingObject ConvertVehicleToMovingObject(cav_msgs::TrajectoryPlan& tp, const geometry_msgs::Vector3& size, geometry_msgs::Twist& veloctiy){
+        collision_detection::MovingObject ConvertVehicleToMovingObject(const cav_msgs::TrajectoryPlan& tp, const geometry_msgs::Vector3& size, const geometry_msgs::Twist& veloctiy){
             
             collision_detection::MovingObject v;
 
