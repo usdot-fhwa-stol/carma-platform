67aeba289a2eb87268c973e536b93021ac59629e
dan-du-car <dan.du@leidos.com>
2020-08-20 11:37:09 -0700

    update test for route package and remove route_generator package

--- route/test/test_route_generator.cpp
+++ route/test/test_route_generator.cpp
@@ -60,7 +60,7 @@ TEST(RouteGeneratorTest, testLaneletRouting)
     // <geoReference>+proj=tmerc +lat_0=0 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +geoidgrids=egm96_15.gtx +vunits=m +no_defs</geoReference>
 
     // File location of osm file
-    std::string file = "../resource/map/test_vector_map.osm";
+    std::string file = "../resource/map/vector_map.osm";
     // Starting and ending lanelet IDs. It's easiest to grab these from JOSM
     lanelet::Id start_id = 1346;
     lanelet::Id end_id = 1349;
@@ -73,6 +73,7 @@ TEST(RouteGeneratorTest, testLaneletRouting)
     // Grabs lanelet elements from the start and end IDs. Fails the unit test if there is no lanelet with the matching ID
     lanelet::Lanelet start_lanelet;
     lanelet::Lanelet end_lanelet;
+
     try {
         start_lanelet = map->laneletLayer.get(start_id);
     }
@@ -96,6 +97,7 @@ TEST(RouteGeneratorTest, testLaneletRouting)
     auto route = map_graph->getRoute(start_lanelet, end_lanelet);
     if(!route) {
         ASSERT_FALSE(true);
+        std::cout << "Route not generated." << " ";
     } else {
         std::cout << "shortest path: \n";
         for(const auto& ll : route.get().shortestPath()) {
@@ -110,17 +112,24 @@ TEST(RouteGeneratorTest, testReadRouteFile)
     tf2_ros::Buffer tf_buffer;
     carma_wm::WorldModelConstPtr wm;
     route::RouteGeneratorWorker worker(tf_buffer);
-    worker.set_route_file_path("../../src/route/resource/route/");
+    worker.set_route_file_path("../resource/route/");
     cav_srvs::GetAvailableRoutesRequest req;
     cav_srvs::GetAvailableRoutesResponse resp;
     ASSERT_TRUE(worker.get_available_route_cb(req, resp));
+    std::cout << "route id : " << resp.availableRoutes.front().route_id <<"\n";
     ASSERT_EQ("tfhrc_test_route", resp.availableRoutes.front().route_id);
+    std::cout <<"C-HUB : " << resp.availableRoutes.front().route_name << "\n";
     ASSERT_EQ("C-HUB", resp.availableRoutes.front().route_name);
+    std::cout << "Available Route : " << resp.availableRoutes.size() << "\n";
     ASSERT_EQ(1, resp.availableRoutes.size());
     auto points = worker.load_route_destinations_in_ecef("tfhrc_test_route");
+    std::cout << "Point Size : " << points.size()<<"\n";
     ASSERT_EQ(5, points.size());
+    std::cout << "X : " << points[0].getX() << "\n";
     ASSERT_NEAR(1106580, points[0].getX(), 5.0);
+    std::cout << "Y : " << points[0].getY() << "\n";
     ASSERT_NEAR(894697, points[0].getY(), 5.0);
+    std::cout << "Z : " << points[0].getZ() << "\n";
     ASSERT_NEAR(-6196590, points[0].getZ(), 5.0);
 }
 
