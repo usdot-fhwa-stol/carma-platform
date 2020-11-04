598bf3a36cd43edc150eddc2ae5593187b354131
Misheel Bayartsengel <misheel.bayart@gmail.com>
2020-11-04 14:47:12 -0500

    Update drivers_ready state

--- lightbar_manager/src/lightbar_manager/lightbar_manager_sm.cpp
+++ lightbar_manager/src/lightbar_manager/lightbar_manager_sm.cpp
@@ -68,12 +68,12 @@ void LightBarManagerStateMachine::handleStateChange(const cav_msgs::GuidanceStat
     switch (msg_ptr->state)
     {
         case cav_msgs::GuidanceState::STARTUP:
-        case cav_msgs::GuidanceState::DRIVERS_READY:
         case cav_msgs::GuidanceState::SHUTDOWN:
         onDisengage();
         break;
         case cav_msgs::GuidanceState::ACTIVE:
         case cav_msgs::GuidanceState::INACTIVE:
+        case cav_msgs::GuidanceState::DRIVERS_READY:
         onActive();
         break;
         case cav_msgs::GuidanceState::ENGAGED:
