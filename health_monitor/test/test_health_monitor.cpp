e3addb1179696de62942e337171df062655c8087
Isaiah <isaiah.d.lewis@leidos.com>
2020-11-05 14:14:41 +0000

    Commit PR changes

---
+++ health_monitor/test/test_health_monitor.cpp
@@ -0,0 +1,50 @@
+#include <carma_utils/CARMAUtils.h>
+#include <cav_srvs/PluginList.h>
+#include <cav_srvs/PluginActivation.h>
+#include <cav_msgs/Plugin.h>
+#include <cav_msgs/DriverStatus.h>
+#include "health_monitor.h"
+#include "driver_manager.h"
+#include <gtest/gtest.h>
+
+namespace health_monitor
+{
+    
+
+TEST(HealthMonitorTest, runTest)
+{
+   /* health_monitor::HealthMonitor node;
+    bool publish_status_check;
+
+    node.setIsPublishedFalse();
+    node.run();*/
+    
+
+
+}
+
+
+TEST(HealthMonitorTest, setIsPublishedFalseTest)
+{
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
