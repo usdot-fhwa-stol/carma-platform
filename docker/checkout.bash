52c7bdb1b31a3b8b083fdb2ff29b07220c19d5d8
mishkamn <bayartsem@gmail.com>
2020-11-23 17:21:06 +0000

    fix typo

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -45,6 +45,6 @@ if [[ "$BRANCH" = "develop" ]]; then
 else
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch develop
       git clone --depth=1 https://github.com/usdot-fhwa-stol/novatel_gps_driver.git --branch CARMANovatelGpsDriver_1.2.0
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch --branch develop
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch develop
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch develop
 fi
