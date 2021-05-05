af6eb8877ff2f78cab05b747a364b2b31edbf02d
Isaiah <isaiah.d.lewis@leidos.com>
2021-05-05 14:29:51 +0000

    Commit change to ConrtolRequestTimer

--- carma_wm_ctrl/src/WMBroadcasterNode.cpp
+++ carma_wm_ctrl/src/WMBroadcasterNode.cpp
@@ -80,7 +80,8 @@ int WMBroadcasterNode::run()
   pnh2_.getParam("/config_speed_limit", config_limit);
   wmb_.setConfigSpeedLimit(config_limit);
 
-  timer = cnh_.createTimer(ros::Duration(10.0), [this](auto){wmb_.routeCallbackMessage(wmb_.getRoute());}, &wmb_);
+  if(wmb_.getRoute().route_path_lanelet_ids.size() > 0)
+    timer = cnh_.createTimer(ros::Duration(10.0), [this](auto){wmb_.routeCallbackMessage(wmb_.getRoute());}, &wmb_);
  
   // Spin
   cnh_.spin();
