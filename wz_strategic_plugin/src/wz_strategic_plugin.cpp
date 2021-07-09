1b97c39aa49c35a048ad5a8e22fb53e6427ff888
itabrl <samir.manafzadehtabriz@leidos.com>
2021-07-08 21:29:21 -0500

    Update

--- wz_strategic_plugin/src/wz_strategic_plugin.cpp
+++ wz_strategic_plugin/src/wz_strategic_plugin.cpp
@@ -67,9 +67,124 @@ namespace wz_strategic_plugin
 
     bool WzStrategicPlugin::planManeuverCb(cav_srvs::PlanManeuversRequest &req, cav_srvs::PlanManeuversResponse &resp)
     {
+
+        // find car_down_track
+        //
+        //
+
+        double current_car_down_track = 0;
+
+        // find traffic_light_down_track
+        //
+        //
+
+        double traffic_light_down_track = 0;
+
+        double distance_remaining_to_traffic_light  = traffic_light_down_track - current_car_down_track;
+        double time_remaining_to_traffic_light = distance_remaining_to_traffic_light / current_speed_;
+
+        // get traffic light timeming 
+
+        double traffic_light_next_state = 0 // 0 is red 
+                                            // 1 is green
+        
+
+
+        if(time_remaining_to_traffic_light > current_traffic_light_state_remaining_time) {
+
+            if(traffic_light_next_state == "red") {
+
+                // stop_and_wait
+                composeStopandWaitManeuverMessage( current_car_down_track, traffic_light_down_track, current_speed_, start_lane_id, end_lane_id, current_time, end_time)
+
+                // workzone_tactical
+
+            }
+
+            if(traffic_light_next_state == "green") {
+
+                // LaneFollowing
+                composeLaneFollowingManeuverMessage(current_car_down_track, traffic_light_down_track, current_speed_, current_speed_, lane_id)
+
+                // workzone_tactical
+
+            }
+
+        }
+
+        if(time_remaining_to_traffic_light < current_traffic_light_state_remaining_time) {
+
+            if(traffic_light_current_state == "red") {
+
+
+
+                // stop_and_wait
+                composeStopandWaitManeuverMessage( current_car_down_track, traffic_light_down_track, current_speed_, start_lane_id, end_lane_id, current_time, end_time)
+                // workzone_tactical
+
+            }
+
+            if(traffic_light_current_state == "green") {
+
+                // LaneFollowing
+                composeLaneFollowingManeuverMessage(current_car_down_track, traffic_light_down_track, current_speed_, current_speed_, lane_id)
+                // workzone_tactical
+
+            }
+
+        }
+        
         return true;
     }
 
+    cav_msgs::Maneuver WzStrategicPlugin::composeLaneFollowingManeuverMessage(double start_dist, double end_dist, double start_speed, double target_speed, lanelet::Id lane_id) const
+    {
+        cav_msgs::Maneuver maneuver_msg;
+        maneuver_msg.type = cav_msgs::Maneuver::LANE_FOLLOWING;
+        maneuver_msg.lane_following_maneuver.parameters.neogition_type = cav_msgs::ManeuverParameters::NO_NEGOTIATION;
+        maneuver_msg.lane_following_maneuver.parameters.presence_vector = cav_msgs::ManeuverParameters::HAS_TACTICAL_PLUGIN;
+        maneuver_msg.lane_following_maneuver.parameters.planning_tactical_plugin = lanefollow_planning_tactical_plugin_;
+        maneuver_msg.lane_following_maneuver.parameters.planning_strategic_plugin = planning_strategic_plugin_;
+        maneuver_msg.lane_following_maneuver.start_dist = start_dist;
+        maneuver_msg.lane_following_maneuver.start_speed = start_speed;
+        maneuver_msg.lane_following_maneuver.end_dist = end_dist;
+        maneuver_msg.lane_following_maneuver.end_speed = target_speed;
+        maneuver_msg.lane_following_maneuver.lane_id = std::to_string(lane_id);
+        //Start time and end time for maneuver are assigned in updateTimeProgress
+
+        ROS_INFO_STREAM("Creating lane follow start dist: " << start_dist << " end dist: " << end_dist);
+        
+        return maneuver_msg;
+    }
+
+    cav_msgs::Maneuver RouteFollowingPlugin::composeStopandWaitManeuverMessage(double current_dist, double end_dist, double current_speed, int start_lane_id, int end_lane_id, ros::Time current_time, double end_time)
+    {
+        cav_msgs::Maneuver maneuver_msg;
+        maneuver_msg.type = cav_msgs::Maneuver::STOP_AND_WAIT;
+        maneuver_msg.stop_and_wait_maneuver.parameters.neogition_type = cav_msgs::ManeuverParameters::NO_NEGOTIATION;
+        maneuver_msg.stop_and_wait_maneuver.parameters.presence_vector = cav_msgs::ManeuverParameters::HAS_TACTICAL_PLUGIN;
+        maneuver_msg.stop_and_wait_maneuver.parameters.planning_tactical_plugin = "StopandWaitPlugin";
+        maneuver_msg.stop_and_wait_maneuver.parameters.planning_strategic_plugin = planning_strategic_plugin_;
+        maneuver_msg.stop_and_wait_maneuver.start_dist = current_dist;
+        maneuver_msg.stop_and_wait_maneuver.start_speed = current_speed;
+        maneuver_msg.stop_and_wait_maneuver.end_dist = end_dist;
+        maneuver_msg.stop_and_wait_maneuver.start_time = current_time;
+        maneuver_msg.stop_and_wait_maneuver.starting_lane_id = std::to_string(start_lane_id);
+        maneuver_msg.stop_and_wait_maneuver.ending_lane_id = std::to_string(end_lane_id);
+        if(end_time < mvr_duration_){
+            end_time = mvr_duration_;
+        }
+
+        maneuver_msg.stop_and_wait_maneuver.end_time = current_time + ros::Duration(end_time);
+
+        ROS_INFO_STREAM("Creating lane follow start dist: " << current_dist << " end dist: " << end_dist);
+
+        return maneuver_msg;
+    }
+
+    cav_msgs::Maneuver WzStrategicPlugin::composeWorkZoneManeuverMessage(double start_dist, double end_dist, double start_speed, double target_speed, lanelet::Id lane_id) const
+    {
+    }
 
     void WzStrategicPlugin::run()
     {
