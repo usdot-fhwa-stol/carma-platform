060f912fdac513a35d03df07e2b9f6d091c0fa80
Misheel Bayartsengel <misheel.bayart@gmail.com>
2021-07-12 13:56:29 -0400

    pure pursuit traj topic fixed

--- trajectory_executor/src/trajectory_executor/trajectory_executor.cpp
+++ trajectory_executor/src/trajectory_executor/trajectory_executor.cpp
@@ -45,7 +45,7 @@ namespace trajectory_executor
 
         //Hardcoding jerk control wrapper
         std::string control_plugin1 = "default";
-        std::string control_plugin_topic1 = "/guidance/pure_pursuit/plan_jerk_trajectory";
+        std::string control_plugin_topic1 = "/guidance/pure_pursuit/plan_trajectory";
         out[control_plugin1] = control_plugin_topic1;
 
         std::string control_plugin2 = "PlatooningControlPlugin";
