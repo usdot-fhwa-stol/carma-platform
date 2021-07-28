04ac7f7049670784876f2bdd6c606c1d2b82f6bd
Michael McConnell <Michael.McConnell-2@leidos.com>
2021-07-28 17:59:08 +0000

    disable single trajectory

--- stop_and_wait_plugin/src/stop_and_wait_plugin.cpp
+++ stop_and_wait_plugin/src/stop_and_wait_plugin.cpp
@@ -102,13 +102,13 @@ bool StopandWait::plan_trajectory_cb(cav_srvs::PlanTrajectoryRequest& req, cav_s
 
   std::string maneuver_id = req.maneuver_plan.maneuvers[req.maneuver_index_to_plan].stop_and_wait_maneuver.parameters.maneuver_id;
 
-  if (planned_trajectories_.find(maneuver_id) != planned_trajectories_.end()) // Maneuver already planned
-  {
-    ROS_INFO_STREAM("Maneuver already planned returning previous trajectory");
-    resp.trajectory_plan = planned_trajectories_[maneuver_id]; // TODO_SINGLE_TRAJ
+  // if (planned_trajectories_.find(maneuver_id) != planned_trajectories_.end()) // Maneuver already planned
+  // {
+  //   ROS_INFO_STREAM("Maneuver already planned returning previous trajectory");
+  //   resp.trajectory_plan = planned_trajectories_[maneuver_id]; // TODO_SINGLE_TRAJ
 
-    return true;
-  }
+  //   return true;
+  // }
 
   ROS_INFO_STREAM("Maneuver not yet planned planning new trajectory");
 
