e45ea781abe2effba6073e30e55a370b5f69d1eb
saina-ramyar <saina.ramyar@leidos.com>
2021-08-31 11:29:46 -0400

    add tsmo case determination

--- inlanecruising_plugin/include/inlanecruising_plugin/inlanecruising_plugin.h
+++ inlanecruising_plugin/include/inlanecruising_plugin/inlanecruising_plugin.h
@@ -20,6 +20,7 @@
 #include <cav_msgs/TrajectoryPlan.h>
 #include <cav_msgs/TrajectoryPlanPoint.h>
 #include <cav_msgs/Plugin.h>
+#include <cav_msgs/MobilityOperation.h>
 #include <boost/shared_ptr.hpp>
 #include <carma_utils/CARMAUtils.h>
 #include <boost/geometry.hpp>
@@ -34,6 +35,8 @@
 #include <autoware_msgs/Lane.h>
 #include <ros/ros.h>
 #include <carma_debug_msgs/TrajectoryCurvatureSpeeds.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 namespace inlanecruising_plugin
 {
@@ -92,7 +95,78 @@ public:
    */
   bool validate_yield_plan(const cav_msgs::TrajectoryPlan& yield_plan);
 
+  /**
+   * \brief parse strategy parameters
+   * 
+   * \param strategy_params input string of strategy params from mob op msg
+   */
+  void parse_strategy_params(const std::string& strategy_params);
+
+  /**
+   * \brief calculate the time vehicle will stop with optimal decelarion
+   *
+   * \param stop_dist distance to stop line
+   *
+   * \param current_speed current speed of vehicle
+   *
+   * \return the time vehicle will stop with optimal decelarion
+   */
+  double calc_estimated_stop_time(double stop_dist, double current_speed);
+
+  /**
+   * \brief calculate the speed, right before the car starts to decelerate for stopping
+    *
+   * \param stop_time time the vehicle must stop
+   *
+   * \param stop_dist distance to stop line
+   *
+   * \param current_speed current speed of vehicle
+   *
+   * \return speed value
+   */
+  double calc_stop_speed(double stop_time, double stop_dist, double current_speed);
+
+  /**
+   * \brief Determine the speed profile case fpr approaching an intersection
+   * 
+   * \param stop_dist distance to stop line
+   *
+   * \param current_speed current speed of vehicle
+   *
+   * \param schedule_stop_time scheduled stop time
+   *
+   * \param speed_limit speed limit
+   *
+   * \return integer case number
+   */
+  int determine_case(double stop_dist, double current_speed, double schedule_stop_time, double speed_limit);
+
+  /**
+   * \brief callback function for mobility operation
+   * 
+   * \param msg input mobility operation msg
+   */
+  void mob_op_cb(const cav_msgs::MobilityOperationConstPtr& msg);
+
+
   cav_msgs::VehicleState ending_state_before_buffer_; //state before applying extra points for curvature calculation that are removed later
+  
+  // CARMA Streets Variakes
+  // timestamp for msg received from carma streets
+  uint32_t street_msg_timestamp_ = 0.0;
+  // scheduled stop time
+  uint32_t scheduled_stop_time_ = 0.0;
+  // scheduled enter time
+  uint32_t scheduled_enter_time_ = 0.0;
+  // scheduled depart time
+  uint32_t scheduled_depart_time_ = 0.0;
+  // scheduled latest depart time
+  uint32_t scheduled_latest_depart_time_ = 0.0;
+  // flag to show if the vehicle is allowed in intersection
+  bool is_allowed_int_ = false;
+
+
+
   
 private:
 
@@ -104,6 +178,10 @@ private:
   cav_msgs::Plugin plugin_discovery_msg_;
   DebugPublisher debug_publisher_;
   carma_debug_msgs::TrajectoryCurvatureSpeeds debug_msg_;
+  std::string stop_controlled_intersection_strategy_ = "Carma/stop_controlled_intersection";
+  std::string previous_strategy_params_ = "";
+  // approximate speed limit 
+  double speed_limit_ = 100.0;
 
 };
 
