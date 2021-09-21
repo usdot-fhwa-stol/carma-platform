b1d3b709a062ba39990b553c7dcf4cd78acc667e
Misheel Bayartsengel <misheel.bayart@gmail.com>
2021-09-21 11:51:09 -0400

    Fix topic name and cleanup prints

--- intersection_transit_maneuvering/src/intersection_transit_maneuvering.cpp
+++ intersection_transit_maneuvering/src/intersection_transit_maneuvering.cpp
@@ -38,9 +38,9 @@ namespace intersection_transit_maneuvering
  */
 std::ostream& operator<<(std::ostream& os, cav_msgs::ManeuverParameters m) {
     os << "maneuver_id: " << m.maneuver_id
-        << " negotiation_type: " << m.negotiation_type
+        << " negotiation_type: " << unsigned(m.negotiation_type)
         << " planning_strategic_plugin: " << m.planning_strategic_plugin
-        << " presence_vector: " << m.presence_vector
+        << " presence_vector: " << unsigned(m.presence_vector)
         << " planning_tactical_plugin: " << m.planning_tactical_plugin;
     return os;
 }
@@ -104,6 +104,8 @@ std::ostream& operator<<(std::ostream& os, cav_msgs::LaneFollowingManeuver m) {
 }
 
 std::ostream& operator<<(std::ostream& os, cav_msgs::Maneuver m) {
+
+    os << "Maneuver Type: " << unsigned(m.type);
     switch(m.type) {
         case cav_msgs::Maneuver::INTERSECTION_TRANSIT_STRAIGHT:
             os << m.intersection_transit_straight_maneuver;
@@ -118,7 +120,7 @@ std::ostream& operator<<(std::ostream& os, cav_msgs::Maneuver m) {
             os << m.lane_following_maneuver;
             break;
         default:
-            os << "Maneuver Type: " << m.type << " not yet supported for printing. ";
+            os << " not yet supported for printing. ";
     }
 
     return os;
@@ -140,20 +142,14 @@ std::ostream& operator<<(std::ostream& os, cav_msgs::Maneuver m) {
     bool IntersectionTransitManeuvering::plan_trajectory_cb(cav_srvs::PlanTrajectoryRequest& req, cav_srvs::PlanTrajectoryResponse& resp)
     {
 
-        ROS_DEBUG("HERE 1");
         ros::WallTime start_time = ros::WallTime::now();  // Start timeing the execution time for planning so it can be logged
 
         std::vector<cav_msgs::Maneuver> maneuver_plan;
 
-        ROS_DEBUG("HERE 2");
         auto related_maneuvers = resp.related_maneuvers;
 
-        ROS_DEBUG("HERE 3");
         ROS_DEBUG_STREAM("Starting planning for maneuver index: " << req.maneuver_index_to_plan);
         ROS_DEBUG_STREAM("req.maneuver_plan.maneuvers.size(): " << req.maneuver_plan.maneuvers.size());
-        ROS_DEBUG("HERE 4");
-        ROS_DEBUG("HERE 5");
-        ROS_DEBUG("HERE 6");
         for(size_t i = req.maneuver_index_to_plan; i < req.maneuver_plan.maneuvers.size(); i++)
         {
             ROS_DEBUG("Looping");
@@ -171,16 +167,10 @@ std::ostream& operator<<(std::ostream& os, cav_msgs::Maneuver m) {
                 }
         }
 
-        ROS_DEBUG("HERE 7");
-        ROS_DEBUG("HERE 8");
-        ROS_DEBUG("HERE 9");
 
         converted_maneuvers_ = convert_maneuver_plan(maneuver_plan);
         cav_srvs::PlanTrajectoryRequest new_req;
 
-        ROS_DEBUG("HERE 10");
-        ROS_DEBUG("HERE 11");
-        ROS_DEBUG("HERE 12");
 
         for(auto i : converted_maneuvers_)
         {
@@ -191,27 +181,17 @@ std::ostream& operator<<(std::ostream& os, cav_msgs::Maneuver m) {
             new_req.vehicle_state = req.vehicle_state;
             new_req.initial_trajectory_plan = req.initial_trajectory_plan;
 
-        ROS_DEBUG("HERE 13");
-        ROS_DEBUG("HERE 14");
-        ROS_DEBUG("HERE 15");
 
         if(object_->call(new_req,resp))//Since we're using an interface for this process, the call() functionality will come from somewhere else
         {
             ROS_DEBUG_STREAM("Call Successful");
         }
 
-        ROS_DEBUG("HERE 16");
-        ROS_DEBUG("HERE 17");
-        ROS_DEBUG("HERE 18");
 
         resp.related_maneuvers = related_maneuvers; // Set the related maneuvers using the origional maneuver indexs not those sent to inlane-cruising
         for (auto maneuver : related_maneuvers) {
             resp.maneuver_status.push_back(cav_srvs::PlanTrajectory::Response::MANEUVER_IN_PROGRESS);
         }
-
-        ROS_DEBUG("HERE 19");
-        ROS_DEBUG("HERE 20");
-        ROS_DEBUG("HERE 21");
 
         ros::WallTime end_time = ros::WallTime::now();
 
