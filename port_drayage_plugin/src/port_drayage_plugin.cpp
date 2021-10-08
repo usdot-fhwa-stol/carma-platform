670c1e47b91842ad70a56bfca3ca0ac79d36f2ec
JonSmet <34752540+JonSmet@users.noreply.github.com>
2021-10-08 15:04:22 -0400

    Update Port Drayage Plugin to publish UI Instructions to the Web UI when a route to a newly received destination has been generated. (#1438)
    * Updatearrival message contents
    
    * Remove print statement from unit test
    
    * Add subscription to topic for current vehicle latitude/longitude
    
    * Update cargo_id config parameter in params.yml
    
    * Add config parameter for enabling port drayage operations
    
    * Implement arrival message broadcast for loading area
    
    * Add unit tests for arrival at loading area
    
    * Address PR comments
    
    * Remove novatel_gps_msgs dependency
    
    * Add comment regarding size of unsigned long
    
    * Update unit tests
    
    * Fix merge conflict
    
    * Update cargo_id value so CMV begins without cargo
    
    * Updates for pickup action
    
    * Update unit tests
    
    * Updates from integration testing
    
    * Update unit tests
    
    * Code cleanup
    
    * Fix code smells
    
    * Fix errors from merge conflict resolution
    
    * Disable port drayage using config param
    
    * Add updates to publish UIInstructions msg for new route
    
    * Update unit tests for composed UI Instructions
    
    * Notify user of previously completed action
    
    * Cleanup whitespace
    
    * Remove redundant mobility operation msg parsing lines
    
    * Update comment
    
    * Update comment
    
    * Update parameter type passed to compose_ui_instructions
    
    * Fix unit test
    
    Co-authored-by: Misheel Bayartsengel <misheel.bayart@gmail.com>
    Co-authored-by: John Stark <starkj@leidos.com>

--- port_drayage_plugin/src/port_drayage_plugin.cpp
+++ port_drayage_plugin/src/port_drayage_plugin.cpp
@@ -45,6 +45,9 @@ namespace port_drayage_plugin
         ros::Publisher outbound_mob_op = _nh->advertise<cav_msgs::MobilityOperation>("outgoing_mobility_operation", 5);
         _outbound_mobility_operations_publisher = std::make_shared<ros::Publisher>(outbound_mob_op);
 
+        ros::Publisher ui_instructions_pub = _nh->advertise<cav_msgs::UIInstructions>("ui_instructions", 5);
+        _ui_instructions_publisher = std::make_shared<ros::Publisher>(ui_instructions_pub);
+
         _set_active_route_client = _nh->serviceClient<cav_srvs::SetActiveRoute>("/guidance/set_active_route");
 
         PortDrayageWorker pdw{
@@ -53,6 +56,9 @@ namespace port_drayage_plugin
             host_id,
             [this](cav_msgs::MobilityOperation msg) {
                _outbound_mobility_operations_publisher->publish<cav_msgs::MobilityOperation>(msg);
+            },
+            [this](cav_msgs::UIInstructions msg) {
+               _ui_instructions_publisher->publish<cav_msgs::UIInstructions>(msg);
             },
             speed_epsilon,
             enable_port_drayage,
