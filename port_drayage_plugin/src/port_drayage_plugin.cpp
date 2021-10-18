d45a1afbf1efdb0b8cd62fcec5a3033b7306df33
JonSmet <34752540+JonSmet@users.noreply.github.com>
2021-10-18 14:53:49 -0400

    Add processing of remaining 'operation's to port_drayage_plugin (#1480)
    * Updatearrival message contents
    
    * Remove print statement from unit test
    
    * Add subscription to topic for current vehicle latitude/longitude
    
    * Update cargo_id config parameter in params.yml
    
    * Add config parameter for enabling port drayage operations
    
    * Implement arrival message broadcast for loading area
    
    * Add unit tests for arrival at loading area
    
    * Address PR comments
    
    * Remove novatel_gps_msgs dependency
    
    * Add comment regarding size of unsigned long
    
    * Update unit tests
    
    * Fix merge conflict
    
    * Update cargo_id value so CMV begins without cargo
    
    * Updates for pickup action
    
    * Update unit tests
    
    * Updates from integration testing
    
    * Update unit tests
    
    * Code cleanup
    
    * Fix code smells
    
    * Fix errors from merge conflict resolution
    
    * Disable port drayage using config param
    
    * Add updates to publish UIInstructions msg for new route
    
    * Update unit tests for composed UI Instructions
    
    * Notify user of previously completed action
    
    * Cleanup whitespace
    
    * Remove redundant mobility operation msg parsing lines
    
    * Update comment
    
    * Update comment
    
    * Update parameter type passed to compose_ui_instructions
    
    * Fix unit test
    
    * Add remaining port drayage operations
    
    * Store the previously completed operation string
    
    * Add configuration param for initial arrival operation ID
    
    * Remove duplicate unit test
    
    * Address PR comments
    
    * Update comments
    
    * Update OperationID class
    
    * Update operation_to_string method
    
    * Update OperationID class
    
    Co-authored-by: Misheel Bayartsengel <misheel.bayart@gmail.com>
    Co-authored-by: John Stark <starkj@leidos.com>
    Co-authored-by: Kyle Rush <kyle.rush@leidos.com>

--- port_drayage_plugin/src/port_drayage_plugin.cpp
+++ port_drayage_plugin/src/port_drayage_plugin.cpp
@@ -27,12 +27,15 @@ namespace port_drayage_plugin
             return -1;
         }
 
+        // Read in configuration parameters
         double speed_epsilon = _pnh->param("stop_speed_epsilon", 1.0);
         declaration = _pnh->param("declaration", 1.0);
         std::string cargo_id;
         _pnh->param<std::string>("cargo_id", cargo_id, "UNDEFINED-CARGO-ID"); 
         std::string host_id;
         _pnh->param<std::string>("host_id", host_id, "UNDEFINED-HOST-ID");
+        bool starting_at_staging_area;
+        _pnh->param<bool>("starting_at_staging_area", starting_at_staging_area, true);
         bool enable_port_drayage;
         _pnh->param<bool>("enable_port_drayage", enable_port_drayage, false);
 
@@ -54,6 +57,7 @@ namespace port_drayage_plugin
             cmv_id,
             cargo_id,
             host_id,
+            starting_at_staging_area,
             [this](cav_msgs::MobilityOperation msg) {
                _outbound_mobility_operations_publisher->publish<cav_msgs::MobilityOperation>(msg);
             },
