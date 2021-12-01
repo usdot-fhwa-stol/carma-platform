1149486b7d8a05b3d24dae6ff1a46ca50645c603
saina-ramyar <saina.ramyar@leidos.com>
2021-12-01 11:46:06 -0500

    add more logs

--- arbitrator/src/arbitrator.cpp
+++ arbitrator/src/arbitrator.cpp
@@ -185,6 +185,9 @@ namespace arbitrator
         // convert to front bumper
         tf2::Stamped<tf2::Transform> bumper_transform;
         tf2::fromMsg(tf_, bumper_transform);
+        
+        ROS_DEBUG_STREAM("transform x: " << bumper_transform.transform.translation.x);
+        ROS_DEBUG_STREAM("transform y: " << bumper_transform.transform.translation.y);
 
         geometry_msgs::Pose front_bumper_pose = shift_to_frontbumper(msg->pose, bumper_transform);
 
