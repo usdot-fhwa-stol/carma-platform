c870bac0cd096d29bf078f96aabfd06bf23f8dfc
Michael McConnell <Michael.McConnell-2@leidos.com>
2021-12-09 21:31:23 +0000

    Return all deps to foxy develop

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -42,11 +42,11 @@ cd ${dir}/src
 
 if [[ "$BRANCH" = "develop" ]]; then
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch $BRANCH
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch feature/helper_methods_for_parameters
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch $BRANCH
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch $BRANCH
 else
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch foxy/develop
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch feature/helper_methods_for_parameters
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch foxy/develop
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch foxy/develop
 fi
 
