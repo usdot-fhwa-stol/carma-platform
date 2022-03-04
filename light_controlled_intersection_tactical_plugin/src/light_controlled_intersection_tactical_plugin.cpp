212d0af3da9e31b042f968614f4152f665e4541f
mishkamn <misheel.bayart@gmail.com>
2022-03-04 21:23:49 +0000

    fix build

--- light_controlled_intersection_tactical_plugin/src/light_controlled_intersection_tactical_plugin.cpp
+++ light_controlled_intersection_tactical_plugin/src/light_controlled_intersection_tactical_plugin.cpp
@@ -141,12 +141,12 @@ bool LightControlledIntersectionTacticalPlugin::plan_trajectory_cb(cav_srvs::Pla
     if (last_case_ && last_case_.get() == static_cast<SpeedProfileCase>GET_MANEUVER_PROPERTY(maneuver_plan.front(), parameters.int_valued_meta_data[0]))
     {
       resp.trajectory_plan = last_trajectory_;
-      ROS_ERROR_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_);
-      ROS_ERROR_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_);
-      ROS_ERROR_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_);
-      ROS_DEBUG_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_);
-      ROS_DEBUG_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_);
-      ROS_DEBUG_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_);
+      ROS_ERROR_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_.get());
+      ROS_ERROR_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_.get());
+      ROS_ERROR_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_.get());
+      ROS_DEBUG_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_.get());
+      ROS_DEBUG_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_.get());
+      ROS_DEBUG_STREAM("!!!!!! DOES NOT MATTER USING LAST!!! : " << (int)last_case_.get());
       
     }  
     else
@@ -154,12 +154,12 @@ bool LightControlledIntersectionTacticalPlugin::plan_trajectory_cb(cav_srvs::Pla
       last_trajectory_ = trajectory;
       resp.trajectory_plan = trajectory;
       last_case_ = static_cast<SpeedProfileCase>GET_MANEUVER_PROPERTY(maneuver_plan.front(), parameters.int_valued_meta_data[0]);
-      ROS_ERROR_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_);
-      ROS_ERROR_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_);
-      ROS_ERROR_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_);
-      ROS_DEBUG_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_);
-      ROS_DEBUG_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_);
-      ROS_DEBUG_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_);
+      ROS_ERROR_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_.get());
+      ROS_ERROR_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_.get());
+      ROS_ERROR_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_.get());
+      ROS_DEBUG_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_.get());
+      ROS_DEBUG_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_.get());
+      ROS_DEBUG_STREAM("++++ USING NEW CASE!!! : " << (int)last_case_.get());
       
     }
 
