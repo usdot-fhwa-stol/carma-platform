68ddd5c030d9be03af5958fe01e2ffe7f1696adf
itabrl <samir.manafzadehtabriz@leidos.com>
2022-03-10 11:51:44 -0800

    Update

---
+++ gnss_to_map_convertor/README.md
@@ -0,0 +1,3 @@
+# gnss_to_map_convertor
+
+TODO for USER: Add description of package and link to confluence documentation.
---
+++ gnss_to_map_convertor/include/gnss_to_map_convertor/gnss_to_map_convertor_config.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+/*
+ * Copyright (C) 2022 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#include <iostream>
+#include <vector>
+
+namespace gnss_to_map_convertor
+{
+
+  /**
+   * \brief Stuct containing the algorithm configuration values for gnss_to_map_convertor
+   */
+  struct Config
+  {
+    //! Example parameter
+    std::string base_link_frame = "base_link";
+    std::string map_frame = "map";
+    std::string heading_frame = "ned_heading";
+
+    // Stream operator for this config
+    // TODO for USER: Update prints for the added parameters
+    friend std::ostream &operator<<(std::ostream &output, const Config &c)
+    {
+      output << "gnss_to_map_convertor::Config { " << std::endl
+           << "base_link_frame: " << c.base_link_frame << std::endl
+           << "map_frame: " << c.map_frame << std::endl
+           << "heading_frame: " << c.heading_frame << std::endl
+           << "}" << std::endl;
+      return output;
+    }
+  };
+
+} // gnss_to_map_convertor
