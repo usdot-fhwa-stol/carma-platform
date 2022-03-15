306d98188ffa5e57d58190578bf0cbb5f88e8a23
itabrl <samir.manafzadehtabriz@leidos.com>
2022-03-15 07:37:41 -0700

    Update

--- gnss_to_map_convertor/test/node_test.cpp
+++ gnss_to_map_convertor/test/node_test.cpp
@@ -324,8 +324,7 @@ TEST(GNSSToMapConvertor, gnssFixCb)
 
   tf2::Vector3 solTrans(2.22638982, 2.10574276, 0);  // At offset location with baselink transformation
   tf2::Quaternion solRot;
-  solRot.setRPY(0, 0,
-                90.0 * wgs84_utils::DEG2RAD);  // Facing north. setRPY uses extrinsic fixed frame not intrinsic frame
+  solRot.setRPY(0, 0, 90.0 * wgs84_utils::DEG2RAD);  // Facing north. setRPY uses extrinsic fixed frame not intrinsic frame
   tf2::Transform solution(solRot, solTrans);
 
   double error_bound_dist = 0.0001;
@@ -349,8 +348,112 @@ TEST(GNSSToMapConvertor, gnssFixCb)
   // ASSERT_NEAR(solution.getRotation().getZ(), pose_msg->pose.orientation.z, error_bound_rad);
   // ASSERT_NEAR(solution.getRotation().getW(), pose_msg->pose.orientation.w, error_bound_rad);
 
-
 }
+
+
+// This test depends on the poseFromGNSS test passing
+// TEST(GNSSToMapConvertor, gnssFixCb)
+// {
+//   boost::optional<geometry_msgs::msg> pose_msg;
+//   bool fail = false; // flag for transform lookup failure since FAIL() cannot be used inside lambdas
+
+//   gnss_to_map_convertor::GNSSToMapConvertor convertor(  // Create without transforms
+//       [&](auto msg) {
+//         pose_msg = msg;  // Record the pose message when it is set
+//       },
+//       [](auto a, auto b) -> boost::optional<geometry_msgs::TransformStamped> { return boost::none; }, "map",
+//       "base_link", "ned_heading");
+
+//   gps_common::GPSFix fix_msg;
+//   fix_msg.header.frame_id = "sensor";
+
+//   gps_common::GPSFixConstPtr fix_ptr(new gps_common::GPSFix(fix_msg));
+
+//   convertor.gnssFixCb(fix_ptr);  // Call before any transforms are set
+//   ASSERT_FALSE(!!pose_msg);
+
+//   convertor = gnss_to_map_convertor::GNSSToMapConvertor(  // Create with only baselink transform
+//       [&](auto msg) {
+//         pose_msg = msg;  // Record the pose message when it is set
+//       },
+//       [](auto a, auto b) -> boost::optional<geometry_msgs::TransformStamped> {
+//         if (a.compare("sensor") == 0 && b.compare("base_link") == 0)
+//         {
+//           geometry_msgs::TransformStamped msg;
+//           msg.transform.translation.x = 1.0;
+//           msg.transform.rotation.w = 1.0;
+//           return msg;
+//         }
+//         return boost::none;
+//       },
+//       "map", "base_link", "ned_heading");
+
+//   convertor.gnssFixCb(fix_ptr);  // Call after baselink transform is set
+//   ASSERT_FALSE(!!pose_msg);
+
+//   convertor = gnss_to_map_convertor::GNSSToMapConvertor(  // Create with transforms
+//       [&](auto msg) {
+//         pose_msg = msg;  // Record the pose message when it is set
+//       },
+//       [&fail](auto a, auto b) -> boost::optional<geometry_msgs::TransformStamped> {
+//         if (a.compare("sensor") == 0 && b.compare("base_link") == 0)
+//         {
+//           geometry_msgs::TransformStamped msg;
+//           msg.transform.translation.x = 1.0;
+//           msg.transform.rotation.w = 1.0;
+//           return msg;
+//         }
+
+//         if (a.compare("sensor") == 0 && b.compare("ned_heading") == 0)
+//         {
+//           geometry_msgs::TransformStamped msg;
+//           msg.transform.rotation.x = 1.0;
+//           return msg;
+//         }
+//         fail = true;
+//         return boost::none;
+//       },
+//       "map", "base_link", "ned_heading");
+
+//   // Test point at prime meridian and equator with 0 heading
+//   fix_msg.latitude = 0.00001;
+//   fix_msg.longitude = 0.00002;
+//   fix_msg.altitude = 0;
+
+//   fix_msg.track = 0;
+
+//   fix_ptr = gps_common::GPSFixConstPtr(new gps_common::GPSFix(fix_msg));
+
+//   convertor.gnssFixCb(fix_ptr);  // Call before projection is set
+//   ASSERT_FALSE(fail) << "System attempted to lookup transforms that were not expected ";
+//   ASSERT_FALSE(!!pose_msg);
+
+//   std::string base_proj = "+proj=tmerc +lat_0=0.0 +lon_0=0.0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +vunits=m "
+//                           "+no_defs";
+//   std_msgs::String msg;
+//   msg.data = base_proj;
+//   convertor.geoReferenceCallback(msg);  // Set projection
+//   auto rotation = convertor.getNedInMapRotation();
+//   auto projector = convertor.getMapProjector();
+//   ASSERT_TRUE(!!rotation);
+//   ASSERT_TRUE(!!projector);
+
+//   fix_ptr = gps_common::GPSFixConstPtr(new gps_common::GPSFix(fix_msg));
+//   convertor.gnssFixCb(fix_ptr);  // call after projection is set
+
+//   ASSERT_TRUE(!!pose_msg);
+
+//   tf2::Vector3 solTrans(2.22638982, 2.10574276, 0);  // At offset location with baselink transformation
+//   tf2::Quaternion solRot;
+//   solRot.setRPY(0, 0,
+//                 90.0 * wgs84_utils::DEG2RAD);  // Facing north. setRPY uses extrinsic fixed frame not intrinsic frame
+//   tf2::Transform solution(solRot, solTrans);
+
+//   double error_bound_dist = 0.0001;
+//   double error_bound_rad = 0.000001;
+//   assertNear(solution, pose_msg->pose, error_bound_dist, error_bound_rad);
+// }
+
 
 int main(int argc, char ** argv)
 {
