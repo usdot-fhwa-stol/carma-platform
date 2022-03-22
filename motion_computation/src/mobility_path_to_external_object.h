26ac2dc6d18bcc19af8412d99ceb81cbe8c2560e
Michael McConnell <Michael.McConnell-2@leidos.com>
2022-03-22 20:29:07 +0000

    WIP

--- motion_computation/src/mobility_path_to_external_object.h
+++ motion_computation/src/mobility_path_to_external_object.h
@@ -1,176 +1,192 @@
+#include "message_to_external_object_convertor.h"
+#include <carma_v2x_msgs/mobility_path.hpp>
+#include <carma_perception_msgs/external_object.hpp>
 
-// TODO cleanup this file
-carma_perception_msgs::msg::ExternalObject MotionComputationWorker::mobilityPathToExternalObject(const carma_v2x_msgs::msg::MobilityPath::UniquePtr& msg) const
+namespace object
+{
+
+    class MobilityPathToExternalObject : public MessageToExternalObjectConvertor<carma_v2x_msgs::msg::MobilityPath>
     {
-        carma_perception_msgs::msg::ExternalObject output;
 
-        output.size.x = 2.5; // TODO identify better approach for object side in mobility path
-        output.size.y = 2.25;
-        output.size.z = 2.0;
-
-        // get reference origin in ECEF (convert from cm to m)
-        double ecef_x = (double)msg->trajectory.location.ecef_x/100.0;
-        double ecef_y = (double)msg->trajectory.location.ecef_y/100.0;
-        double ecef_z = (double)msg->trajectory.location.ecef_z/100.0;
-
-        // Convert general information
-        output.presence_vector |= carma_perception_msgs::msg::ExternalObject::ID_PRESENCE_VECTOR; 
-        output.presence_vector |= carma_perception_msgs::msg::ExternalObject::POSE_PRESENCE_VECTOR;
-        output.presence_vector |= carma_perception_msgs::msg::ExternalObject::VELOCITY_PRESENCE_VECTOR;
-        output.presence_vector |= carma_perception_msgs::msg::ExternalObject::OBJECT_TYPE_PRESENCE_VECTOR;
-        output.presence_vector |= carma_perception_msgs::msg::ExternalObject::BSM_ID_PRESENCE_VECTOR;
-        output.presence_vector |= carma_perception_msgs::msg::ExternalObject::DYNAMIC_OBJ_PRESENCE;
-        output.presence_vector |= carma_perception_msgs::msg::ExternalObject::PREDICTION_PRESENCE_VECTOR;
-        output.object_type = carma_perception_msgs::msg::ExternalObject::SMALL_VEHICLE;
-        std::hash<std::string> hasher;
-        auto hashed = hasher(msg->m_header.sender_id); //TODO hasher returns size_t, message accept uint32_t which we might lose info
-        output.id = (uint32_t)hashed;
-        
-        // convert hex std::string to uint8_t array
-        for (size_t i = 0; i < msg->m_header.sender_bsm_id.size(); i+=2) 
+        void convert(const carma_v2x_msgs::msg::MobilityPath &in_msg, carma_perception_msgs::msg::ExternalObject &out_msg)
         {
-            int num = 0;
-            sscanf(msg->m_header.sender_bsm_id.substr(i, i + 2).c_str(), "%x", &num);
-            output.bsm_id.push_back((uint8_t)num);
-        }
 
-        // first point's timestamp
-        output.header.stamp = rclcpp::Time((uint64_t)msg->m_header.timestamp *1e6); // ms to nanoseconds
+            out_msg.size.x = 2.5; // TODO identify better approach for object side in mobility path
+            out_msg.size.y = 2.25;
+            out_msg.size.z = 2.0;
 
-        // If it is a static object, we finished processing
-        if (msg->trajectory.offsets.size() < 2)
-        {
-            output.dynamic_obj = false;
-            return output;
-        }
-        output.dynamic_obj = true;
+            // get reference origin in ECEF (convert from cm to m)
+            double ecef_x = (double)in_msg.trajectory.location.ecef_x / 100.0;
+            double ecef_y = (double)in_msg.trajectory.location.ecef_y / 100.0;
+            double ecef_z = (double)in_msg.trajectory.location.ecef_z / 100.0;
 
-        // get planned trajectory points
-        carma_perception_msgs::msg::PredictedState prev_state;
-        tf2::Vector3 prev_pt_ecef {ecef_x, ecef_y, ecef_z};
+            // Convert general information
+            out_msg.presence_vector |= carma_perception_msgs::msg::ExternalObject::ID_PRESENCE_VECTOR;
+            out_msg.presence_vector |= carma_perception_msgs::msg::ExternalObject::POSE_PRESENCE_VECTOR;
+            out_msg.presence_vector |= carma_perception_msgs::msg::ExternalObject::VELOCITY_PRESENCE_VECTOR;
+            out_msg.presence_vector |= carma_perception_msgs::msg::ExternalObject::OBJECT_TYPE_PRESENCE_VECTOR;
+            out_msg.presence_vector |= carma_perception_msgs::msg::ExternalObject::BSM_ID_PRESENCE_VECTOR;
+            out_msg.presence_vector |= carma_perception_msgs::msg::ExternalObject::DYNAMIC_OBJ_PRESENCE;
+            out_msg.presence_vector |= carma_perception_msgs::msg::ExternalObject::PREDICTION_PRESENCE_VECTOR;
+            out_msg.object_type = carma_perception_msgs::msg::ExternalObject::SMALL_VEHICLE;
+            std::hash<std::string> hasher;
+            auto hashed = hasher(in_msg.m_header.sender_id); // TODO hasher returns size_t, message accept uint32_t which we might lose info
+            out_msg.id = (uint32_t)hashed;
 
-        auto prev_pt_map = transform_to_map_frame(prev_pt_ecef);
-        double prev_yaw = 0.0;
-
-        double message_offset_x = 0.0; // units cm
-        double message_offset_y = 0.0;
-        double message_offset_z = 0.0;
-
-        for (size_t i = 0; i < msg->trajectory.offsets.size(); i ++)
-        {
-            auto curr_pt_msg = msg->trajectory.offsets[i];
-
-            message_offset_x = (double)curr_pt_msg.offset_x + message_offset_x;
-            message_offset_y = (double)curr_pt_msg.offset_y + message_offset_y;
-            message_offset_z = (double)curr_pt_msg.offset_z + message_offset_z;
-
-            tf2::Vector3 curr_pt_ecef {ecef_x + message_offset_x /100.0, ecef_y + message_offset_y /100.0, ecef_z + message_offset_z /100.0}; // ecef_x is in m while message_offset_x is in cm. Want m as final result
-            auto curr_pt_map = transform_to_map_frame(curr_pt_ecef);
-
-            carma_perception_msgs::msg::PredictedState curr_state;
-            
-            if (i == 0) // First point's state should be stored outside "predictions"
+            // convert hex std::string to uint8_t array
+            for (size_t i = 0; i < in_msg.m_header.sender_bsm_id.size(); i += 2)
             {
-                auto res = composePredictedState(curr_pt_map, prev_pt_map, output.header.stamp, prev_yaw); // Position returned is that of prev_pt_map NOT curr_pt_map 
-                curr_state = std::get<0>(res);
-                prev_yaw = std::get<1>(res);
-                // Compute output pose
-                output.pose.pose = curr_state.predicted_position; // Orientation computed from first point in offsets with location
-                output.velocity.twist = curr_state.predicted_velocity; // Velocity derived from first point
-            }
-            else    
-            {
-                rclcpp::Time updated_time_step = rclcpp::Time(prev_state.header.stamp) + rclcpp::Duration(mobility_path_prediction_time_step_ * 1e9);
-                auto res = composePredictedState(curr_pt_map, prev_pt_map, updated_time_step, prev_yaw);
-                curr_state = std::get<0>(res);
-                prev_yaw = std::get<1>(res);
-                output.predictions.push_back(curr_state);
+                int num = 0;
+                sscanf(in_msg.m_header.sender_bsm_id.substr(i, i + 2).c_str(), "%x", &num);
+                out_msg.bsm_id.push_back((uint8_t)num);
             }
 
-            if (i == msg->trajectory.offsets.size() - 1) // if last point, copy the prev_state velocity & orientation to the last point too
+            // first point's timestamp
+            out_msg.header.stamp = rclcpp::Time((uint64_t)in_msg.m_header.timestamp * 1e6); // ms to nanoseconds
+
+            // If it is a static object, we finished processing
+            if (in_msg.trajectory.offsets.size() < 2)
             {
-                curr_state.predicted_position.position.x = curr_pt_map.x();
-                curr_state.predicted_position.position.y = curr_pt_map.y();
-                curr_state.predicted_position.position.z = curr_pt_map.z();
-                output.predictions.push_back(curr_state);
+                out_msg.dynamic_obj = false;
+                return;
             }
-            
-            prev_state = curr_state;
-            prev_pt_map = curr_pt_map;
-        }
+            out_msg.dynamic_obj = true;
 
-        calculateAngVelocityOfPredictedStates(output);        
+            // get planned trajectory points
+            carma_perception_msgs::msg::PredictedState prev_state;
+            tf2::Vector3 prev_pt_ecef{ecef_x, ecef_y, ecef_z};
 
-        return output;
-    }
+            auto prev_pt_map = transform_to_map_frame(prev_pt_ecef);
+            double prev_yaw = 0.0;
 
-    std::pair<carma_perception_msgs::msg::PredictedState, double> MotionComputationWorker::composePredictedState(const tf2::Vector3& curr_pt, const tf2::Vector3& prev_pt, const rclcpp::Time& prev_time_stamp, double prev_yaw) const
-    {
-        carma_perception_msgs::msg::PredictedState output_state;
-        // Set Position
-        output_state.predicted_position.position.x = prev_pt.x();
-        output_state.predicted_position.position.y = prev_pt.y();
-        output_state.predicted_position.position.z = prev_pt.z();
+            double message_offset_x = 0.0; // units cm
+            double message_offset_y = 0.0;
+            double message_offset_z = 0.0;
 
-        // Set Orientation
-        Eigen::Vector2d vehicle_vector = {curr_pt.x() - prev_pt.x() ,curr_pt.y() - prev_pt.y()};
-        Eigen::Vector2d x_axis = {1, 0};
-        double yaw = 0.0;
-        if (vehicle_vector.norm() < 0.000001) { // If there is zero magnitude use previous yaw to avoid devide by 0
-            yaw = prev_yaw;
-            RCLCPP_DEBUG_STREAM(logger_->get_logger(), "Two identical points sent for predicting heading. Forced to use previous yaw or 0 if first point");
-        } else {
-            yaw = std::acos(vehicle_vector.dot(x_axis)/(vehicle_vector.norm() * x_axis.norm()));
-        }
-    
-        tf2::Quaternion vehicle_orientation;
-        vehicle_orientation.setRPY(0, 0, yaw);
-        output_state.predicted_position.orientation.x = vehicle_orientation.getX();
-        output_state.predicted_position.orientation.y = vehicle_orientation.getY();
-        output_state.predicted_position.orientation.z = vehicle_orientation.getZ();
-        output_state.predicted_position.orientation.w = vehicle_orientation.getW();
+            for (size_t i = 0; i < in_msg.trajectory.offsets.size(); i++)
+            {
+                auto curr_pt_msg = in_msg.trajectory.offsets[i];
 
-        // Set velocity
-        output_state.predicted_velocity.linear.x = vehicle_vector.norm() / mobility_path_prediction_time_step_;
+                message_offset_x = (double)curr_pt_msg.offset_x + message_offset_x;
+                message_offset_y = (double)curr_pt_msg.offset_y + message_offset_y;
+                message_offset_z = (double)curr_pt_msg.offset_z + message_offset_z;
 
-        // Set timestamp
-        output_state.header.stamp = builtin_interfaces::msg::Time(prev_time_stamp);
+                tf2::Vector3 curr_pt_ecef{ecef_x + message_offset_x / 100.0, ecef_y + message_offset_y / 100.0, ecef_z + message_offset_z / 100.0}; // ecef_x is in m while message_offset_x is in cm. Want m as final result
+                auto curr_pt_map = transform_to_map_frame(curr_pt_ecef);
 
-        return std::make_pair(output_state, yaw);
-    }
+                carma_perception_msgs::msg::PredictedState curr_state;
 
-    void MotionComputationWorker::calculateAngVelocityOfPredictedStates(carma_perception_msgs::msg::ExternalObject& object) const
-    {
-        if (!object.dynamic_obj)
-        {
+                if (i == 0) // First point's state should be stored outside "predictions"
+                {
+                    auto res = composePredictedState(curr_pt_map, prev_pt_map, out_msg.header.stamp, prev_yaw); // Position returned is that of prev_pt_map NOT curr_pt_map
+                    curr_state = std::get<0>(res);
+                    prev_yaw = std::get<1>(res);
+                    // Compute out_msg pose
+                    out_msg.pose.pose = curr_state.predicted_position;      // Orientation computed from first point in offsets with location
+                    out_msg.velocity.twist = curr_state.predicted_velocity; // Velocity derived from first point
+                }
+                else
+                {
+                    rclcpp::Time updated_time_step = rclcpp::Time(prev_state.header.stamp) + rclcpp::Duration(mobility_path_prediction_time_step_ * 1e9);
+                    auto res = composePredictedState(curr_pt_map, prev_pt_map, updated_time_step, prev_yaw);
+                    curr_state = std::get<0>(res);
+                    prev_yaw = std::get<1>(res);
+                    out_msg.predictions.push_back(curr_state);
+                }
+
+                if (i == in_msg.trajectory.offsets.size() - 1) // if last point, copy the prev_state velocity & orientation to the last point too
+                {
+                    curr_state.predicted_position.position.x = curr_pt_map.x();
+                    curr_state.predicted_position.position.y = curr_pt_map.y();
+                    curr_state.predicted_position.position.z = curr_pt_map.z();
+                    out_msg.predictions.push_back(curr_state);
+                }
+
+                prev_state = curr_state;
+                prev_pt_map = curr_pt_map;
+            }
+
+            calculateAngVelocityOfPredictedStates(out_msg);
+
             return;
         }
 
-        // Object's current angular velocity
-        object.velocity.twist.angular.z = (getYawFromQuaternionMsg(object.pose.pose.orientation) - 
-                                            getYawFromQuaternionMsg(object.predictions[0].predicted_position.orientation)) / mobility_path_prediction_time_step_;
-        
-        // Predictions' angular velocities
-        auto prev_orient = object.pose.pose.orientation;
-        for (auto& pred: object.predictions)
+        std::pair<carma_perception_msgs::msg::PredictedState, double> composePredictedState(const tf2::Vector3 &curr_pt, const tf2::Vector3 &prev_pt, const rclcpp::Time &prev_time_stamp, double prev_yaw) const
         {
-            pred.predicted_velocity.angular.z = (getYawFromQuaternionMsg(prev_orient) - 
-                                            getYawFromQuaternionMsg(pred.predicted_position.orientation)) / mobility_path_prediction_time_step_;
+            carma_perception_msgs::msg::PredictedState output_state;
+            // Set Position
+            output_state.predicted_position.position.x = prev_pt.x();
+            output_state.predicted_position.position.y = prev_pt.y();
+            output_state.predicted_position.position.z = prev_pt.z();
+
+            // Set Orientation
+            Eigen::Vector2d vehicle_vector = {curr_pt.x() - prev_pt.x(), curr_pt.y() - prev_pt.y()};
+            Eigen::Vector2d x_axis = {1, 0};
+            double yaw = 0.0;
+            if (vehicle_vector.norm() < 0.000001)
+            { // If there is zero magnitude use previous yaw to avoid devide by 0
+                yaw = prev_yaw;
+                RCLCPP_DEBUG_STREAM(logger_->get_logger(), "Two identical points sent for predicting heading. Forced to use previous yaw or 0 if first point");
+            }
+            else
+            {
+                yaw = std::acos(vehicle_vector.dot(x_axis) / (vehicle_vector.norm() * x_axis.norm()));
+            }
+
+            tf2::Quaternion vehicle_orientation;
+            vehicle_orientation.setRPY(0, 0, yaw);
+            output_state.predicted_position.orientation.x = vehicle_orientation.getX();
+            output_state.predicted_position.orientation.y = vehicle_orientation.getY();
+            output_state.predicted_position.orientation.z = vehicle_orientation.getZ();
+            output_state.predicted_position.orientation.w = vehicle_orientation.getW();
+
+            // Set velocity
+            output_state.predicted_velocity.linear.x = vehicle_vector.norm() / mobility_path_prediction_time_step_;
+
+            // Set timestamp
+            output_state.header.stamp = builtin_interfaces::msg::Time(prev_time_stamp);
+
+            return std::make_pair(output_state, yaw);
         }
-    }
 
-    double getYawFromQuaternionMsg(const geometry_msgs::msg::Quaternion& quaternion)
-    {
-        tf2::Quaternion orientation;
-        orientation.setX(quaternion.x);
-        orientation.setY(quaternion.y);
-        orientation.setZ(quaternion.z);
-        orientation.setW(quaternion.w);
+        void calculateAngVelocityOfPredictedStates(carma_perception_msgs::msg::ExternalObject &object) const
+        {
+            if (!object.dynamic_obj)
+            {
+                return;
+            }
 
-        double roll;
-        double pitch;
-        double yaw;
-        tf2::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+            // Object's current angular velocity
+            object.velocity.twist.angular.z = (getYawFromQuaternionMsg(object.pose.pose.orientation) -
+                                               getYawFromQuaternionMsg(object.predictions[0].predicted_position.orientation)) /
+                                              mobility_path_prediction_time_step_;
 
-        return yaw;
-    }
+            // Predictions' angular velocities
+            auto prev_orient = object.pose.pose.orientation;
+            for (auto &pred : object.predictions)
+            {
+                pred.predicted_velocity.angular.z = (getYawFromQuaternionMsg(prev_orient) -
+                                                     getYawFromQuaternionMsg(pred.predicted_position.orientation)) /
+                                                    mobility_path_prediction_time_step_;
+            }
+        }
+
+        double getYawFromQuaternionMsg(const geometry_msgs::msg::Quaternion &quaternion)
+        {
+            tf2::Quaternion orientation;
+            orientation.setX(quaternion.x);
+            orientation.setY(quaternion.y);
+            orientation.setZ(quaternion.z);
+            orientation.setW(quaternion.w);
+
+            double roll;
+            double pitch;
+            double yaw;
+            tf2::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+
+            return yaw;
+        }
+    };
+
+}
+// TODO cleanup this file
--- motion_computation/src/psm_to_external_object_convertor.h
+++ motion_computation/src/psm_to_external_object_convertor.h
@@ -1,14 +1,180 @@
-template<class T>
-class MessageToExternalObjectConvertor
+#include "message_to_external_object_convertor.h"
+#include <carma_v2x_msgs/psm.hpp>
+#include <carma_perception_msgs/external_object.hpp>
+
+namespace object
 {
 
-    public:
-        /**
-         * @brief Virtual destructor to ensure delete safety for pointers to implementing classes
-         *
-         */
-        virtual ~MessageConvertor(){};
+    class PsmToExternalObject : public MessageToExternalObjectConvertor<carma_v2x_msgs::msg::PSM>
+    {
 
-        virtual void convert(const T& in_msg, carma_perception_msgs::msg::ExternalObject& out_msg) = 0;
+        std::string frame_id_;
+        PsmToExternalObject(std::string frame_id) {
+            frame_id_ = frame_id;
+        }
+        
+        void convert(const carma_v2x_msgs::msg::PSM &in_msg, carma_perception_msgs::msg::ExternalObject &out_msg)
+        {            
+            out_msg.dynamic_obj = true; // If a PSM is sent then the object is dynamic since its a living thing
 
-};
+            // Generate a unique object id from the psm id
+            out_msg.id = 0
+            for (int i = in_msg.id.id.size() - 1; i >= 0; i--) { // using signed iterator to handle empty case
+                out_msg.id |= in_msg.id.id[i] << (8*i);
+            }
+
+            // Additionally, store the id in the bsm_id field
+            out_msg.bsm_id = in_msg.id.id;
+
+            // Compute the pose
+            out_msg.pose = pose_from_gnss(
+                map_projector_, 
+                ned_in_map_rotation_,
+                { in_msg.position.latitude, in_msg.position.longitude, in_msg.position.elevation }, 
+                in_msg.heading.heading
+            );
+
+            // Compute the position covariance
+            // TODO
+
+            in_msg.sec_mark.millisecond
+
+            // Compute the timestamp
+
+            out_msg.header.stamp = builtin_interfaces::msg::Time( get_psm_timestamp(in_msg) );
+            out_msg.header.frame_id = frame_id_;
+
+            // Set the type
+            if (in_msg.basic_type.type == carma_v2x_msgs::PersonalDeviceUserType::A_PEDESTRIAN
+                || in_msg.basic_type.type == carma_v2x_msgs::PersonalDeviceUserType::A_PUBLIC_SAFETY_WORKER
+                || in_msg.basic_type.type == carma_v2x_msgs::PersonalDeviceUserType::AN_ANIMAL) // Treat animals like people since we have no internal class for that
+            {
+                out_msg.object_type = carma_perception_msgs::msg::ExternalObject::PEDESTRIAN;
+
+                // Default pedestrian size
+                // Assume a 
+                // ExternalObject dimensions are half actual size
+                // Here we assume 1.0, 1.0, 2.0
+                out_msg.size.x = 0.5;
+                out_msg.size.y = 0.5;
+                out_msg.size.z = 1.0;
+
+            } else if (in_msg.basic_type.type == carma_v2x_msgs::PersonalDeviceUserType::A_PEDALCYCLIST) {
+                
+                out_msg.object_type = carma_perception_msgs::msg::ExternalObject::MOTORCYCLE; // Currently external object cannot represent bicycles, but motor cycle seems like the next best choice
+            
+                // Default bicycle size
+                out_msg.size.x = 1.0;
+                out_msg.size.y = 0.5;
+                out_msg.size.z = 1.0;
+
+            } else {
+                
+                out_msg.object_type = carma_perception_msgs::msg::ExternalObject::UNKNOWN;
+            
+                // Default pedestrian size
+                out_msg.size.x = 0.5;
+                out_msg.size.y = 0.5;
+                out_msg.size.z = 1.0;
+
+            }
+
+            // Set the velocity
+            out_msg.velocity.twist.linear.x = in_msg.velocity.velocity;
+
+            // Compute the velocity covariance TODO
+
+            // Set the confidence
+            // TODO
+
+            // Compute predictions 
+            // TODO
+            // For prediction, if the prediction is available we will sample it
+            // If not then assume linear motion
+
+
+
+            
+        }
+
+        rclcpp::Time get_psm_timestamp(const carma_v2x_msgs::msg::PSM &in_msg) {
+            
+            boost::posix_time::ptime utc_time_of_current_psm; 
+
+            // Get the utc epoch start time
+            static const boost::posix_time::ptime inception_boost( boost::posix_time::time_from_string("1970-01-01 00:00:00.000") ); 
+
+            // Determine if the utc time of the path history can be used instead of the sec_mark
+            // The sec_mark is susceptible to large error on minute transitions due to missing "minute of the year" field
+            // If the second mark in the path history is identical and the full utc time is provided with ms resolution 
+            // then it can be assumed the initial_position is the same as the PSM data and the utc_time can be used instead of sec_mark
+            if ((in_msg.presence_vector & carma_v2x_msgs::msg::PSM::HAS_PATH_HISTORY) 
+                && (in_msg.path_history.presence_vector & carma_v2x_msgs::msg::PathHistory::HAS_INITIAL_POSITION)
+                && (in_msg.path_history.initial_position.presence_vector & carma_v2x_msgs::msg::FullPositionVector::HAS_UTC_TIME)
+                && (in_msg.path_history.initial_position.utc_time.presence_vector 
+                    & carma_v2x_msgs::msg::FullPositionVector::YEAR
+                    & carma_v2x_msgs::msg::FullPositionVector::MONTH
+                    & carma_v2x_msgs::msg::FullPositionVector::DAY
+                    & carma_v2x_msgs::msg::FullPositionVector::HOUR
+                    & carma_v2x_msgs::msg::FullPositionVector::MINUTE 
+                    & carma_v2x_msgs::msg::FullPositionVector::SECOND)
+                && in_msg.sec_mark.millisecond == in_msg.path_history.initial_position.utc_time.second)
+            {
+                RCLCPP_DEBUG_STREAM(get_logger(), "Using UTC time of path history to determine PSM timestamp. Assumed valid since UTC is fully specified and sec_mark == utc_time.seconds in this message.");
+
+                boost::posix_time::time_duration time_of_day = hours(in_msg.path_history.initial_position.utc_time.hour) 
+                    + minutes(in_msg.path_history.initial_position.utc_time.minute) 
+                    + milliseconds(in_msg.path_history.initial_position.utc_time.second);
+                
+                boost::gregorian::date utc_day(in_msg.path_history.initial_position.utc_time.year, in_msg.path_history.initial_position.utc_time.month, in_msg.path_history.initial_position.utc_time.day);
+
+                utc_time_of_current_psm = boost::posix_time::ptime(utc_day) + time_of_day;
+                
+            } else { // If the utc time of the path history cannot be used to account for minute change over, then we have to default to the sec mark
+                
+                RCLCPP_WARN_STREAM_THROTTLE(get_logger(), get_clock(), rclcpp::Duration(5, 0), 
+                    "PSM PathHistory utc timstamp does not match sec_mark. Unable to determine the minute of the year used for PSM data. Assuming local clock is exactly synched. This is NOT ADVISED.");
+
+                // Get the current ROS time
+                auto current_time = get_clock()->now();
+                
+                // Convert the ros time to a boost duration
+                boost::posix_time::time_duration duration_since_inception( lanelet::time::durationFromSec(current_time.seconds()) );
+                
+                // Get the current ROS time in UTC
+                auto curr_time_boost = inception_boost + duration_since_inception;
+
+                // Get duration of current day
+                auto duration_in_day_till_current_time = curr_time_boost.time_of_day();
+
+                // Extract hours and minutes
+                long hour_count_in_day = duration_in_day_till_current_time.hours()
+                long minute_count_in_hour = duration_in_day_till_current_time.minutes()
+
+                // Get the duration of the minute in the day
+                auto start_of_minute_in_day = hours(hour_count_in_day) + minutes(minute_count_in_hour)
+                
+                // Get the start of the day in ROS time
+                boost::posix_time::ptime start_of_day(curr_time_boost.date());
+
+                // Ge the start of the current minute in ROS time
+                boost::posix_time::ptime utc_start_of_current_minute = start_of_day + start_of_minute_in_day;
+
+                // Compute the UTC PSM stamp from the sec_mark using ROS time as the clock
+                boost::posix_time::time_duration s_in_cur_minute = milliseconds(in_msg.sec_mark.millisecond);
+
+                utc_time_of_current_psm = utc_start_of_current_minute + s_in_cur_minute;
+
+            }
+
+            boost::posix_time::time_duration nsec_since_epoch = utc_time_of_current_psm - inception_boost;
+
+            if (nsec_since_epoch.is_special()) {
+                RCLCPP_ERROR_STREAM(get_logger(), "Computed psm nsec_since_epoch is special (computation failed). Value effectively undefined.")
+            }
+
+            return rclcpp::Time(nsec_since_epoch.total_nanoseconds());
+        }
+    };
+
+}
