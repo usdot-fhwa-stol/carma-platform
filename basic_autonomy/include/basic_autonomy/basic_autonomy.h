d2274ad87fbaa3bc8ce3179a6d970bd8c3983c53
Anish <Anishdeva17@gmail.com>
2022-04-28 10:41:43 -0400

    fix

--- basic_autonomy/include/basic_autonomy/basic_autonomy.h
+++ basic_autonomy/include/basic_autonomy/basic_autonomy.h
@@ -273,6 +273,7 @@ namespace basic_autonomy
       * \param ending_downtrack The downtrack distance at which the lane change maneuver end
       * \param wm Pointer to intialized world model for semantic map access
       * \param state The vehicle state at the time the function is called
+      * \param downsample_ratio TODO: add description
       * \param buffer_ending_downtrack The additional downtrack beyond requested end dist used to fit points along spline
       * 
       * \return A vector of geometry points as lanelet::basicpoint2d
