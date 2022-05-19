094c50f3e01d86eff7092130610bd65d288d9875
Michael McConnell <Michael.McConnell-2@leidos.com>
2022-05-19 12:50:17 -0700

    temp change

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -45,9 +45,10 @@ if [[ "$BRANCH" = "develop" ]]; then
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch $BRANCH
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch $BRANCH
 else
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch develop
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch develop
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch develop
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/hudson
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch release/hudson
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch release/hudson
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/autoware.auto.git --branch hotfix/higher_point_limit
 fi
 
 # add astuff messages
