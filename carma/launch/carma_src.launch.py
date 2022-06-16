c439fe0861afd4818cc504d4ad3bf832fb4ea7fe
CARMA Developer <carma@dot.gov>
2022-06-16 15:39:45 -0400

    fixes

--- carma/launch/carma_src.launch.py
+++ carma/launch/carma_src.launch.py
@@ -167,6 +167,7 @@ def generate_launch_description():
                 launch_arguments={
                     'route_file_folder' : route_file_folder,
                     'vehicle_characteristics_param_file' : vehicle_characteristics_param_file, 
+                    'vehicle_config_param_file' : vehicle_config_param_file,
                     'enable_guidance_plugin_validator' : enable_guidance_plugin_validator,
                     'strategic_plugins_to_validate' : strategic_plugins_to_validate,
                     'tactical_plugins_to_validate' : tactical_plugins_to_validate,
