88e80c245ea92de0762e9ca6611e9a59e6b922b1
CARMA Developer <jonsmet37@gmail.com>
2022-06-24 06:57:10 -0700

    Initial guidance ROS2

--- carma/launch/guidance.launch.py
+++ carma/launch/guidance.launch.py
@@ -53,6 +53,9 @@ def generate_launch_description():
     
     route_param_file = os.path.join(
         get_package_share_directory('route'), 'config/parameters.yaml')
+
+    guidance_param_file = os.path.join(
+        get_package_share_directory('guidance'), 'config/parameters.yaml')
     
     env_log_levels = EnvironmentVariable('CARMA_ROS_LOGGING_CONFIG', default_value='{ "default_level" : "WARN" }')
 
@@ -126,6 +129,23 @@ def generate_launch_description():
                     route_param_file
                 ]
             ),
+            ComposableNode(
+                package='guidance',
+                plugin='guidance::GuidanceWorker',
+                name='guidance_node',
+                extra_arguments=[
+                    {'use_intra_process_comms': True}, 
+                    {'--log-level' : GetLogLevel('route', env_log_levels) }
+                ],
+                remappings = [
+                    ("vehicle_status", [ EnvironmentVariable('CARMA_INTR_NS', default_value=''), "/vehicle_status" ] ),
+                    ("robot_status", [ EnvironmentVariable('CARMA_INTR_NS', default_value=''), "/controller/robot_status" ] ),
+                    ("enable_robotic", [ EnvironmentVariable('CARMA_INTR_NS', default_value=''), "/controller/enable_robotic" ] ),
+                ],
+                parameters=[
+                    guidance_param_file
+                ]
+            )
         ]
     )
 
--- guidance/src/main.cpp
+++ guidance/src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2021 LEIDOS.
+ * Copyright (C) 2022 LEIDOS.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -14,15 +14,20 @@
  * the License.
  */
 
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #include "guidance/guidance_worker.hpp"
 
-/*!
- * \brief Main entry point for Guidance package
- */
-int main(int argc, char** argv)
+int main(int argc, char **argv) 
 {
-    ros::init(argc, argv, "guidance");
-    guidance::GuidanceWorker worker;
-    return worker.run();
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<guidance::GuidanceWorker>(rclcpp::NodeOptions());
+  
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node->get_node_base_interface());
+  executor.spin();
+
+  rclcpp::shutdown();
+
+  return 0;
 }
