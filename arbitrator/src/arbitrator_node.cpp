47bbdcd45a91a1234a8f437c901886832323ddd3
Aswath <32398753+aswath1@users.noreply.github.com>
2022-11-01 14:09:59 -0400

    Update arbitrator_node.cpp

--- arbitrator/src/arbitrator_node.cpp
+++ arbitrator/src/arbitrator_node.cpp
@@ -102,6 +102,9 @@ namespace arbitrator
         arbitrator_run_ = create_timer(get_clock(),
                                 std::chrono::duration<double>(1/(config_.planning_frequency * 2 )), //there is waiting state between each planning state
                                 [this]() {this->arbitrator_->run();});
+        //The intention is to keep the arbitrator planning at intended frequency - 1s. Looks like ROS2 conversion kept the WAITING state from ROS1,
+        //which occurs between PLANNING state in the state machine. So if the timer calls state callback each 1 second, the arbitrator ends up planning 
+        //every 2s due to PLANNING > WAITING > PLANNING transition. That's why the frequency is doubled.
         RCLCPP_INFO_STREAM(rclcpp::get_logger("arbitrator"), "Arbitrator started, beginning arbitrator state machine.");
         return CallbackReturn::SUCCESS;
     }
