93497d51e6f3894c6eb3107eeee0a5683da9a39e
Anish <Anishdeva17@gmail.com>
2022-12-19 11:54:56 -0500

    sync develop with isetta changes

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -45,13 +45,13 @@ if [[ "$BRANCH" = "develop" ]]; then
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch $BRANCH
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch $BRANCH
 else
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/isetta
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch release/isetta
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch release/isetta
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch develop
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch develop
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch develop
 fi
 
 # Get humble branch of message filters which supports template Node arguments (foxy version supports rclcpp::Node only)
-git clone https://github.com/usdot-fhwa-stol/carma-message-filters.git --branch release/isetta
+git clone https://github.com/usdot-fhwa-stol/carma-message-filters.git --branch develop
 
 # add astuff messages
 # NOTE: The ibeo_msgs package is ignored because on build the cmake files in that package run a sed command 
@@ -70,14 +70,14 @@ echo "" > COLCON_IGNORE
 cd ../
 
 #rosbridge_suite is a ROS meta-package including all the rosbridge packages.
-git clone https://github.com/usdot-fhwa-stol/rosbridge_suite -b ros2
+git clone https://github.com/usdot-fhwa-stol/rosbridge_suite -branch ros2
 
 
 cd ${dir}/src
 
 # git clone --branch master --depth 1 https://github.com/nitroshare/qhttpengine.git
-git clone -b master --depth 1 https://github.com/etherealjoy/qhttpengine.git
-git clone -b isetta --depth 1 https://github.com/usdot-fhwa-OPS/V2X-Hub.git
+git clone -brnach master --depth 1 https://github.com/etherealjoy/qhttpengine.git
+git clone -branch develop --depth 1 https://github.com/usdot-fhwa-OPS/V2X-Hub.git
 cd V2X-Hub
 git config core.sparsecheckout true
 git sparse-checkout init
