801b255fac8369cb2dd69ac54ee84a1ac9a74f35
aswath <as10616@nyu.edu>
2023-04-19 04:27:03 -0700

    intersection_tm_ros2

--- intersection_transit_maneuvering/test/test_main.cpp
+++ intersection_transit_maneuvering/test/test_main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 LEIDOS.
+ * Copyright (C) 2023 LEIDOS.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,12 +15,23 @@
  */
 
 #include <gtest/gtest.h>
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 
-// Run all the tests
-int main(int argc, char** argv)
-{
-  testing::InitGoogleTest(&argc, argv);
-  ros::Time::init();
-  return RUN_ALL_TESTS();
+/*!
+* \brief Main entrypoint for unit tests
+*/
+int main (int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  //Initialize ROS
+  rclcpp::init(argc, argv);
+  auto ret = rcutils_logging_set_logger_level(
+          rclcpp::get_logger("intersection_transit_maneuvering").get_name(), RCUTILS_LOG_SEVERITY_DEBUG);
+
+  bool success = RUN_ALL_TESTS();
+
+  //shutdown ROS
+  rclcpp::shutdown();
+
+  return success;
 }
