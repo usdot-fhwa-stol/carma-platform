af49454d3457ab92aff5bc7b8196e1d301611dc6
CARMA Developer <jonsmet37@gmail.com>
2023-08-22 15:11:05 -0700

    Updates for ROS1 rosbag logging

--- carma_record/scripts/carma_record.sh
+++ carma_record/scripts/carma_record.sh
@@ -2,6 +2,14 @@
 
 while true
     do
+        # Get the flag that indiciates whether a ROS 1 rosbag should be recorded
+        use_ros1_rosbag="$(rosparam get /use_ros1_rosbag)"
+        if [ "$use_ros1_rosbag" == "false" ]
+        then
+            echo "A ROS 1 rosbag will not be recorded"
+            exit 0
+        fi
+
         # Get the regex of the excluded topics
         excluded_topics="$(rosparam get exclude_regex)"
         no_exclusions="$(rosparam get no_exclusions)"
