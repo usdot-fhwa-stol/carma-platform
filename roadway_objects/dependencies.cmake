9deda74547b4132cd3b528592fc7e0d1091394bd
Adam Morrissett <adam.l.morrissett@leidos.com>
2023-09-11 12:22:58 -0400

    #2144 roadway_objects: update CMake configurations
    Add project-specific CMake options for building tests and exporting
    compiler commands. Move dependency configurations to separate
    dependencies.cmake file. The configuration now also has clang-format
    linting added to the tests.

---
+++ roadway_objects/dependencies.cmake
@@ -0,0 +1,35 @@
+# Copyright 2023 Leidos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This will pull dependencies from <build_depend>...</build_depend> tags in the
+# package.xml file. It saves us from having to manually call find_package(...)
+# for each dependency.
+ament_auto_find_build_dependencies()
+
+if(roadway_objects_BUILD_TESTS)
+  # These CMake commands were added to ament_cmake_auto in ROS 2 Humble. Until
+  # CARMA supports ROS 2 Humble, we will use package-local copies.
+  include(cmake/ament_auto_find_test_dependencies.cmake)
+  include(cmake/ament_auto_add_gtest.cmake)
+
+  ament_auto_find_test_dependencies()
+
+  list(APPEND AMENT_LINT_AUTO_EXCLUDE
+    ament_cmake_uncrustify  # Using clang-format instead
+  )
+
+  set(ament_cmake_clang_format_CONFIG_FILE ${PROJECT_SOURCE_DIR}/.clang-format)
+
+  ament_lint_auto_find_test_dependencies()
+endif()
