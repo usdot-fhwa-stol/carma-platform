0ad01c97b7a3108435739791754af40b05699d32
Anish <Anishdeva17@gmail.com>
2023-10-03 08:41:12 -0400

    update script

--- engineering_tools/carma_script_extensions/configure-cloud-sim.bash
+++ engineering_tools/carma_script_extensions/configure-cloud-sim.bash
@@ -14,24 +14,164 @@
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
-CARMA_CLOUD_SIM_CONFIG_PATH="${CARMA_CLOUD_SIM_CONFIG_DIR}/config.txt"
+# Function to display help message
+display_help() {
+        cat <<HELP
+-------------------------------------------------------------------------------
+| USDOT FHWA STOL CARMA                                              |
+-------------------------------------------------------------------------------
+Usage: $0 {init|stop|help}
+Please enter one of the following commands when using the configure-cloud-sim extension:
+        init     - Create a VPC with internet gateway and route table"
+        stop     - Delete the created VPC, internet gateway, and route table"
+        help     - Display this help message"
+HELP
+}
 
+# Function to create the VPC, internet gateway, and route table
+create_resources() {
 
-configure-cloud-sim__init() {
-    # Configure carma cloud sim by setting up the required resources
+    if [ ! command -v "aws" &> /dev/null ]; then
+        echo "aws cli is not installed. Attempting to install. This requires sudo."
+        
+        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        unzip awscliv2.zip
+        sudo ./aws/install
 
-    # Extract config
-    local USER_ARN=$(cat ${CARMA_CLOUD_SIM_CONFIG_PATH} | grep "USER_ARN=" | sed "s|USER_ARN=||")
-
-    if [ -z "$USER_ARN" ]; then
-        echo "Configuration does not include AWS User ARN. Please run: carma cloud-sim config -a <User ARN>"
+        echo "NOTE: You need to manually configure the aws cli for your user access key. Refer to this video for instructions https://www.youtube.com/watch?v=Rp-A84oh4G8"
+        exit 1
     fi
+    
+    # Try deleting XiL Cloud resources if any exist
+    echo "Checking and deleting existing XiL-Cloud resources first"
+    delete_resources
+
+    vpc_success=0
+    igw_success=0
+    route_table_success=0
+
+    # Set your desired CIDR block here
+    CIDR_Block="10.0.0.0/16"
 
     # Create VPC
-    aws ec2 create-vpc --cidr-block <CIDR_Block> --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=XiL-Cloud VPC}]'
-    local VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=XiL-Cloud VPC" --query 'Vpcs[].VpcId' --output text)
-    echo $VPC_ID
+    vpc_id=$(aws ec2 create-vpc --cidr-block $CIDR_Block --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=XiL-Cloud VPC}]' \
+    --query 'Vpc.VpcId' --output text) 
 
+    # Check the exit code to determine if the VPC creation was successful
+    if [ $? -eq 0 ]; then
+        vpc_success=1
+        echo "Created vpc: $vpc_id"
+    else
+        echo "Error creating XiL-Cloud VPC: $vpc_id"
+    fi
+
+    # Create Internet Gateway
+    igw_id=$(aws ec2 create-internet-gateway \
+    --tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Name,Value=XiL-Cloud IGW}]' --query 'InternetGateway.InternetGatewayId' \
+    --output text) 
+
+    # Check the exit code to determine if the Internet Gateway creation was successful
+    if [ $? -eq 0 ]; then
+        igw_success=1
+        echo "Created Internet Gateway: $igw_id"
+    else
+        echo "Error creating Internet Gateway: $igw_id"
+    fi
+
+    # Attach Internet Gateway to VPC
+    aws ec2 attach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id
+
+    # Create Route Table
+    route_table_id=$(aws ec2 create-route-table --vpc-id $vpc_id  \
+    --tag-specifications 'ResourceType=route-table,Tags=[{Key=Name,Value=XiL-Cloud RouteTable}]' --query 'RouteTable.RouteTableId' \
+    --output text)
+
+    # Check the exit code to determine if the Route Table creation was successful
+    if [ $? -eq 0 ]; then
+        route_table_success=1
+        echo "Created route table: $route_table_id"
+    else
+        echo "Error creating Route Table: $route_table_id"
+    fi
+
+    # Create a default route to the Internet Gateway
+    aws ec2 create-route --route-table-id $route_table_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id &> /dev/null
+
+    if [ $? -eq 0 ]; then
+        echo "Default route created successfully!"
+    else
+        echo "Error creating default route to Internet Gateway."
+    fi
+
+
+    if [ $vpc_success -eq 1 ] && [ $igw_success -eq 1 ] && [ $route_table_success -eq 1 ]; then
+        echo "XiL-Cloud required resources were created successfully!"
+    else
+        echo "Some resources encountered errors. See above for details."
+    fi
+
+}
+
+# Function to delete the VPC, internet gateway, and route table based on tag name
+delete_resources() {
+
+    if [ ! command -v "aws" &> /dev/null ]; then
+        echo "aws cli is not installed. Attempting to install. This requires sudo."
+        
+        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        unzip awscliv2.zip
+        sudo ./aws/install
+
+        echo "NOTE: You need to manually configure the aws cli for your user access key. Refer to this video for instructions https://www.youtube.com/watch?v=Rp-A84oh4G8"
+        exit 1
+    fi
+
+    vpc_success=0
+    igw_success=0
+    route_table_success=0
+
+    # Get VPC ID based on tag name
+    vpc_id=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=XiL-Cloud VPC" --query 'Vpcs[0].VpcId' --output text)
+
+    # Get Internet Gateway ID based on tag name
+    igw_id=$(aws ec2 describe-internet-gateways --filters "Name=tag:Name,Values=XiL-Cloud IGW" \
+    --query 'InternetGateways[0].InternetGatewayId' --output text)
+
+
+    # Get Route Table ID based on tag name
+    route_table_id=$(aws ec2 describe-route-tables --filters "Name=tag:Name,Values=XiL-Cloud RouteTable" \
+    --query 'RouteTables[0].RouteTableId' --output text)
+
+    if [ -z "$route_table_id" ] || [ $route_table_id != "None" ]; then
+        # Delete the route table
+        aws ec2 delete-route-table --route-table-id $route_table_id
+        route_table_success=1
+        echo "Deleted Route Table: $route_table_id"
+    fi
+
+    if [ -z "$igw_id" ] || [ $igw_id != "None" ]; then
+        # Detach and delete the Internet Gateway
+        aws ec2 detach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id
+        aws ec2 delete-internet-gateway --internet-gateway-id $igw_id
+        igw_success=1
+        echo "Deleted Internet Gateway: $igw_id"
+    fi
+
+    if [ -z "$vpc_id" ] || [ $vpc_id != "None" ]; then
+        # Delete the VPC
+        aws ec2 delete-vpc --vpc-id $vpc_id
+        vpc_success=1
+        echo "Deleted VPC: $vpc_id"
+    fi
+
+    if [ $vpc_success -eq 1 ] && [ $igw_success -eq 1 ] && [ $route_table_success -eq 1 ]; then
+        echo "XiL-Cloud VPC, Internet Gateway and Route Table were deleted successfully!"
+    else
+        echo "No resources required to be deleted"
+    fi
+
+    
+    
 }
 
 configure-cloud-sim__help() {
@@ -40,19 +180,28 @@ configure-cloud-sim__help() {
 | USDOT FHWA STOL CARMA                                              |
 -------------------------------------------------------------------------------
 
-Please enter one of the following commands when using the cloud-sim extension:
-    carma
-        
+Please enter one of the following commands when using the configure-cloud-sim extension:
+        init     - Create a VPC with internet gateway and route table"
+        stop     - Delete the created VPC, internet gateway, and route table"
+        help     - Display this help message"
 HELP
 }
 
 
-configure-cloud-sim() {
-    local cmdname=$1; shift
-    if type "configure-cloud-sim__$cmdname" >/dev/null 2>&1; then
-        "configure-cloud-sim__$cmdname" "$@"
-    else
-        configure-cloud-sim_help
+# Check for the argument and perform the corresponding action
+case "$1" in
+    init)
+        create_resources
+        ;;
+    stop)
+        delete_resources
+        ;;
+    help)
+        display_help
+        ;;
+    *)
+        echo "Invalid argument."
+        display_help
         exit 1
-    fi
-}
+        ;;
+esac
