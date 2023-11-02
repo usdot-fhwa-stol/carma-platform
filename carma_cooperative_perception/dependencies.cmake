dbbddd39f92b7501693160b0c979d428559ce644
Adam Morrissett <adam.l.morrissett@leidos.com>
2023-11-02 09:43:19 -0400

    #2164 Update multiple_object_tracking dependency
    The carma_cooperative_perception library was previously installing the
    library as a source dependency, but now colcon will build it for us.

--- carma_cooperative_perception/dependencies.cmake
+++ carma_cooperative_perception/dependencies.cmake
@@ -28,19 +28,21 @@ CPMAddPackage(NAME units
 
 include(FetchContent)
 
-FetchContent_Declare(cooperative_perception
-  GIT_REPOSITORY https://github.com/usdot-fhwa-stol/multiple_object_tracking
-  GIT_TAG c6e0fbeebb775b93392738985d948c67784c9328
-)
+# FetchContent_Declare(cooperative_perception
+#   GIT_REPOSITORY https://github.com/usdot-fhwa-stol/multiple_object_tracking
+#   GIT_TAG origin/develop
+# )
 
-FetchContent_GetProperties(cooperative_perception)
-if(NOT cooperative_perception_POPULATED)
-  FetchContent_Populate(cooperative_perception)
+# FetchContent_GetProperties(cooperative_perception)
+# if(NOT cooperative_perception_POPULATED)
+#   FetchContent_Populate(cooperative_perception)
 
-  set(CMAKE_POSITION_INDEPENDENT_CODE TRUE CACHE INTERNAL "")
+#   set(CMAKE_POSITION_INDEPENDENT_CODE TRUE CACHE INTERNAL "")
 
-  add_subdirectory(${cooperative_perception_SOURCE_DIR} ${cooperative_perception_BINARY_DIR})
-endif()
+#   add_subdirectory(${cooperative_perception_SOURCE_DIR} ${cooperative_perception_BINARY_DIR})
+# endif()
+
+find_package(cooperative_perception_core REQUIRED)
 
 # CARMA currently uses PROJ version 6.3.1, which is not designed to be incorporated
 # as a subdirectory into larger projects. If CARMA upgrades to a newer version, we
