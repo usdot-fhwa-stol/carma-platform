623f636dd6b2ccbacc6b9cd97546de9f1f92d7e9
CARMA Developer <willjohnsonk@gmail.com>
2024-01-05 09:52:18 -0800

    Minor directory access change

--- engineering_tools/rsync_carma_config.sh
+++ engineering_tools/rsync_carma_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
 inotifywait -e modify,create,delete,move -r /var/lib/docker/volumes/carma-config-data && \
-rsync -av --delete /var/lib/docker/volumes/carma-config-data/_data /export/carma_volume
+rsync -av --delete /var/lib/docker/volumes/carma-config-data/_data/ /export/carma_volume
 
 done
