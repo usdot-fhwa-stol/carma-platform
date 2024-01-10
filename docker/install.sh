03c2ef561ac93cc45fefec4e13148fec55b67f66
Adam Morrissett <adam.l.morrissett@leidos.com>
2024-01-10 12:34:39 -0500

    #2267 Move multiple object tracking dep install
    The multiple object tracking library dependency installation script
    invocation moved to install.sh.

--- docker/install.sh
+++ docker/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #  Copyright (C) 2018-2023 LEIDOS.
-# 
+#
 #  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 #  use this file except in compliance with the License. You may obtain a copy of
 #  the License at
-# 
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -66,6 +66,8 @@ fi
 cd ~/carma_ws
 
 echo "Building ROS2 CARMA Components"
+
+./multiple_object_tracking/scripts/install_dependencies.sh
 
 if [[ ! -z "$ROS1_PACKAGES$ROS2_PACKAGES" ]]; then
     if [[ ! -z "$ROS2_PACKAGES" ]]; then
