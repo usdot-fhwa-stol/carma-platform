eae6c0dbd8a58abcaa632070c866573a4db44575
CARMA Developer <jonsmet37@gmail.com>
2024-04-03 11:55:14 -0700

    Revert carma-utils branch in checkout.bash

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -46,7 +46,7 @@ if [[ "$BRANCH" = "develop" ]]; then
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch $BRANCH
 else
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/lavida
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch fix/system-alert-transient-local
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-utils.git --branch release/lavida
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-messenger.git --branch release/lavida
 fi
 
