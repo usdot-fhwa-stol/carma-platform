a43a6aab484a4a0c92a7a12012955f688ae38a0c
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-01-17 12:01:18 -0700

    Remove unnecessary

--- carma/launch/ros2_rosbag.launch.py
+++ carma/launch/ros2_rosbag.launch.py
@@ -36,7 +36,6 @@ def record_ros2_rosbag(context: LaunchContext, vehicle_config_param_file, rosbag
 
     overriding_qos_profiles = context.perform_substitution(rosbag2_qos_override_param_file)
 
-    print(f"=====Location found: {overriding_qos_profiles}")
     # Open vehicle config params file to process various rosbag settings
     with open(vehicle_config_param_file_string, 'r') as f:
 
