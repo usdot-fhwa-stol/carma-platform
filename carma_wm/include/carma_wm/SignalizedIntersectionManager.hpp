a880719286e86618e46ab2cf2dd41d807fa4204d
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-02-11 14:20:57 -0500

    Feature/display map msg (#2485)

--- carma_wm/include/carma_wm/SignalizedIntersectionManager.hpp
+++ carma_wm/include/carma_wm/SignalizedIntersectionManager.hpp
@@ -55,17 +55,17 @@ public:
 
   SignalizedIntersectionManager(){}
 
-  /*! 
+  /*!
   *
-  *  \brief Copy constructor that copies everything except the traffic signal states. 
+  *  \brief Copy constructor that copies everything except the traffic signal states.
   *         This is to keep the states although the map is updated or a similar event happened
   *         NOTE: The function does not update the map with new elements
   *  \param[out] other manager
   */
   SignalizedIntersectionManager(const SignalizedIntersectionManager& other);
-  
-  /*! 
-  *  \brief Assignment operator that copies everything except the traffic signal states. 
+
+  /*!
+  *  \brief Assignment operator that copies everything except the traffic signal states.
   *         This is to keep the states although the map is updated or a similar event happened
   *         NOTE: The function does not update the map with new elements
   *  \param[out] other manager
@@ -73,60 +73,60 @@ public:
   SignalizedIntersectionManager& operator=(SignalizedIntersectionManager other);
 
   /*!
-  *  \brief Equality operator that checks if every mapping are same except the traffic signal states. 
+  *  \brief Equality operator that checks if every mapping are same except the traffic signal states.
   *         This is to keep the states although the map is updated or a similar event happened
   *         NOTE: The function does not update the map with new elements
   *  \param[out] rhs manager
   */
   bool operator==(const SignalizedIntersectionManager& rhs);
 
-  /*! 
+  /*!
   *  \brief Create relevant signalized intersection and carma traffic signals based on the MAP.msg and the lanelet_map
   *         NOTE: The function does not update the map with new elements
   *  \param[out] intersections to return
   *  \param[out] traffic_signals to return
   *  \param map_msg MAP.msg that consists all static data portion of the intersection
-  *  \param map lanelet_map to check 
+  *  \param map lanelet_map to check
   *  \param routing_graph of the lanelet map to accurately detect lanes
   */
-  void createIntersectionFromMapMsg(std::vector<lanelet::SignalizedIntersectionPtr>& intersections, std::vector<lanelet::CarmaTrafficSignalPtr>& traffic_signals, const carma_v2x_msgs::msg::MapData& map_msg, 
+  void createIntersectionFromMapMsg(std::vector<lanelet::SignalizedIntersectionPtr>& intersections, std::vector<lanelet::CarmaTrafficSignalPtr>& traffic_signals, const carma_v2x_msgs::msg::MapData& map_msg,
                                     const std::shared_ptr<lanelet::LaneletMap>& map, std::shared_ptr<const lanelet::routing::RoutingGraph> routing_graph);
 
-  /*! 
+  /*!
   *  \brief Returns mapping of MAP lane id to lanelet id for the given map and intersection.msg in the MAP.msg.
             This function also records signal_group_id_to its own lanelet id, and also signal group to entry and exit lanelets id mappings
   *  \param[out] entry lane id to lanelet id mappings to return
   *  \param[out] exit  lane id to lanelet id mappings to return
   *  \param intersection MAP.msg that consists all static data portion of the intersection
-  *  \param map lanelet_map to check 
+  *  \param map lanelet_map to check
   *  \param routing_graph of the lanelet map to accurately detect lanes
   *  \throw invalid_argument if given coordinates in the msg doesn't exist in the map
   *         TODO: Need to think about error buffer in the future. Map msgs are made from google maps or open streets maps normally so this function might run into some errors from that.
   */
-  void convertLaneToLaneletId(std::unordered_map<uint8_t, lanelet::Id>& entry, std::unordered_map<uint8_t, lanelet::Id>& exit, const carma_v2x_msgs::msg::IntersectionGeometry& intersection, 
+  void convertLaneToLaneletId(std::unordered_map<uint8_t, lanelet::Id>& entry, std::unordered_map<uint8_t, lanelet::Id>& exit, const carma_v2x_msgs::msg::IntersectionGeometry& intersection,
                               const std::shared_ptr<lanelet::LaneletMap>& map, std::shared_ptr<const lanelet::routing::RoutingGraph> current_routing_graph);
 
   /*!
-   * \brief Sets the max lane width in meters. Map msg points are associated to a lanelet if they are 
+   * \brief Sets the max lane width in meters. Map msg points are associated to a lanelet if they are
    *        within this distance to a lanelet as map msg points are guaranteed to apply to a single lane
    */
   void setMaxLaneWidth(double max_lane_width);
 
-  /*! 
+  /*!
   *  \brief Returns existing signalized intersection with same entry and exit llts if exists.
   *  \param entry_llts of the intersection
   *  \param exit_llts of the intersection
   *  \return id of the matching intersection in the map, or lanelet::InvalId if none exists
   */
   lanelet::Id matchSignalizedIntersection(const lanelet::Lanelets& entry_llts, const lanelet::Lanelets& exit_llts);
-  
-  /*! 
+
+  /*!
   *  \brief Saves the georeference string to be used for converting MAP.msg coordinates
   *  \param target_frame PROJ string of the map
   */
   void setTargetFrame(const std::string& target_frame);
 
-  /*! 
+  /*!
   *  \brief Returns carma traffic signal and saves all id relevant id mappings (signal group to lanelet id) internally
   *  \param signal_group_id of the traffic signal
   *  \param exit_llts of the signal_group
@@ -135,7 +135,7 @@ public:
   */
   std::shared_ptr<lanelet::CarmaTrafficSignal> createTrafficSignalUsingSGID(uint8_t signal_group_id, const lanelet::Lanelets& entry_lanelets, const lanelet::Lanelets& exit_lanelets);
 
-  /*! 
+  /*!
   *  \brief Returns carma traffic signal and saves all id relevant id mappings (signal group to lanelet id) internally
   *  \param signal_group_id of the traffic signal
   *  \param exit_llts of the signal_group
@@ -143,12 +143,18 @@ public:
   *  \return traffic signal corresponding to the signal group
   */
   lanelet::Lanelets identifyInteriorLanelets(const lanelet::Lanelets& entry_llts, const std::shared_ptr<lanelet::LaneletMap>& map);
-  
+
+  // SignalizedIntersection's geometry points from MAP Msg
+  std::unordered_map<uint16_t, std::vector<lanelet::Point3d>> intersection_nodes_;
+
   // SignalizedIntersection's reference point correction pair of (x, y) for each intersection_id
   std::unordered_map<uint16_t, std::pair<double, double>> intersection_coord_correction_;
-  
+
   // SignalizedIntersection quick id lookup
   std::unordered_map<uint16_t, lanelet::Id> intersection_id_to_regem_id_;
+
+  // SignalizedIntersection reverse quick id lookup
+  std::unordered_map<lanelet::Id, uint16_t> regem_id_to_intersection_id_;
 
   // CarmaTrafficSignal quick id lookup
   std::unordered_map<uint8_t, lanelet::Id> signal_group_to_traffic_light_id_;
--- carma_wm_ctrl/src/WMBroadcasterNode.cpp
+++ carma_wm_ctrl/src/WMBroadcasterNode.cpp
@@ -139,6 +139,9 @@ carma_ros2_utils::CallbackReturn WMBroadcasterNode::handle_on_configure(const rc
   //TCM Visualizer pub
   tcm_visualizer_pub_= create_publisher<visualization_msgs::msg::MarkerArray>("tcm_visualizer",1);
 
+  //J2735 MAP msg Visualizer pub
+  j2735_map_msg_visualizer_pub_= create_publisher<visualization_msgs::msg::MarkerArray>("j2735_map_msg_visualizer",1);
+
   //TCR Visualizer pub (visualized on UI)
   tcr_visualizer_pub_ = create_publisher<carma_v2x_msgs::msg::TrafficControlRequestPolygon>("tcr_bounding_points",1);
 
@@ -191,6 +194,7 @@ carma_ros2_utils::CallbackReturn WMBroadcasterNode::handle_on_activate(const rcl
 bool WMBroadcasterNode::spin_callback()
 {
   tcm_visualizer_pub_->publish(wmb_->tcm_marker_array_);
+  j2735_map_msg_visualizer_pub_->publish(wmb_->j2735_map_msg_marker_array_);
   tcr_visualizer_pub_->publish(wmb_->tcr_polygon_);
   wmb_->publishLightId();
   //updating upcoming traffic signal group id and intersection id
