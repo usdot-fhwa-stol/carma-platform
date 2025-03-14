98e567df9bfbdc56f2374868fb71f9866d4b47aa
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-03-14 10:18:27 -0400

    Arc 303 remove foxy references (#2516)

--- carma_cooperative_perception/src/host_vehicle_filter_component.cpp
+++ carma_cooperative_perception/src/host_vehicle_filter_component.cpp
@@ -196,8 +196,6 @@ auto euclidean_distance_squared(
 }  // namespace carma_cooperative_perception
 
 // This is not our macro, so we should not worry about linting it.
-// clang-tidy added support for ignoring system macros in release 14.0.0 (see the release notes
-// here: https://releases.llvm.org/14.0.0/tools/clang/tools/extra/docs/ReleaseNotes.html), but
-// ament_clang_tidy for ROS 2 Foxy specifically looks for clang-tidy-6.0.
+
 RCLCPP_COMPONENTS_REGISTER_NODE(                        // NOLINT
   carma_cooperative_perception::HostVehicleFilterNode)  // NOLINT
