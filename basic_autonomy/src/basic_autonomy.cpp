996f0143dcc03a166e505a6123911a2c855a32d8
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-03-25 10:51:55 -0400

    leftover

--- basic_autonomy/src/basic_autonomy.cpp
+++ basic_autonomy/src/basic_autonomy.cpp
@@ -839,10 +839,10 @@ namespace basic_autonomy
                 resized_basic_points.resize(400);
                 RCLCPP_DEBUG_STREAM(rclcpp::get_logger(BASIC_AUTONOMY_LOGGER), "Resized basic_points size: " << resized_basic_points.size());
 
-                size_t left_points_size = basic_points.size() - resized_basic_points.size();
+                size_t left_points_size = points_with_min_dis.size() - resized_basic_points.size();
                 RCLCPP_DEBUG_STREAM(rclcpp::get_logger(BASIC_AUTONOMY_LOGGER), "Number of left out basic_points size: " << left_points_size);
 
-                float percent_points_lost = 100.0 * (float)left_points_size/basic_points.size();
+                float percent_points_lost = 100.0 * (float)left_points_size/points_with_min_dis.size();
 
                 if (percent_points_lost > 50.0)
                 {
