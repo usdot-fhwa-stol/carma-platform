979d2d978b1eb4d696593d13c50cceede6f8edf6
CARMA Developer <jonsmet37@gmail.com>
2025-06-05 07:55:18 -0700

    Additional updates to switch to prod carma-cloud instance and improve comments

--- carma_cloud_client/launch/scripts/call.sh
+++ carma_cloud_client/launch/scripts/call.sh
@@ -1,7 +1,7 @@
 
 REMOTE_USER="ubuntu";
-REMOTE_ADDR="carma-cloud.com";
-KEY_FILE="carma-cloud-1.pem";
+REMOTE_ADDR="carma-cloud.com"; # Dev instance is www.carma-cloud.com, prod instance is carma-cloud.com
+KEY_FILE="carma-cloud-1.pem"; # Dev key is carma-cloud-test-1.pem, prod key is carma-cloud-1.pem
 HOST_PORT="33333"; # This port is forwarded to remote host (carma-cloud) and port: 8080
 REMOTE_PORT="22222"; # This port is forwarded to local host (v2xhub) and port: 22222
 sudo ./open_tunnels.sh -u $REMOTE_USER -a $REMOTE_ADDR -r $HOST_PORT -k $KEY_FILE -p $REMOTE_PORT
