00338b668a14ff11d89760496a8d61d5c1689272
Misheel Bayartsengel <misheel.bayart@gmail.com>
2026-08-03 15:37:38 +0000

    Fix build

--- carma_wm/src/WMListenerWorker.cpp
+++ carma_wm/src/WMListenerWorker.cpp
@@ -118,30 +118,30 @@ void WMListenerWorker::enableUpdatesWithoutRoute()
 }
 
 // helper function to log SignalizedIntersectionManager content
-void logSignalizedIntersectionManager(const carma_wm::SignalizedIntersectionManager& sim)
+void logSignalizedIntersectionManager(const carma_wm::SignalizedIntersectionManager& sim, const rclcpp::Logger& logger)
 {
   for (auto const &[intersection_id, regulatory_element_id] : sim.intersection_id_to_regem_id_)
   {
-    RCLCPP_DEBUG_STREAM(logger_,
+    RCLCPP_DEBUG_STREAM(logger,
       "inter id: " << intersection_id << ", regem id: " << regulatory_element_id);
   }
   for (auto const &[signal_id, entry_llt_ids] : sim.signal_group_to_entry_lanelet_ids_)
   {
     for (const auto & entry_llt_id : entry_llt_ids)
     {
-      RCLCPP_DEBUG_STREAM(logger_, "signal id: " << (int)signal_id << ", entry llt id: " << entry_llt_id);
+      RCLCPP_DEBUG_STREAM(logger, "signal id: " << (int)signal_id << ", entry llt id: " << entry_llt_id);
     }
   }
   for (auto const &[signal_id, exit_llt_ids] : sim.signal_group_to_exit_lanelet_ids_)
   {
     for (const auto & exit_llt_id : exit_llt_ids)
     {
-      RCLCPP_DEBUG_STREAM(logger_, "signal id: " << (int)signal_id << ", exit llt id: " << exit_llt_id);
+      RCLCPP_DEBUG_STREAM(logger, "signal id: " << (int)signal_id << ", exit llt id: " << exit_llt_id);
     }
   }
   for (auto const &[signal_id, regem_id] : sim.signal_group_to_traffic_light_id_)
   {
-    RCLCPP_DEBUG_STREAM(logger_, "signal id: " << (int)signal_id << ", regem id: " << regem_id);
+    RCLCPP_DEBUG_STREAM(logger, "signal id: " << (int)signal_id << ", regem id: " << regem_id);
   }
 }
 
@@ -234,7 +234,7 @@ void WMListenerWorker::mapUpdateCallback(autoware_lanelet2_msgs::msg::MapBin::Sh
   if (gf_ptr->sim_.intersection_id_to_regem_id_.size() > 0)
   {
     world_model_->sim_ = gf_ptr->sim_;
-    logSignalizedIntersectionManager(world_model_->sim_);
+    logSignalizedIntersectionManager(world_model_->sim_, logger_);
   }
 
   RCLCPP_DEBUG_STREAM(logger_, "Geofence id" << gf_ptr->id_ << " requests removal of size: " << gf_ptr->remove_list_.size());
